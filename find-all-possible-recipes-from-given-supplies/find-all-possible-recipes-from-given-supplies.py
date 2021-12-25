31dd6f38c41ac7af9014e93c111859ce51ffa351
Himanshu Soni <himanshusoni3105@outlook.com>
2021-12-25 21:44:04 +0530

    Time: 936 ms (18.75%), Space: 16.8 MB (87.50%) - LeetHub

---
+++ find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        graph = defaultdict(lambda:[])
+        req = defaultdict(lambda:[])
+        
+        for (i, recipe) in enumerate(recipes):
+            for ingredient in ingredients[i]:
+                graph[ingredient].append(recipe)
+                
+            req[recipe] = len(ingredients[i])
+            
+        stack = [supply for supply in supplies]
+        ans = []
+        
+        while stack: 
+            u = stack.pop()
+            for v in graph[u]:
+                req[v] -= 1
+                if req[v] == 0:
+                    stack.append(v)
+                    ans.append(v)
+                    
+        return ans
