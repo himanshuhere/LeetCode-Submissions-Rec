18f39ba0c93d443535786430112e007b5869250c
Himanshu Soni <himanshusoni3105@outlook.com>
2021-12-28 19:09:57 +0530

    Time: 92 ms (7.69%), Space: 57.4 MB (5.03%) - LeetHub

--- best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -6,10 +6,26 @@ class Solution:
         #we ll buy that so technically this code covers all this
         
         #note: you can buy it then immediately sell it on the same day.
-        profit = 0
         
-        for i in range(1, len(prices)):
-            if prices[i] > prices[i-1]:
-                profit += prices[i]-prices[i-1]
+#         profit = 0
+#         for i in range(1, len(prices)):
+#             if prices[i] > prices[i-1]:
+#                 profit += prices[i]-prices[i-1]
 
-        return profit
+#         return profit
+    
+        
+        n = len(prices)
+        
+        @lru_cache(None)
+        def dp(i, canBuy):
+            if i == n:
+                return 0
+            ans = dp(i+1, canBuy)  # Skip
+            if canBuy:
+                ans = max(ans, dp(i+1, False) - prices[i])  # Buy
+            else:
+                ans = max(ans, dp(i+1, True) + prices[i])  # Sell
+            return ans
+        
+        return dp(0, True)
