1ab42cfb9c3c24ee0f22920654c02f494523922f
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-05 13:02:01 +0530

    Time: 1106 ms (7.00%), Space: 30.4 MB (34.74%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -0,0 +1,27 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        #check
+        def isPalind(i, j, s):
+            while i <= j:
+                if s[i] != s[j]:
+                    return False
+                i += 1
+                j -= 1
+            return True
+        
+        #Backtracking
+        def backtrack(i, tmp):
+            if i >= len(s):
+                res.append(tmp[:])
+                return
+            
+            for j in range(i, len(s)):
+                if isPalind(i, j, s):
+                    tmp.append(s[i:j+1])
+                    backtrack(j+1, tmp)
+                    tmp.pop()
+        res = []
+        tmp = []
+        backtrack(0, tmp)
+        return res
+        
