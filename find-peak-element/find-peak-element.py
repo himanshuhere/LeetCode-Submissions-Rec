0aa531db1c6bd0eaf1a6e7bb6c4e46538528bb8b
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-05 18:30:51 +0530

    Time: 93 ms, Memory: 14.5 MB - LeetHub

---
+++ find-peak-element/find-peak-element.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        #Binary Search on answer - withoit sorted array
+        if len(nums) == 1:  return 0
+        
+        lo, hi = 0, len(nums) - 1
+        while lo <= hi:
+            mid = lo + (hi - lo) // 2
+            
+            #edges
+            if mid == 0 and nums[mid] > nums[mid + 1]:  return mid
+            if mid == len(nums) - 1 and nums[mid] > nums[mid - 1]:  return mid   
+            
+            if nums[mid - 1] < nums[mid] > nums[mid + 1]: return mid
+            
+            elif nums[mid] < nums[mid + 1]:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+        return -1
+                
+                    
