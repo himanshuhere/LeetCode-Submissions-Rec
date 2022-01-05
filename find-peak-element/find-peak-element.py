53c64a9dfd1e67523585960fbc40a972e3993974
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-05 19:07:12 +0530

    Time: 63 ms, Memory: 14.5 MB - LeetHub

--- find-peak-element/find-peak-element.py
+++ find-peak-element/find-peak-element.py
@@ -1,22 +1,30 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         #Binary Search on answer - withoit sorted array
-        if len(nums) == 1:  return 0
+#         if len(nums) == 1:  return 0
         
-        lo, hi = 0, len(nums) - 1
-        while lo <= hi:
-            mid = lo + (hi - lo) // 2
+#         lo, hi = 0, len(nums) - 1
+#         while lo <= hi:
+#             mid = lo + (hi - lo) // 2
             
-            #edges
-            if mid == 0 and nums[mid] > nums[mid + 1]:  return mid
-            if mid == len(nums) - 1 and nums[mid] > nums[mid - 1]:  return mid   
+#             #edges
+#             if mid == 0 and nums[mid] > nums[mid + 1]:  return mid
+#             if mid == len(nums) - 1 and nums[mid] > nums[mid - 1]:  return mid   
             
-            if nums[mid - 1] < nums[mid] > nums[mid + 1]: return mid        #accessing mid, thus TEMPlate 1
+#             if nums[mid - 1] < nums[mid] > nums[mid + 1]: return mid        #accessing mid, thus TEMPlate 1
             
-            elif nums[mid] < nums[mid + 1]:
-                lo = mid + 1
-            else:
-                hi = mid - 1
-        return -1
+#             elif nums[mid] < nums[mid + 1]:
+#                 lo = mid + 1
+#             else:
+#                 hi = mid - 1
+#         return -1
                 
-                    
+        #2 Adv Template
+        lo, hi = 0, len(nums)-1
+        while lo < hi:
+            mid = lo + (hi-lo)//2
+            if nums[mid] > nums[mid + 1]:
+                hi = mid
+            else:
+                lo = mid + 1
+        return lo
