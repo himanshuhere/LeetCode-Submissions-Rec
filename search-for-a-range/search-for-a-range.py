9c5aff9504cf11625efc6604aa83b5fbbf4d58b0
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-06 12:11:06 +0530

    Time: 109 ms, Memory: 15.4 MB - LeetHub

---
+++ search-for-a-range/search-for-a-range.py
@@ -0,0 +1,39 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        result = []
+        result.append(self.getfirstposition(nums, target))
+        result.append(self.getlastposition(nums, target))
+        return result
+        
+    def getfirstposition(self, nums, target):
+        index = -1
+        low = 0 
+        high = len(nums) - 1
+        while low <= high:
+            midposition = low + (high-low)//2
+            if nums[midposition] == target:
+                index = midposition
+            #we cant do it else if. after finding we have idnex then we wll check on left side
+            if nums[midposition] >= target:     ####
+                high = midposition - 1
+            else:
+                low = midposition + 1
+        return index
+    
+    
+    def getlastposition(self, nums, target):
+        index = -1
+        low = 0 
+        high = len(nums) - 1
+        while low <= high:
+            midposition = low + (high-low)//2
+            if nums[midposition] == target:
+                index = midposition
+            #we cant do it else if. after finding we have idnex then we wll check on left side
+  
+            if nums[midposition] > target:      ###
+                high = midposition - 1
+            else:
+                low = midposition + 1
+        return index
+        
