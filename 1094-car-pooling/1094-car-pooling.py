067e4c875f953fc979087b6bb5ae08913853c6c9
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-06 10:53:58 +0530

    Time: 68 ms (69.03%), Space: 14.9 MB (18.85%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,10 +1,12 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        #I did myself
         m = collections.defaultdict(int)
         for p,u,v in trips:
             m[u] += p
             m[v] += -p
         A = sorted(m)
+        
         curc = 0
         for i in A:
             curc += (m[i])
