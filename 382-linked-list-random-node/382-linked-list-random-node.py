5876a5dd280c2ac92bb095edc56a0b499592810f
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-07 12:22:19 +0530

    Time: 133 ms (31.76%), Space: 17.4 MB (23.25%) - LeetHub

--- 382-linked-list-random-node/382-linked-list-random-node.py
+++ 382-linked-list-random-node/382-linked-list-random-node.py
@@ -19,7 +19,29 @@ class Solution:
         # The choices() method returns a list with the randomly selected element from the specified sequence.
         # As we want in range of 0 to n - 1
 	    return random.choice(self.res)
+    
+    #Reservoir Sampling - o(n)/o(1)
+    def __init__(self, head: ListNode):
         
+        # Note that the head is guaranteed to be not null, so it contains at least one node.
 
+        self.head = head
+
+    def getRandom(self) -> int:
+       
+        # Returns a random node's value.
+        
+        curr = self.head
+        res = 0
+        index = 1
+        
+        while curr:
+            if random.random() < (1/index):
+                res = curr.val
+            
+            index += 1
+            curr = curr.next
+        
+        return res
 
 #Reservoir sampling which is a family of randomized algorithms for sampling from a population of unknown size.
