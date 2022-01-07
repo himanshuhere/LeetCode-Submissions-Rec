3b4083ba2c0a31cc882d04de579537e8fac47561
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-07 19:47:34 +0530

    Time: 132 ms, Memory: 14.8 MB - LeetHub

---
+++ find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.py
@@ -0,0 +1,25 @@
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        #Find Smallest Letter Greater Than Target
+        #find ceil of ele in sorted array, dont include ele fins greater than that
+        #find the next char in sorted string or albhabets
+        #binary search
+        index = bisect.bisect(letters, target)
+        return letters[index % len(letters)]
+        
+        
+        
+        lo = 0
+        hi = len(letters)
+        res = ""
+        
+        while lo <= hi:
+            mid = lo + (hi - lo // 2)
+            if letters[mid] == target:
+                lo = mid + 1
+            elif letters[mid] < target:
+                lo = mid + 1
+            else:
+                res = letters[mid]
+                hi = mid - 1
+        return res
