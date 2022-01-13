627eaee62303a76207b6bd3f7e7ed3848a814270
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-13 20:11:23 +0530

    Time: 1499 ms (39.68%), Space: 59.2 MB (35.36%) - LeetHub

---
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        #sort on ending pos, shoot on first balloons ending pos, assuming that if end is sorted folowing baloons will be inside this cur end and now take care of start only. if cur shot pos is lesser than start then new arrow and update the arrow pos to its end else no new arrow so no new c++.
+        
+        ans = 1
+        points = sorted(points, key=lambda x:x[1])
+        arrowpos = points[0][1]
+        
+        for i in range(1, len(points)):
+            if arrowpos >= points[i][0]:    #same arrow will work
+                continue
+            else:
+                ans += 1
+                arrowpos = points[i][1]
+        return ans
