90a5ef0aec8ee3a307a1ebf0984e9a54cb3872ee
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-15 19:53:55 +0530

    Time: 728 ms (44.53%), Space: 29.1 MB (57.32%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.py
+++ 1345-jump-game-iv/1345-jump-game-iv.py
@@ -50,7 +50,7 @@ class Solution:
                 # As that is a possibility (You visit all 100s, then when you are visiting
                 # the next 100 in your graph, you have to iterate through all 100s again, 
 				# that is inefficient so we empty the 100s connections)
-                del m[arr[i]] 
+                del m[arr[i]]       #or m[arr[i]] = []
             res += 1		
             
         # if we don't get an answer, return -1. Not needed as we are guaranteed an answer but some error checking is a best practice so why not
