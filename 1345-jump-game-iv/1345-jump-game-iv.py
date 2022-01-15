6af5448255dca4c148c1593644b3ebf915738b2e
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-15 19:53:43 +0530

    Time: 767 ms (36.83%), Space: 28.9 MB (61.17%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.py
+++ 1345-jump-game-iv/1345-jump-game-iv.py
@@ -8,7 +8,7 @@ class Solution:
         for i in range(n):
             m[arr[i]].append(i)
         
-        # dont want to revisit the same index
+        #dont want to revisit the same index
         visited = set()
         q = deque()
         # add the 0th node in our queue as that is our starting point
@@ -40,17 +40,17 @@ class Solution:
                     
 
                 # let's add every possible value that has the same value as the current one to our list to visit
-                for neighbor in m[arr[i]]:
-                    if neighbor in visited:
+                for k in m[arr[i]]:
+                    if k in visited:
                         continue
-                    q.append(neighbor)
+                    q.append(k)
                 # No longer need to visit this node's partner values ever again, 
                 # so let's remove all of it's partner values. 
                 # This also stops the partner nodes from visiting each other again
                 # As that is a possibility (You visit all 100s, then when you are visiting
                 # the next 100 in your graph, you have to iterate through all 100s again, 
 				# that is inefficient so we empty the 100s connections)
-                m[arr[i]] = []
+                del m[arr[i]] 
             res += 1		
             
         # if we don't get an answer, return -1. Not needed as we are guaranteed an answer but some error checking is a best practice so why not
