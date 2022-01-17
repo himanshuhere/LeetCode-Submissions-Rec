720025705288ec9a2e07dd466a432ffce7533a44
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-17 20:40:17 +0530

    Time: 405 ms (41.62%), Space: 41.4 MB (10.66%) - LeetHub

--- 494-target-sum/494-target-sum.py
+++ 494-target-sum/494-target-sum.py
@@ -10,4 +10,26 @@ class Solution:
         
         return dp(0, target)
         
+        #this i did, considering 01 knapsack that we have two choice for every ele that +/-, so whatevr not going to be target=0 will return 0 else 1.
+        #This works, but aditya's logic is that we have assign half elements as +vs other half -ve
+        #so tecnically it is s1-s2 as [1,2,3,2,3] so [+1,+2,+3] and [-2,-3] sp == [1,2,3] - [2,3]
+        #so it is reduced to finding the number of subsets with diff as target
+        # s1 - s2 = target, this and since s1+s2 = sum(arr) and cancelling eq will give us s1 = target(diff)+sum(arr)//2 , so just find out "count subsets with sum as target"
+    #here target = s1 = target(diff)+sum(arr)//2
         
+        s1 = (target+sum(nums))//2
+        if target > sum(nums) or target+sum(nums)%2==1:
+            return 0
+        
+        t = [[0 for _ in range(s1+1)] for _ in range(len(nums)+1) ]
+        
+        for i in range(len(nums)+1):
+            for j in range(s1+1):
+                if i==0 and j==0:   t[i][j] = 1
+                elif i==0:  t[i][j] = 0
+                elif j==0:  t[i][j] = 1
+                elif nums[i-1] <= j:
+                    t[i][j] = t[i-1][j-nums[i-1]] + t[i-1][j]
+                else:
+                    t[i][j] = t[i-1][j]
+        return t[-1][-1]
