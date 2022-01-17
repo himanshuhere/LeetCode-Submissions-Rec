489bcb223fcaa2ed808be348883bba74d35eda13
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-17 18:37:53 +0530

    Time: 169 ms (7.52%), Space: 14.7 MB (32.36%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.py
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.py
@@ -28,15 +28,10 @@ class Solution:
         
         for i in range(n+1):
             for j in range(s+1):
-                
                 if i==0 and j==0:   dp[i][j]=True
                 elif j==0:          dp[i][j]=True
                 elif i==0:          dp[i][j]=False
-                    
-        for i in range(1,n+1):
-            for j in range(1,s+1):
-                    
-                if arr[i-1] <= j:
+                elif arr[i-1] <= j:
                     dp[i][j] = dp[i-1][j] or dp[i-1][j-arr[i-1]]
                 else:
                     dp[i][j] = dp[i-1][j]
