4d9adc6d884686777fc69274012bcfa578785ad5
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-18 12:47:56 +0530

    Time: 283 ms (48.48%), Space: 41.1 MB (17.16%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -1,19 +1,19 @@
 class Solution:
     def change(self, amt: int, coins: List[int]) -> int:
         #Unbounded knapsack
-#         @lru_cache(None)
-#         def f(i, amt):
-#             if amt == 0:
-#                 return 1
-#             if i == -1:
-#                 return 0
-#             #if amt < 0:  return 0 and remove if else
-#             if coins[i] <= amt:
-#                 return f(i, amt-coins[i]) + f(i-1,amt)
-#             else:
-#                 return f(i-1, amt)
+        @lru_cache(None)
+        def f(i, amt):
+            if amt == 0:
+                return 1
+            if i == -1:
+                return 0
+            #if amt < 0:  return 0 and remove if else
+            if coins[i] <= amt:
+                return f(i, amt-coins[i]) + f(i-1,amt)
+            else:
+                return f(i-1, amt)
         
-#         return f(len(coins)-1, amt)
+        return f(len(coins)-1, amt)
         
         n = len(coins)
         t = [[0 for _ in range(amt+1)] for _ in range(n+1)]
