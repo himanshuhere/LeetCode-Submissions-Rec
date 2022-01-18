4755be340f298b9318c4cc090be2c77fa99441c9
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-18 11:18:40 +0530

    Time: 323 ms (5.08%), Space: 14.5 MB (84.06%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.py
+++ 605-can-place-flowers/605-can-place-flowers.py
@@ -6,11 +6,14 @@ class Solution:
         f = [0] + f + [0]       #but it will make copy or array to add 0s, lets handle edge cases in loop
         
         for i in range(1, len(f)-1):
-            prev = f[i-1] if i-1 >= 0 else 0
-            nxt = f[i+1] if i+1 <= len(f)-1 else 0
+            # prev = f[i-1] if i-1 >= 0 else 0
+            # nxt = f[i+1] if i+1 <= len(f)-1 else 0
             
-            if prev == f[i] == nxt == 0:
+            if f[i-1] == f[i] == f[i+1] == 0:
                 n -= 1
                 f[i] = 1
                 if n == 0:  return True
         return False
+    # addinf 0 to both end is a clever move to ignore handling edge cases but its a reasonable trade off with 
+        # making a copy of array while adding Os, either we cud also handle edge cases if dont want
+        # to make new copy. like if i+1>len(flowerbed)?0:a[i+1] i-1<0?0:a[i-1] 
