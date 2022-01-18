9334bb2f0b03e457d8d9e89a307c0c8b75a2e252
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-18 12:48:25 +0530

    Time: 213 ms (60.92%), Space: 41.1 MB (17.16%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -3,16 +3,11 @@ class Solution:
         #Unbounded knapsack
         @lru_cache(None)
         def f(i, amt):
-            if amt == 0:
-                return 1
-            if i == -1:
-                return 0
+            if amt == 0:    return 1
+            if i == -1:     return 0
             #if amt < 0:  return 0 and remove if else
-            if coins[i] <= amt:
-                return f(i, amt-coins[i]) + f(i-1,amt)
-            else:
-                return f(i-1, amt)
-        
+            if coins[i] <= amt: return f(i, amt-coins[i]) + f(i-1,amt)
+            else:               return f(i-1, amt)
         return f(len(coins)-1, amt)
         
         n = len(coins)
