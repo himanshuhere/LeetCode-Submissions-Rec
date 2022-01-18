d1b32531fbfaaedd07a3da25b14d6f41f90aebbd
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-18 12:42:45 +0530

    Time: 124 ms (99.48%), Space: 41.1 MB (17.19%) - LeetHub

---
+++ 518-coin-change-2/518-coin-change-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def change(self, amt: int, coins: List[int]) -> int:
+        @lru_cache(None)
+        def f(i, amt):
+            if amt == 0:
+                return 1
+            if i == -1:
+                return 0
+            if coins[i] <= amt:
+                return f(i, amt-coins[i]) + f(i-1,amt)
+            else:
+                return f(i-1, amt)
+        
+        return f(len(coins)-1, amt)
