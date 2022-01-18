69b714d98b2869cca857a19f8ec948828aa1c8ff
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-18 13:21:16 +0530

    Time: 2264 ms (21.24%), Space: 556.6 MB (5.06%) - LeetHub

---
+++ 322-coin-change/322-coin-change.py
@@ -0,0 +1,32 @@
+class Solution:
+    def coinChange(self, coins: List[int], amt: int) -> int:
+        #Unbounded knapsack
+        @lru_cache(None)
+        def f(i, amt):
+            if amt == 0:
+                return 0
+            if i == -1:
+                return math.inf
+            
+            if coins[i] <= amt: 
+                return min(1 + f(i, amt-coins[i]), f(i-1,amt))
+            else:               
+                return f(i-1, amt)
+            
+        ans = f(len(coins)-1, amt)
+        return ans if ans < math.inf else -1
+        
+        n = len(coins)
+        t = [[0 for _ in range(amt+1)] for _ in range(n+1)]
+        for i in range(n+1):
+            for j in range(amt+1):
+                if i == 0:  t[i][j] = 0
+                elif j == 0:    t[i][j] = 1
+                elif coins[i-1] <= j:   
+                    t[i][j] = t[i][j-coins[i-1]] + t[i-1][j]
+                else:
+                    t[i][j] = t[i-1][j]
+        return t[-1][-1]
+             
+    
+        
