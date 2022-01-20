8e930e9caff855aba213febb02d8548b42ae397e
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-20 18:28:17 +0530

    Time: 7564 ms (52.51%), Space: 31 MB (12.40%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.py
+++ 312-burst-balloons/312-burst-balloons.py
@@ -8,13 +8,13 @@ class Solution:
                 return 0
             
             coins = 0
-            for k in range(i, j):
+            for k in range(i, j):       #i to j-1
                 tmp = mcm(i, k) + mcm(k+1, j) + (nums[i-1]*nums[k]*nums[j])
                 coins = max(coins, tmp)
             
             return coins
         
         nums = [1] + nums + [1]
-        return mcm(1, len(nums)-1)
+        return mcm(1, len(nums)-1)      #for loop range is till j-1 so j wud be left for multipication
     
     #1. 3 1 5 8 .1
