6bbe2bd66acb940a02a7172e8e82b70dfb65030e
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-22 15:41:29 +0530

    Time: 3951 ms (18.56%), Space: 161 MB (11.38%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.py
@@ -0,0 +1,22 @@
+class Solution:
+    def winnerSquareGame(self, n: int) -> bool:
+        #ques is very good if you think and visualize it properly. see first of all after analyzing i got to know that we dont need to maintain alice bob state yes no need only n param is enought i will tell how, because two base case for n, n==1(Alice wins) and n==0(Alice loses) is already enough to make more bigger results. now see second misinterpretaion i had that alice/bob will pull max square if n is >= 4 else pull 1. So technicslly for every n, user will try from 1 ... j where j will if for j result is already available else at pos where sq of n is not poss, seee code you eill get this
+        #ex, n=5 --> next calls = n= 1, 4
+        #n=15, ---> n = 1, 4, 9
+        #yes
+        
+        @lru_cache(None)
+        def f(n):
+            if n == 0:
+                return False
+            if n == 1:
+                return True
+            
+            j = 1
+            while j*j <= n:
+                if f(n - j*j) == False:   #Bob won there so now with this one step we can make alice won
+                    return True
+                j += 1
+            return False
+        
+        return f(n)
