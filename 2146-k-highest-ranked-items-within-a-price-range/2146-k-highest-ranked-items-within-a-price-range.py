ec88ad88a2419ada209e0af84aa7507b3488c2b2
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-22 23:43:05 +0530

    Time: 4384 ms (7.14%), Space: 71.2 MB (7.14%) - LeetHub

--- 2146-k-highest-ranked-items-within-a-price-range/2146-k-highest-ranked-items-within-a-price-range.py
+++ 2146-k-highest-ranked-items-within-a-price-range/2146-k-highest-ranked-items-within-a-price-range.py
@@ -2,7 +2,7 @@ class Solution:
     def highestRankedKItems(self, grid: List[List[int]], pricing: List[int], start: List[int], k: int) -> List[List[int]]:
         #mene dfs laga k 2 ghante de diye but nahi chala pta ku sab ho gaya multikey sorting etc but issue was that my d was not getting min value for any pos, jo b recursion call pehle pahuch gayi kisi i,j me uska d save hogya waha. wo dikkat BFS me nhi ho rhi same code but BFS lets see
         #IMP TO READ
-        #Why BFS Worked, you shoul know. Because here dist is imp on that basis only sorting is happennig while in DFSwe were sending sidt+1 to every call but DFS was going depth deep and was updating every pos then we also try to visit pos erytime but thats will be infinite recursion. wehenervr dist or how much steps you ve covered is imp always go for BFS, bcs in BFS at every instance all four pos will be in queue and will be moving uniformly in all direction which will  make more sence and justice to distance thats why no dfs. anytime you see uniform movememtn is needed go for BFS. analyze before pls
+        #Why BFS Worked, you shoul know. Because here dist is imp on that basis only sorting is happennig while in DFSwe were sending dist+1 to every call but DFS was going depth deep and was updating every pos it is getting first then we also try to visit pos erytime but thats will be infinite recursion. wehenervr dist or how much steps you ve covered is imp always go for BFS, bcs in BFS at every instance all four pos will be in queue and will be moving uniformly in all direction which will  make more sence and justice to distance thats why no dfs. anytime you see uniform movememtn is needed go for BFS. analyze before pls
         
         R = len(grid)
         C = len(grid[0]) if R else 0
