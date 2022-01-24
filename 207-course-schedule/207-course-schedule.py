30a2290b393052d6aa482d263c427463be81bde5
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-24 22:58:50 +0530

    Time: 101 ms (57.08%), Space: 16.9 MB (43.30%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.py
@@ -0,0 +1,40 @@
+#its just finding cycle in directed graph
+
+import sys
+sys.setrecursionlimit(10**6)    #imp 
+g=[]
+vis=[]
+selfvis=[]
+
+
+class Solution:
+    def canFinish(self, A: int, B: List[List[int]]) -> bool:
+        global vis,selfvis,g
+        
+        def dfs(v): 
+            vis[v] = True
+            selfvis[v] = True
+
+            for k in g[v]: 
+                if not vis[k]: 
+                    if dfs(k): 
+                        return True
+                elif selfvis[k] == True: 
+                    return True     #cycle
+
+            selfvis[v] = False
+            return False
+        
+        
+        vis = [0]*(A+1)
+        selfvis = [0]*(A+1)
+        g=[[] for i in range(A+1)]
+
+        for edge in B:      #directed graph
+            g[edge[0]].append(edge[1])
+
+        for i in range(1,A+1):
+            if vis[i]==0: 
+                if dfs(i):
+                    return False
+        return True
