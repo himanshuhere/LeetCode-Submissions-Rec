0908f13bd92d62be6e2bddce770092637e754208
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-25 21:00:25 +0530

    Time: 4801 ms (30.80%), Space: 71.6 MB (75.43%) - LeetHub

---
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -0,0 +1,34 @@
+class Solution:
+    def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
+        #idea
+        #first remove the sub island which is there in 2 but not in 1
+        #second - after first we ll have valid 1s in grid2, now count them
+        #we ll use same dfs function, where instead visisted we ll use changing i,j pos value and that will help in removing invalid sub island too see
+        
+        m=len(grid1)
+        n=len(grid1[0])
+
+        def dfs(i,j):
+            if i<0 or i>=m or j<0 or j>=n or grid2[i][j]==0:
+                return
+
+            grid2[i][j]=0
+            dfs(i+1,j)
+            dfs(i,j+1)
+            dfs(i,j-1)
+            dfs(i-1,j)
+
+        # removing all the non-common sub-islands
+        for i in range(m):
+            for j in range(n):
+                if grid2[i][j]==1 and grid1[i][j]==0:
+                    dfs(i,j)
+
+        c=0
+        # counting sub-islands
+        for i in range(m):
+            for j in range(n):
+                if grid2[i][j]==1:
+                    dfs(i,j)
+                    c+=1
+        return c
