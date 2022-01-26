f5545df0c7130388f81cbecb836bca1eb9b4c86a
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-26 11:36:50 +0530

    Time: 544 ms (29.56%), Space: 22.5 MB (40.88%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
@@ -19,3 +19,5 @@ class Solution:
         dfs(root1)
         dfs(root2)
         return sorted(res)
+    
+    #optimal wud be to use inorder and get two sorted list, then merge then in o(n) rather sorting
