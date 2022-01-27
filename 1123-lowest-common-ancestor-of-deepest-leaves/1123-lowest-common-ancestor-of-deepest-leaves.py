c06a927e13195b8134d44949bf8591975134099f
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-27 18:00:01 +0530

    Time: 68 ms (33.61%), Space: 14.4 MB (99.83%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.py
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.py
@@ -1,14 +1,16 @@
 class Solution:
     def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         #to decide levels of nodes
-#         def dfs(r):
+#         def height(r):
 #             if not r:
 #                 return 0
 #             l, r = dfs(r.left), dfs(r.right)
 #             return max(l, r) + 1            #means leaf nodes is at level 1
 #this, function doing two things parallely - deciding on levels and sange deciding on LCA.
 # fo if both branch is of same height then root is lca else if left is bigger LCA is from the left side, might be this could change on more upper side. dont get cofuse thats y put level code first then merged code. it is base, hypothesis and induction step only or if you see it is modified LCA only. but u need to understand ques first, if any of the leaf is more deeper then that is LCA of itselft thats why l>r return left and if two leaf are at same level the their ultimate root is the LCA
+#input = [1234] , Since the set contain only one deepest leaf i.e [4], so its lca is node itself.
     
+    #you could make height function separate then call it every time from dfs yes o(n^2), but it will send your idea in simpler way to interview so dont jump directly on merged def. better first o(n^2) then o(n) on doing same thing in one function using tuples/pair
         def dfs(r):
             if not r:
                 return (r, 0)
