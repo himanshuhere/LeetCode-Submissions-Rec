29aaeef13bf8dd17ad0d74a4febe8ca334305517
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-27 17:29:10 +0530

    Time: 75 ms (25.74%), Space: 14.4 MB (98.51%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.py
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.py
@@ -1,9 +1,3 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         #to decide levels of nodes
@@ -12,7 +6,9 @@ class Solution:
 #                 return 0
 #             l, r = dfs(r.left), dfs(r.right)
 #             return max(l, r) + 1            #means leaf nodes is at level 1
-        
+#this, function doing two things parallely - deciding on levels and sange deciding on LCA.
+# fo if both branch is of same height then root is lca else if left is bigger LCA is from the left side, might be this could change on more upper side. dont get cofuse thats y put level code first then merged code. it is base, hypothesis and induction step only or if you see it is modified LCA only. but u need to understand ques first, if any of the leaf is more deeper then that is LCA of itselft thats why l>r return left and if two leaf are at same level the their ultimate root is the LCA
+    
         def dfs(r):
             if not r:
                 return (r, 0)
@@ -20,12 +16,14 @@ class Solution:
             left, l_lev = dfs(r.left)
             right, r_lev = dfs(r.right)
             
-            if l_lev == r_lev:
+            if l_lev == r_lev:      #then , i am the  LCA
                 return (r, l_lev + 1)
-            if l_lev > r_lev:
+            
+            if l_lev > r_lev:       #LCA should come from left
                 return (left, l_lev + 1)
-            else:
+            
+            else:                   #LCA should come from right
                 return (right, r_lev + 1)
         
-        lca, lev = dfs(root)
-        return lca
+        LCA, lev = dfs(root)
+        return LCA
