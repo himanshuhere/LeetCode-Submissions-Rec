15c857b73389891085bba94acf1235e7f30476ba
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-30 11:14:33 +0530

    Time: 304 ms (46.39%), Space: 25.4 MB (96.40%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k = k % n       #imp to focus, if k is more rotation than n
+        nums[:] = nums[n-k:] + nums[:n-k]
+        return nums
+    
+    
+#         for i in range(k):    #this will lead time limit
+#             self.rotatebyone(nums)
+#         return nums   
+#     def rotatebyone(self, nums: List[int]) -> None:
+#         last = nums[len(nums)-1]
+#         for i in range(len(nums) - 2, -1, -1):
+#             nums[i+1]=nums[i]
+#         nums[0]= last
