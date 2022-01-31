0a41336fbcc29be3b4584676b9070b63ef561625
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-31 22:02:09 +0530

    Time: 141 ms (6.30%), Space: 15.3 MB (21.08%) - LeetHub

---
+++ 120-triangle/120-triangle.py
@@ -0,0 +1,42 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        # 2
+        # 3 4
+        # 6 5 7
+        # 4 1 8 3
+        #technically there is always be j and j+1 so no base boundary case for j but for i only
+        @lru_cache(None)
+        def f(i, j):
+            if i == len(triangle)-1:
+                return triangle[i][j]
+            
+            return triangle[i][j] + min(f(i+1, j) , f(i+1, j+1))
+        #return f(0,0)
+        
+        #2d tabulation
+        dp = [[0]*(i+1) for i in range(len(triangle))]
+        n = len(triangle)
+        for j in range(n):
+            dp[n-1][j] = triangle[n-1][j]
+        
+        for i in range(n-2, -1, -1):
+            for j in range(len(dp[i])-1, -1, -1):
+                dp[i][j] = triangle[i][j] + min(dp[i+1][j], dp[i+1][j+1])
+        #return dp[0][0]
+    
+    
+        #1d since i=1 eberytime
+        dp = [0]*n
+        for j in range(n):
+            dp[j] = triangle[n-1][j]
+            
+        #print(dp)
+        for i in range(n-2, -1, -1):
+            cur = [0]*(i+1)
+            for j in range(len(cur)-1, -1, -1):
+                cur[j] = triangle[i][j] + min(dp[j], dp[j+1])
+            dp = cur
+        return dp[0]
+        
+        
+        
