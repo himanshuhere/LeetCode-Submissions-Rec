14b8eacc77c6e0c085b4cd9745d436e92d3b9e4b
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-31 18:16:45 +0530

    Time: 197 ms (31.50%), Space: 14.7 MB (87.06%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        #variable sliding window
+        m = Counter(t)
+        count = len(m)
+        ans = math.inf
+        
+        i, j = 0, 0
+        start, end = -1, -1
+        
+        while j < len(s):  
+            if s[j] in m:
+                m[s[j]] -= 1
+                if m[s[j]] == 0:    count -= 1
+            
+            while count == 0:
+                if ans > j-i+1:
+                    ans = j-i+1
+                    start, end = i, j
+                if s[i] in m:
+                    m[s[i]] += 1
+                    if m[s[i]] == 1:    count += 1
+                i += 1
+            j += 1
+        
+        return s[start:end+1]
+         
+
+     
