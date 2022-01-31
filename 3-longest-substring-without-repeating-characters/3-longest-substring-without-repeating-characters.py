60d136bfbf1108665b3011e3d2f7be81888e90f5
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-31 14:49:36 +0530

    Time: 165 ms (22.53%), Space: 14 MB (97.07%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,6 +1,6 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        #variable sliding window, k not given but see
+        #variable sliding window, k not given but see, instead of len(map) you can use count logic you know right
         i, j = 0, 0
         map_ = defaultdict(int)
         ans = 0
