f573edecd60f5823012c5bbbd3989af498354574
Himanshu Soni <himanshusoni3105@outlook.com>
2022-01-31 12:05:27 +0530

    Time: 293 ms (48.22%), Space: 16.7 MB (61.38%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        #sliding window can be used variable one but for only positives number, here negatives also there sliding window will not work. but map can be but HOW. think of s[i,j] = s[0,j]-s[0,i], now see will put every sum frim left to right in map if again found will increase count. Since we just need to return count of  window subarryas with sum k, not max windows not min, window can of any size just sum should be k. so whenevr we found that cur sum - k, already present then we found ans, but could be possible that sum-k can also be occured multiple times back then thus we have count in map, take entire count and add that to final res.
+        
+        map_ = defaultdict(int)
+        res = 0
+        sum_ = 0
+        map_[0] = 1
+        for num in nums:
+            sum_ += num
+            if sum_ - k in map_:
+                res += map_[sum_ - k]
+            
+            #fill map
+            map_[sum_] += 1
+        return res
