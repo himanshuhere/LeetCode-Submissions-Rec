2e76a7b0a7676f9d72c65019b58c86ce8e7f5d68
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-01 11:54:01 +0530

    Time: 1664 ms (23.45%), Space: 25 MB (80.69%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -6,3 +6,14 @@ class Solution:
             mini = min(mini, price)
             profit = max(profit, price - mini)
         return profit
+    
+    #you cant sell on day1 so buying is only possibiliy
+        buy = prices[0]
+        pro = 0
+        
+        for i in range(1, len(prices)):
+            if prices[i] < buy:
+                buy = prices[i]
+            else:
+                pro = max(pro, prices[i]-buy)
+        return pro
