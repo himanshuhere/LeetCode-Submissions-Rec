df3a9a9012a6c306bfd544156920b79b3fccf1a7
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-02 10:02:16 +0530

    Time: 696 ms (92.80%), Space: 27.5 MB (75.36%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -4,7 +4,7 @@ class Solution:
         l, r = 0, len(height) - 1
         area = 0
         
-        while l < r:
+        while l <= r:
             if height[l] < height[r]:
                 area = max(area, (r-l)*height[l])   #r-l = width, smaller height
                 l += 1                              #r is bigger, move l might get bigger area next (greed)
