e2a72a9b77134126379ee553bdd4d9044ed0718c
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-02 10:01:50 +0530

    Time: 884 ms (47.38%), Space: 27.4 MB (75.36%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        #Two edge pointer + Greedy movement of pointers
+        l, r = 0, len(height) - 1
+        area = 0
+        
+        while l < r:
+            if height[l] < height[r]:
+                area = max(area, (r-l)*height[l])   #r-l = width, smaller height
+                l += 1                              #r is bigger, move l might get bigger area next (greed)
+            else:
+                area = max(area, (r-l)*height[r])
+                r -= 1
+        return area
