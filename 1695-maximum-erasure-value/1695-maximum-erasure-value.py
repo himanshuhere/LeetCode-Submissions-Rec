35d43d02aa683fc35436b5178d2817bab0ced095
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-02 22:18:47 +0530

    Time: 1616 ms (52.04%), Space: 27.2 MB (97.43%) - LeetHub

---
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:
+        m = defaultdict(int)
+        ans = 0
+        i, j = 0, 0
+        count = 0
+        subsum = 0
+        while j < len(nums):
+            m[nums[j]] += 1
+            subsum += nums[j]
+            if m[nums[j]] == 1:
+                count += 1
+            if count == (j-i+1):
+                ans = max(ans, subsum)
+            while count < (j-i+1):
+                #ans = max(ans, subsum)
+                if nums[i] in m:
+                    m[nums[i]] -= 1
+                    if m[nums[i]] == 0:
+                        count -= 1
+                subsum -= nums[i]
+                i+=1
+            j += 1
+        return ans
+                
