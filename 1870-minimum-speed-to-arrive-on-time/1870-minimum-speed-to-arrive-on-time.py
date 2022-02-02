efdb0a73b8e7f20fc7210a9cb1502491602fbc49
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-02 13:00:58 +0530

    Time: 5340 ms (29.79%), Space: 28.6 MB (41.41%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
+        def blackBox(s):
+            h = 0
+            for i in range(len(dist)-1):
+                h += ceil(dist[i]/s)
+            h += dist[-1]/s     #we dont need tp wait afte this, so optimizing out hours
+            
+            return h <= hour
+        
+        lo, hi = 1, 100000000
+        while lo < hi:
+            mid = lo + (hi-lo)//2
+            if(blackBox(mid)):
+                hi = mid
+            else:
+                lo = mid + 1
+        return lo if lo != 100000000 else -1
+            
