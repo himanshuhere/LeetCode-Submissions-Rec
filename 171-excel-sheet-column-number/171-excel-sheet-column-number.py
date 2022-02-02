a2ddb82ec042163173887ae3ff3f71edf5f8acbc
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-02 17:05:12 +0530

    Time: 36 ms (68.36%), Space: 13.9 MB (90.86%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.py
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.py
@@ -4,3 +4,9 @@ class Solution:
         for ch in col:
             res = res*26 + (ord(ch) - ord('A') + 1)
         return res
+    
+    
+    
+    # It is like converting a decimal number to base26 except there is no zero in that we need to subtract 1 from n to make that adjustment to get the answer we get from convert a normal decimal number to any base.
+# Time: O(k) - k is the numer of digits in the input
+# Space: O(1)
