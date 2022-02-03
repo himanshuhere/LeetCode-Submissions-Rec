c775c4163a72d8cab3899e27f5070e5aed902b39
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-03 13:49:09 +0530

    Time: 1094 ms (36.95%), Space: 14.1 MB (99.75%) - LeetHub

---
+++ 454-4sum-ii/454-4sum-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        hashtable = {}
+        for a in nums1:
+            for b in nums2 :
+                hashtable[a+b] = hashtable.get(a+b, 0) + 1
+                
+        count = 0         
+        for c in nums3 :
+            for d in nums4 :
+                if -(c+d) in hashtable :
+                    count += hashtable[-c-d]
+        return count
