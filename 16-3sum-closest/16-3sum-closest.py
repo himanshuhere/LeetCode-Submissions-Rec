6db6bb300a106e82f8f381881130158ba13e1813
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-03 17:05:07 +0530

    Time: 476 ms (12.70%), Space: 13.9 MB (98.65%) - LeetHub

---
+++ 16-3sum-closest/16-3sum-closest.py
@@ -0,0 +1,21 @@
+class Solution:
+    def threeSumClosest(self, A: List[int], target: int) -> int:
+        #3 sum is n^2, will do same
+        #as per constraint size will be min 3 so consider all them as res
+        res = A[0] + A[1] + A[2]
+        
+        A= sorted(A)
+        for i in range(len(A)-2):
+            l, r = i+1, len(A)-1
+            
+            while l < r:
+                cursum = A[i]+A[l]+A[r]
+                if cursum > target:
+                    r-=1
+                else:
+                    l+=1
+                
+                if abs(res-target) > abs(cursum-target):
+                    res = cursum
+        return res
+        
