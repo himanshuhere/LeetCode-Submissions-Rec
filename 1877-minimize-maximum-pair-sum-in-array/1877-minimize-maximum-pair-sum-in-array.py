0c52db6bb12ad5a3df11d63eeb7b34a2b47f29cb
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-03 20:02:31 +0530

    Time: 2473 ms (5.02%), Space: 27.8 MB (89.48%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.py
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.py
@@ -5,6 +5,6 @@ class Solution:
         l, r = 0, len(nums)-1
         while l<r:
             ans = max(ans, nums[l]+nums[r])
-            l+=1
+            l+=1                #kayki bol rha max n/2 pairs and one ele only in one pair
             r-=1
         return ans
