a1ed82567e709b4cca327f8856223d6a339dad97
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-05 11:20:21 +0530

    Time: 72 ms (88.83%), Space: 16.7 MB (52.00%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
@@ -5,6 +5,7 @@ class Solution:
         sm=0
         ans=0
         maxlen=math.inf
+        
         while j < len(nums):
             sm+=nums[j]
             
@@ -14,6 +15,11 @@ class Solution:
                 i+=1
             j+=1
         return maxlen if maxlen!=math.inf else 0
+    
+    #negative ele me kaam nhi hoga babu fir kese karoge bolo bool bol radha bol solution hpga k nahi, hoga
+    #ek kam karo min ele nikalo, sabse negative wala hoga, ab sare elements me usko add kardo. target me b. now apply sliding window
+    
+    
     
     #can also b done usinf binary search on ans range = [1, len]
     #feasible will be finding max sum for k widnow-fixed sliding window
