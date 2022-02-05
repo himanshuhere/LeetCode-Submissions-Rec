c24b91e3b50c3e503f9e9ed21c0d23e8d62b30c7
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-05 10:46:56 +0530

    Time: 180 ms (7.52%), Space: 16.7 MB (15.39%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
@@ -1,22 +1,19 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         #variable sliding window
-        sm= 0
-        i,j =0,0
-        ans =math.inf
-        while j< len(nums):
+        i, j = 0, 0
+        sm=0
+        ans=0
+        maxlen=math.inf
+        while j < len(nums):
             sm+=nums[j]
-            if sm < target:
-                j+=1
-            # elif sm>=target:
-            else:
-                while sm>=target:
-                    ans = min(ans, j-i+1)
-                    sm-=nums[i]
-                    i+=1
-                j+=1
-            #j+=1
-        return ans if ans!=math.inf else 0
+            
+            while sm >= target:
+                maxlen=min(maxlen, j-i+1)
+                sm-=nums[i]
+                i+=1
+            j+=1
+        return maxlen if maxlen!=math.inf else 0
     
     #can also b done usinf binary search on ans range = [1, len]
     #feasible will be finding max sum for k widnow-fixed sliding window
