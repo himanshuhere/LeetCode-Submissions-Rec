7dbecde5b62f8c6171969d3e55782342690f20a0
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-06 18:56:26 +0530

    Time: 220 ms (65.75%), Space: 16.2 MB (74.53%) - LeetHub

---
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        #even at 0. odd at 1. move two both aage
+        i, j = 0, 1
+        while i < len(nums) and j < len(nums):
+            if nums[i] & 1 == 0:    #even
+                i += 2
+            elif nums[j] & 1 == 1:  #odd
+                j += 2
+            else:
+                nums[i], nums[j] = nums[j], nums[i]
+                i+=2
+                j-=2
+        return nums
