bfae12e5b7429f1a20b25cb44d59a94201648524
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-06 19:07:54 +0530

    Time: 216 ms (71.17%), Space: 16.2 MB (74.53%) - LeetHub

--- 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.py
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.py
@@ -2,24 +2,23 @@ class Solution:
     def sortArrayByParityII(self, nums: List[int]) -> List[int]:
         #two pointer approach
         #even at 0. odd at 1. move two both aage
-        i, j = 0, 1
-        while i < len(nums) and j < len(nums):
-            if nums[i] & 1 == 0:    #even
-                i += 2
-            elif nums[j] & 1 == 1:  #odd
-                j += 2
-            else:
-                nums[i], nums[j] = nums[j], nums[i]
-                i+=2
-                j+=2
-        return nums
-    
-    #cud ve done i=0, j=len-1, also but uske liye len cal karke odd even hisab laga k j ko aage piche place karna hota then j-=2 har bar baki pichli wli parity 1 jese hi karna 
-        n = len(nums)-1
-        i = 0
-        j = n-1 if (n-1)&1==1 else n-2      #we need it odd
+        # i, j = 0, 1
+        # while i < len(nums) and j < len(nums):
+        #     if nums[i] & 1 == 0:    #even
+        #         i += 2
+        #     elif nums[j] & 1 == 1:  #odd
+        #         j += 2
+        #     else:
+        #         nums[i], nums[j] = nums[j], nums[i]
+        #         i+=2
+        #         j+=2
+        # return nums
         
-        while i < j:
+        
+        #since arra y has equal odds and even, thus total lenght will always be even and last index be odd. so take j = n-1
+        #do as parity 1, but now we are doing +2, -2 so take care of while condition it is not i<j, that wll pass fast
+        i, j = 0, len(nums)-1
+        while i < len(nums) and j >=0:
             if nums[i] & 1 == 0:    #even
                 i += 2
             elif nums[j] & 1 == 1:  #odd
@@ -29,3 +28,5 @@ class Solution:
                 i+=2
                 j-=2
         return nums
+    
+    
