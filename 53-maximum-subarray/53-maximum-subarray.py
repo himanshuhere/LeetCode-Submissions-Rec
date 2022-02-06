ef05e1cc56a829a0473749d81e5b7fc1fe3853d4
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-06 18:41:02 +0530

    Time: 997 ms (42.67%), Space: 27.9 MB (87.25%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        #brute two loops and generate subarray and update res
+        # Kadane's algorithm, O(n)
+        res = cur = nums[0]
+        for i in range(1, len(nums)):
+            cur = max(cur + nums[i], nums[i])
+            res = max(res, cur)
+        return res
