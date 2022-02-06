08cdd6edce0de5075c99f46fd92fec685029ec43
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-06 19:02:45 +0530

    Time: 200 ms (96.39%), Space: 16.3 MB (74.53%) - LeetHub

--- 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.py
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.py
@@ -11,7 +11,21 @@ class Solution:
             else:
                 nums[i], nums[j] = nums[j], nums[i]
                 i+=2
-                j-=2
+                j+=2
         return nums
     
     #cud ve done i=0, j=len-1, also but uske liye len cal karke odd even hisab laga k j ko aage piche place karna hota then j-=2 har bar baki pichli wli parity 1 jese hi karna 
+        n = len(nums)-1
+        i = 0
+        j = n-1 if (n-1)&1==1 else n-2      #we need it odd
+        
+        while i < j:
+            if nums[i] & 1 == 0:    #even
+                i += 2
+            elif nums[j] & 1 == 1:  #odd
+                j -= 2
+            else:
+                nums[i], nums[j] = nums[j], nums[i]
+                i+=2
+                j-=2
+        return nums
