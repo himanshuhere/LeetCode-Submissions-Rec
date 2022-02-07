6ec24a561accd8887a1d57dffcb6a915756b9327
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-07 10:54:46 +0530

    Time: 205 ms (34.88%), Space: 19.1 MB (9.69%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,46 +1,46 @@
 class Solution:
-    def merge(self, A: List[List[int]]) -> List[List[int]]:
-#         intervals = sorted(intervals)   #first key toh sorted, for second key we keep max element see
-#         ans = []
-#         i = 0
-#         j = -1
-#         while i < len(intervals)-1:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals = sorted(intervals)   #first key toh sorted, for second key we keep max element see
+        ans = []
+        i = 0
+        j = -1
+        while i < len(intervals)-1:
             
-#             if intervals[i][1] >= intervals[i+1][0]:
-#                 j = i
-#                 seckey = intervals[i][1]        #ye nhi kroge to error bahot ayege casec me
-#                 while j < len(intervals)-1 and seckey >= intervals[j+1][0]:
-#                     j += 1
-#                     seckey = max(seckey, intervals[j][1])
+            if intervals[i][1] >= intervals[i+1][0]:
+                j = i
+                seckey = intervals[i][1]        #ye nhi kroge to error bahot ayege casec me
+                while j < len(intervals)-1 and seckey >= intervals[j+1][0]:
+                    j += 1
+                    seckey = max(seckey, intervals[j][1])
                     
-#                 ans.append([intervals[i][0], seckey])
-#                 i = j + 1
+                ans.append([intervals[i][0], seckey])
+                i = j + 1
                 
-#             else:
-#                 ans.append([intervals[i][0], intervals[i][1]])
-#                 i += 1
+            else:
+                ans.append([intervals[i][0], intervals[i][1]])
+                i += 1
         
-#         if j < len(intervals)-1:
-#             ans.append(intervals[-1])
+        if j < len(intervals)-1:
+            ans.append(intervals[-1])
             
-#         return ans
+        return ans
     
         #2 shorter version of same logic
-#         intervals = sorted(intervals)   
-#         ans = []
-#         i = 0
-#         while i < len(intervals):
-#             j = i
-#             seckey = intervals[j][1]        
-#             while j < len(intervals) - 1 and seckey >= intervals[j+1][0]:
-#                 j += 1
-#                 seckey = max(seckey, intervals[j][1])
+        intervals = sorted(intervals)   
+        ans = []
+        i = 0
+        while i < len(intervals):
+            j = i
+            seckey = intervals[j][1]        
+            while j < len(intervals) - 1 and seckey >= intervals[j+1][0]:
+                j += 1
+                seckey = max(seckey, intervals[j][1])
                     
-#             ans.append([intervals[i][0], seckey])
-#             i = j
-#             i += 1
+            ans.append([intervals[i][0], seckey])
+            i = j
+            i += 1
             
-#         return ans
+        return ans
     
     #3 smooth version code, got from discussion
         A = sorted(A)
