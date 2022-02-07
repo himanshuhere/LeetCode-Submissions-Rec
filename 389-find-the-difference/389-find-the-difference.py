88edc6ce83ee62b5653d5be3a1c122cd43f2b178
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-07 10:23:17 +0530

    Time: 32 ms (89.14%), Space: 14 MB (88.02%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,10 +1,16 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        ss = t
-        s, t = Counter(s), Counter(t)
-        for i in ss:
-            if s[i] != t[i]:
-                return i
+        # ss = t
+        # s, t = Counter(s), Counter(t)
+        # for i in ss:
+        #     if s[i] != t[i]:
+        #         return i
+        
+        #XOR
+        xor = 0
+        for c in s+t:
+            xor ^= ord(c)
+        return chr(xor)
         
         
         
