e80b03f27c63ae90500c27a9237a386883a75168
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-08 10:09:22 +0530

    Time: 148 ms (16.83%), Space: 17.3 MB (98.38%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -10,15 +10,16 @@ class Solution:
             i += 1
             
         #keep merging till interval start smaller than new last
-        
+        if i < len(A):
+            minn = min(new[0], A[i][0])         #as sorted, these two are only participants here
+            new[0] = minn
         while i < len(A) and A[i][0] <= new[1]:
-            new[0] = min(new[0], A[i][0])
             new[1] = max(new[1], A[i][1])
             i+=1
+        
         ans.append(new)
         
         #add the rest part
-        
         while i < len(A):
             ans.append(A[i])
             i+=1
