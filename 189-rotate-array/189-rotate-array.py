eb0d928cb96520a91679b3f365a44920a75b279e
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-10 20:03:30 +0530

    Time: 467 ms (22.64%), Space: 25.4 MB (62.82%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -1,15 +1,27 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         n = len(nums)
-        k = k % n       #imp to focus, if k is more rotation than n
-        nums[:] = nums[n-k:] + nums[:n-k]
-        return nums
+        k = k%n
+        
+        def reversed(i, j):
+            while i < j:
+                nums[i], nums[j] = nums[j], nums[i]
+                i+=1
+                j-=1
+                
+        
+        reversed(0, n-1)        
+        reversed(0, k-1)
+        reversed(k, n-1)
+        
+        
+        
+        # n = len(nums)
+        # k = k % n       #imp to focus, if k is more rotation than n
+        # nums[:] = nums[n-k:] + nums[:n-k]
+        # return nums
     
-    #brute with o(n) extra
-        # res = [None]*n
-        # for i in range(n):
-        #     res[i] = nums[(i+k)%n]
-        # return res
+       
         
         #best
 #         for i in range(k):    #this will lead time limit
