3edf611387379c3b0dae45b22faa1cd3e4acec9d
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-10 21:29:59 +0530

    Time: 48 ms (96.49%), Space: 14.5 MB (82.09%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
@@ -6,21 +6,18 @@ class Solution:
             mid = l + (r-l)//2
             if nums[mid] == target:
                 return True
-            
-            # while l < mid and nums[l] == nums[mid]: # tricky part
-            #     l += 1
                 
             # the first half is ordered
             if nums[l] < nums[mid]:
                 # target is in the first half
-                if nums[l] <= target < nums[mid]:
+                if nums[l] <= target <= nums[mid]:
                     r = mid - 1
                 else:
                     l = mid + 1
             # the second half is ordered
             elif nums[l] > nums[mid]:
                 # target is in the second half
-                if nums[mid] < target <= nums[r]:
+                if nums[mid] <= target <= nums[r]:
                     l = mid + 1
                 else:
                     r = mid - 1
