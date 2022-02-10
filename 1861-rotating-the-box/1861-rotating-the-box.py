a60f76c5c5b64b4c2d9b27316c72f020234937e0
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-10 17:42:44 +0530

    Time: 2661 ms (73.54%), Space: 23.6 MB (99.30%) - LeetHub

---
+++ 1861-rotating-the-box/1861-rotating-the-box.py
@@ -0,0 +1,29 @@
+class Solution:
+    def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
+        # For each row:
+        #     empty will point to the last cell that is empty. Initially empty = columns - 1.
+        #     Start from last column in the row, for each column c:
+        #     if current cell contains a stone, we will move it from current cell to the empty cell(which is represented by empty variable).
+        #     if current cell contains an obstacle, we will change value of empty to c-1.
+        
+        R, C = len(box), len(box[0])
+        
+        for i in range(R):
+            empty = C-1
+            for j in range(C-1, -1, -1):
+                if box[i][j] == "*":
+                    empty = j-1
+                    
+                elif box[i][j] == "#":
+                    box[i][j], box[i][empty] = box[i][empty], box[i][j]
+                    empty -= 1
+                    
+        return zip(*box[::-1])
+#         ans = []
+#         for i in range(R):
+#             row = []
+#             for j in range(C-1, -1, -1):
+#                 row.append(box[i][j])
+#             ans.append(row)
+#         return ans
+                
