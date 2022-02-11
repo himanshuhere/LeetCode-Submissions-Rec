8d3edd8046cc6c46374b3d9ddef0f003cefcb79c
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-11 18:37:53 +0530

    Time: 56 ms (92.42%), Space: 14.1 MB (84.23%) - LeetHub

---
+++ 443-string-compression/443-string-compression.py
@@ -0,0 +1,24 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        #two pointer
+        i, j = 0, 0
+        while j < len(chars):
+            curchar = chars[j]
+            cnt = 0
+            while j < len(chars) and chars[j] == curchar:
+                cnt += 1
+                j += 1
+            
+            chars[i] = curchar
+            i += 1
+            
+            #if count is 1 (count allwyas be >= 1), dont add it else add it unit wise
+            if cnt > 1: 
+                for c in str(cnt):
+                    chars[i] = c
+                    i += 1
+        
+            
+        return i
+                
+            
