b27fe2306dea1b25110f39167465632d10189ec3
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-12 15:16:14 +0530

    Time: 428 ms (91.60%), Space: 34.3 MB (71.97%) - LeetHub

--- 815-bus-routes/815-bus-routes.py
+++ 815-bus-routes/815-bus-routes.py
@@ -25,4 +25,16 @@ class Solution:
     #res is actually no of busses travelled
     #pehle vis stops pe lagaya to 44/45 passed, then bus pe lagaya kuki ek bus me travel to dont catch it unnnecerry visist. then passed
     #actually bus ya stop kisi ka b visisted valid hai but agar dono k liye do vis bana do aur dono ko maintain karo to TC optimize ho jayegi, kher yaha bus wala vis accept hogya
+    
+
+# Since this problem doesn't have weight factor, so just BFS I is fine.
+
+# Key Point : What is vertex and what is edge ?
+# In this problem, since we need to find the min buses need to take.
+# So it is a better design if we set bus as the vertex and set stops as edges.
+# The graph might look a little bit weird, but then I can easily use BFS to generate bus.
+
+# Then, the problem is transforming to:
+# Find the min jumps from any Vertex(bus) which has an Edge of source edge(source stop) to any Vertex(bus) which has an Edge of target edge(target stop).
+
             
