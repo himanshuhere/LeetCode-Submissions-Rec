ba3c8a1422605a65d97369ef5b66cfd1c9443fba
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-12 22:14:49 +0530

    Time: 241 ms (45.25%), Space: 15.4 MB (45.89%) - LeetHub

--- 845-longest-mountain-in-array/845-longest-mountain-in-array.py
+++ 845-longest-mountain-in-array/845-longest-mountain-in-array.py
@@ -6,20 +6,20 @@ class Solution:
         i = 0
         while i < n:
             base = i
-            while i < n-1 and arr[i] < arr[i+1]:
+            while i < n-1 and arr[i] < arr[i+1]:    #keep climbing
                 i += 1
-            if i == base:
+            if i == base:                           #dint climb, still at base only come again at i+1
                 i += 1
                 continue
             
             peak = i
-            while i < n-1 and arr[i] > arr[i+1]:
+            while i < n-1 and arr[i] > arr[i+1]:    #keep sliding down
                 i += 1
-            if i == peak:
+            if i == peak:                           #couldnt come, still at peak?
                 i += 1 
                 continue
             
-            mountain = i - base + 1
+            mountain = i - base + 1                 #take the current trek distance
             ans = max(ans, mountain)
         return ans
                 
