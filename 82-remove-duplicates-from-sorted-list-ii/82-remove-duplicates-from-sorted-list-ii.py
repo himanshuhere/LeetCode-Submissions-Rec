f8cd1b98a3685890ea7a2ec803c0e0496435e86a
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-12 17:52:44 +0530

    Time: 74 ms (17.65%), Space: 13.8 MB (99.54%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.py
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.py
@@ -5,6 +5,7 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        #two pointer approach
         dummy = ListNode(None)
         dummy.next = head
         pre = dummy
