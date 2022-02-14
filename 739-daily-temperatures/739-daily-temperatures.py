2cec52bb5931df681145a60d98d05d6b3f423a68
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-15 00:13:17 +0530

    Time: 1748 ms (36.73%), Space: 26 MB (13.25%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -16,17 +16,4 @@ class Solution:
         return right
     
     
-        res = [0] * len(T)
-        stack = []
-        
-        for index in range(len(T)-1, -1, -1):
-            
-            while stack and T[stack[-1]] <= T[index]:
-                stack.pop()
-                        
-            res[index] = stack[-1] - index if stack else 0
-            
-            stack.append(index)
-
-            
-        return res
+    
