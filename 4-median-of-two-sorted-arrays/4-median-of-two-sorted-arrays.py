a80a30e8ab66144373c4588a0b2715827ac7db5d
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-14 10:19:38 +0530

    Time: 100 ms (70.67%), Space: 14.3 MB (67.77%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -2,6 +2,27 @@ class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         #Brute o(m+n), how?- merge/add the both arrays  then find mediam. how? if even length, then avg of two middle elements else if odd then middle
         #but for interview, log(m+n) chaiye
+        merged = []
+        i, j = 0, 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] < nums2[j]:
+                merged.append(nums1[i])
+                i += 1
+            else:
+                merged.append(nums2[j])
+                j += 1
+        while i < len(nums1):
+            merged.append(nums1[i])
+            i += 1
+        while j < len(nums2):
+            merged.append(nums2[j])
+            j += 1
+            
+        n = len(merged)
+        if n & 1 == 1:
+            return merged[n//2]
+        else:
+            return (merged[n//2-1] + merged[(n//2)]) / 2
         
         
         
