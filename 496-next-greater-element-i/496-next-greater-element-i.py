05c94c2b163e262f0673d57dedb7a98f3f505c46
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-15 18:24:20 +0530

    Time: 44 ms (94.46%), Space: 14.2 MB (74.39%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -1,22 +1,16 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         #just create NGR for nums2
-        ngr = [-1]*len(nums2)
+        #ngr = [-1]*len(nums2)      #will use map directly instead
         st = []
         
-        m = defaultdict(int)
+        dic = defaultdict(int)
         
-        for i in range(len(nums2)-1, -1, -1):
-            m[nums2[i]] = i
-                
+        for i in range(len(nums2)-1, -1, -1):                
             while st and nums2[st[-1]] < nums2[i]:
                 st.pop()
-            ngr[i] = nums2[st[-1]] if st else -1
+            dic[nums2[i]] = nums2[st[-1]] if st else -1
             st.append(i)
         
         #now map nums1 to nums2 direct like direct
-        ans = []
-        for i in range(len(nums1)):
-            indToNums2 = m[nums1[i]]
-            ans.append(ngr[indToNums2])
-        return ans
+        return [dic[nums1[i]] for i in range(len(nums1))]
