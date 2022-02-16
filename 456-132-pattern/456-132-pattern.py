72d2286408bb919d936ef45cccfcfe155eec410a
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-16 11:43:58 +0530

    Time: 352 ms (82.68%), Space: 32 MB (98.62%) - LeetHub

--- 456-132-pattern/456-132-pattern.py
+++ 456-132-pattern/456-132-pattern.py
@@ -7,7 +7,7 @@ class Solution:
 #         for i in range(len(nums)):
 #             while st and nums[st[-1]] > nums[i]:
 #                 st.pop()
-#             left[i] = nums[st[-1]] if st else nums[i]-1
+#             left[i] = nums[st[-1]] if st else nums[i]
 #             st.append(i)
             
 #         st = []
@@ -15,7 +15,7 @@ class Solution:
 #         for i in range(len(nums)-1, -1, -1):
 #             while st and nums[st[-1]] > nums[i]:
 #                 st.pop()
-#             right[i] = nums[st[-1]] if st else nums[i]-1
+#             right[i] = nums[st[-1]] if st else nums[i]
 #             st.append(i)
 #         for i in range(len(nums)):
 #             if left[i] < right[i] < nums[i]:
@@ -26,7 +26,8 @@ class Solution:
     #NOPES, didnt work for all test cases, issue is with the ele whose left or right is not possible. i have tried none, max, min nothing worked. That thoda socha solution dekhu to pata chala dimag ka prayog karna hai mene kaha ohh isliye merese nh bana. 
     #Baat esi hai 132, means left me 1 and 1 is smallest of 132. So just create a min prefix array so from any index you can get the minimest ele possible, since 2 is greater than 1 but smallet than 3, so on right side we cant go for the minimal (that worked on left as 132), so for right NSR but for left prefix array . Now while going we ll keep checking if curr nums[i] is > than min[i] then only operate on stack and find the right bigger, once found(after all popping) see the condition met or not if yes return true else revise. See code
     
-        min_list = list(accumulate(nums, min))
+        
+        min_list = list(accumulate(nums, min))      #prefix min
         stack, n = [], len(nums)
         
         #NSR
