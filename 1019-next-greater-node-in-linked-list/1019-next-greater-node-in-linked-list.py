9d7226bae14ab1ef1da72572d5d3d6a411056d35
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-17 19:01:20 +0530

    Time: 432 ms (46.21%), Space: 19 MB (20.30%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.py
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.py
@@ -20,18 +20,34 @@ class Solution:
         #     st.append(i)
         # return r
         
-        #2 wihtout converting
-        n = 0
-        cur = head
-        while cur:
-            n += 1
-            cur = cur.next
+        #2 wihtout converting, but you need calc lenght as need to initialize arr(prev_ind)
+#         n = 0
+#         cur = head
+#         while cur:
+#             n += 1
+#             cur = cur.next
             
+#         cur = head
+#         r = [0]*n
+#         st = []
+#         i = 0
+#         while cur:
+#             while st and st[-1][1] < cur.val:
+#                 prev_ind = st.pop()[0]
+#                 r[prev_ind] = cur.val
+#             st.append((i, cur.val))
+#             i += 1
+#             cur = cur.next
+#         return r
+    
+        #3 without cal lenght, issue was we need right size initialization, lets see hack without lrnght
+        
         cur = head
-        r = [0]*n
+        r = []              #hack
         st = []
         i = 0
         while cur:
+            r.append(0)         #hack, wow created space
             while st and st[-1][1] < cur.val:
                 prev_ind = st.pop()[0]
                 r[prev_ind] = cur.val
@@ -39,5 +55,4 @@ class Solution:
             i += 1
             cur = cur.next
         return r
-    
         
