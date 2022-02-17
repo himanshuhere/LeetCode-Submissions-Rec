658cc1f8373e1a9fa7ed9f16fe99b465c49b14b7
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-17 18:58:20 +0530

    Time: 473 ms (34.52%), Space: 19 MB (29.93%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.py
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.py
@@ -6,19 +6,19 @@
 class Solution:
     def nextLargerNodes(self, head: Optional[ListNode]) -> List[int]:
         #brute comverting list to arr first
-        arr = []
-        cur = head
-        while cur:
-            arr.append(cur.val)
-            cur = cur.next
-        r = [0]*len(arr)
-        st = []
-        for i in range(len(arr)):
-            while st and arr[st[-1]] < arr[i]:
-                prev_ind = st.pop()
-                r[prev_ind] = arr[i]
-            st.append(i)
-        return r
+        # arr = []
+        # cur = head
+        # while cur:
+        #     arr.append(cur.val)
+        #     cur = cur.next
+        # r = [0]*len(arr)
+        # st = []
+        # for i in range(len(arr)):
+        #     while st and arr[st[-1]] < arr[i]:
+        #         prev_ind = st.pop()
+        #         r[prev_ind] = arr[i]
+        #     st.append(i)
+        # return r
         
         #2
         n = 0
@@ -32,11 +32,12 @@ class Solution:
         st = []
         i = 0
         while cur:
-            while st and arr[st[-1]] < cur.val:
-                prev_ind = st.pop()
+            while st and st[-1][1] < cur.val:
+                prev_ind = st.pop()[0]
                 r[prev_ind] = cur.val
-            st.append(i)
+            st.append((i, cur.val))
             i += 1
+            cur = cur.next
         return r
     
         
