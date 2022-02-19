7e7c04ce03142c5ba09304b2f25a6d54348c10d7
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-19 13:15:44 +0530

    Time: 218 ms (62.80%), Space: 14.5 MB (79.92%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.py
+++ 605-can-place-flowers/605-can-place-flowers.py
@@ -1,21 +1,27 @@
 class Solution:
-    def canPlaceFlowers(self, f: List[int], n: int) -> bool:
-        if n == 0:
+    def canPlaceFlowers(self, fl: List[int], n: int) -> bool:
+        if not n:
             return True
-        
-        f = [0] + f + [0]       #but it will make copy or array to add 0s, lets handle edge cases in loop
-        
-        for i in range(1, len(f)-1):
-            # prev = f[i-1] if i-1 >= 0 else 0
-            # nxt = f[i+1] if i+1 <= len(f)-1 else 0
+        if len(fl) == 1:
+            if n == 1 and fl[0] == 0:
+                return True
+            else:
+                return False
             
-            if f[i-1] == f[i] == f[i+1] == 0:
-                n -= 1
-                f[i] = 1
-                if n == 0:  return True
-        return False
-    # addinf 0 to both end is a clever move to ignore handling edge cases but its a reasonable trade off with 
-        # making a copy of array while adding Os, either we cud also handle edge cases if dont want
-        # to make new copy. like if i+1>len(flowerbed)?0:a[i+1] i-1<0?0:a[i-1] 
-        
-        #if (i == 0 or f[i-1] == 0) and (f[i] == 0) and  (i == len(f)-1 or f[i+1] == 0):
+        for i in range(len(fl)):
+            if i == 0:
+                if fl[i] == 0 and fl[i+1] == 0:
+                    n -= 1
+                    fl[i] = 1
+            elif i == len(fl)-1:
+                if fl[i] == 0 and fl[i-1] == 0:
+                    n -= 1
+                    fl[i] = 1
+            else:
+                if fl[i] == 0 and fl[i-1] == 0 and fl[i+1] == 0:
+                    n -= 1
+                    fl[i] = 1
+            if n == 0:
+                return True
+        return True if not n else False
+                
