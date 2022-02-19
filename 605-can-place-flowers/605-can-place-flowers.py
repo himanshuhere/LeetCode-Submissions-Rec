8b8f91bc6262830714102a35117c30f8b2245ca4
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-19 13:21:45 +0530

    Time: 211 ms (65.05%), Space: 14.5 MB (56.87%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.py
+++ 605-can-place-flowers/605-can-place-flowers.py
@@ -1,27 +1,43 @@
 class Solution:
     def canPlaceFlowers(self, fl: List[int], n: int) -> bool:
-        if not n:
+        #first time  worked
+        def intuitive():
+            if not n:
+                return True
+            if len(fl) == 1:
+                if n == 1 and fl[0] == 0:
+                    return True
+                else:
+                    return False
+
+            for i in range(len(fl)):
+                if i == 0:
+                    if fl[i] == 0 and fl[i+1] == 0:
+                        n -= 1
+                        fl[i] = 1
+                elif i == len(fl)-1:
+                    if fl[i] == 0 and fl[i-1] == 0:
+                        n -= 1
+                        fl[i] = 1
+                else:
+                    if fl[i] == 0 and fl[i-1] == 0 and fl[i+1] == 0:
+                        n -= 1
+                        fl[i] = 1
+                if n == 0:
+                    return True
+            return True if not n else False
+        
+        
+        
+        #want to skip edge cases tyr this
+        if n == 0:
             return True
-        if len(fl) == 1:
-            if n == 1 and fl[0] == 0:
-                return True
-            else:
-                return False
-            
-        for i in range(len(fl)):
-            if i == 0:
-                if fl[i] == 0 and fl[i+1] == 0:
-                    n -= 1
-                    fl[i] = 1
-            elif i == len(fl)-1:
-                if fl[i] == 0 and fl[i-1] == 0:
-                    n -= 1
-                    fl[i] = 1
-            else:
-                if fl[i] == 0 and fl[i-1] == 0 and fl[i+1] == 0:
-                    n -= 1
-                    fl[i] = 1
-            if n == 0:
-                return True
-        return True if not n else False
-                
+        
+        fl = [0] + fl + [0]
+        for i in range(1, len(fl)-1):
+            if fl[i-1] == fl[i] == fl[i+1] == 0:
+                fl[i] = 1
+                n -= 1
+                if not n:
+                    return True
+        return False
