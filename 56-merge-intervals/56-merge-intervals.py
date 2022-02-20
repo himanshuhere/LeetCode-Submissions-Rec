69ed426d4145419ace7ef62863f0145f700b864a
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-20 20:48:48 +0530

    Time: 217 ms (37.26%), Space: 18.9 MB (20.02%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -26,3 +26,46 @@ class Solution:
         
         #lets say if only ask to check if overlap present. Do same but return True after if condition
     
+    
+    
+    #1 Meeting rooms - need to check how many meeting one can attend
+    #sort on start and then
+#     //sort intervals by start time
+#       appointmentTimes.sort((a,b) => a[0] -b[0])
+
+#       //check if any two intervals overlap
+#       for(let i = 1; i < appointmentTimes.length; i++) {
+#         if(appointmentTimes[i][0] < appointmentTimes[i-1][1]) {
+#           //note that in the comparison above, it is < and not <=
+#           //while merging we needed <= comparison, as we will be merging the two 
+#           //intervals have conditions appointmentTimes[i][0] === appointmentTimes[i-1][1]
+#           //but such intervals don't represent conflicting appointments
+#           //as one starts right after the other
+#           return false
+#         }
+#       }
+#       return true
+
+
+#meeting - 2
+#use min heap for ending time
+#rooms will have number of rooms currently having meetings, just like real world implementation see
+# //sort meetings on start time
+#   meetings.sort((a,b) => a[0]-b[0])
+  
+#   let rooms = [meetings[0]]
+  
+#   for(let i = 1; i < meetings.length; i++) {
+#     let earliestRoom = getEarliest(rooms)
+#     let currentTime = meetings[i]
+    
+#     //if the room time ends before the currentTime interval starts
+#     //then use the room and update the room end time to currentTime
+#     if(earliestRoom[1] <= currentTime[0]) {
+#       earliestRoom[1] = currentTime[1]
+#     } else {
+#       //create room
+#       rooms.push(currentTime)
+#     }
+#   }
+#   return rooms.length
