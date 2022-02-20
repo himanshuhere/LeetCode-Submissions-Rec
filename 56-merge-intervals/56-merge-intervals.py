ffbe21f395b53d115e7993190c3452e2889946f1
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-20 19:40:28 +0530

    Time: 204 ms (42.63%), Space: 18.9 MB (20.02%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,57 +1,25 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals = sorted(intervals)   #first key toh sorted, for second key we keep max element see
+    #     Sort the intervals on the start time to ensure a.start <= b.start
+    # If a overlaps b (i.e. b.start <= a.end), we need to merge them into a new interval c such that:
+    # c.start = a.start
+    # c.end = max(a.end, b.end)
+    #since sorted on first key, we need to check only second now. do test case graphical
+        n = len(intervals)
         ans = []
-        i = 0
-        j = -1
-        while i < len(intervals)-1:
-            
-            if intervals[i][1] >= intervals[i+1][0]:
-                j = i
-                seckey = intervals[i][1]        #ye nhi kroge to error bahot ayege casec me
-                while j < len(intervals)-1 and seckey >= intervals[j+1][0]:
-                    j += 1
-                    seckey = max(seckey, intervals[j][1])
-                    
-                ans.append([intervals[i][0], seckey])
-                i = j + 1
+        
+        intervals = sorted(intervals)       #sort on first key
+        start = intervals[0][0]
+        end = intervals[0][1]
+        for i in range(1, n):
+            if intervals[i][0] <= end:      #overlapping
+                end = max(end, intervals[i][1])
+            else:                           #no overlap, add previous st,end and update wth current
+                ans.append([start, end])
+                start = intervals[i][0]
+                end = intervals[i][1]
+        #add the last one
+        ans.append([start, end])
+        return ans
                 
-            else:
-                ans.append([intervals[i][0], intervals[i][1]])
-                i += 1
-        
-        if j < len(intervals)-1:
-            ans.append(intervals[-1])
-            
-        return ans
-    
-        #2 shorter version of same logic
-        intervals = sorted(intervals)   
-        ans = []
-        i = 0
-        while i < len(intervals):
-            j = i
-            seckey = intervals[j][1]        
-            while j < len(intervals) - 1 and seckey >= intervals[j+1][0]:
-                j += 1
-                seckey = max(seckey, intervals[j][1])
-                    
-            ans.append([intervals[i][0], seckey])
-            i = j
-            i += 1
-            
-        return ans
-    
-    #3 smooth version code, got from discussion
-        A = sorted(A)
-        i = 0
-        ans = []
-        ans.append(A[0])
-        
-        for i in range(1, len(A)):
-            if ans[-1][1] >= A[i][0]:
-                ans[-1][1] = max(ans[-1][1], A[i][1])
-            else:
-                ans.append(A[i])
-        return ans
     
