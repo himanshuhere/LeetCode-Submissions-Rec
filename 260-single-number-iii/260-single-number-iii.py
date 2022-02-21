874352d282db98a402fb1c1a8852013aaa4c0174
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-21 22:06:37 +0530

    Time: 90 ms (50.14%), Space: 15.7 MB (91.18%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.py
+++ 260-single-number-iii/260-single-number-iii.py
@@ -1,5 +1,10 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
+        #Simple algo, find the xor of all. You will get x1 xor x2. but need to separate number.
+        #Just check if any of the bit is 1, (it will be because both numbers are diff, in case same it is 0 but no)
+        #Now, find that 1 set bit any from right. That will tell that either of number has 0 at that place and other has 1 since 0^1 or 1^0 gives 1.
+        #now segragate numbers on basis of bits and then xor them separately Boom you get the ans
+        
         n1xn2 = 0
         for num in nums:
             n1xn2 ^= num
