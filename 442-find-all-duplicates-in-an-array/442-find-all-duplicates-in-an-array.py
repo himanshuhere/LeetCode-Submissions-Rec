18e8e2973ac545e0658569918d7bfd3a4a5143e2
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-21 17:20:33 +0530

    Time: 441 ms (57.48%), Space: 21.1 MB (96.19%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.py
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.py
@@ -4,9 +4,25 @@ class Solution:
         #last problem 287. Find the Duplicate Number has contraint that we cant modify array
         #this problem dsnt have any such issue so go on
         #we ll make visited index negative 
-        ans = [] 
-        for i in range(len(nums)):
-            if nums[abs(nums[i]) - 1] < 0: 
-                ans.append(abs(nums[i]) - 1 + 1)
-            nums[abs(nums[i]) - 1] = -nums[abs(nums[i]) - 1]
+        # ans = [] 
+        # for i in range(len(nums)):
+        #     if nums[abs(nums[i]) - 1] < 0: 
+        #         ans.append(abs(nums[i]) - 1 + 1)
+        #     nums[abs(nums[i]) - 1] = -nums[abs(nums[i]) - 1]
+        # return ans
+    
+    #or dekh ke CYCLIC SORT yad aaya hoga lets do with that
+        i = 0
+        n = len(nums)
+        while i < n:
+            j = nums[i] - 1
+            if nums[i] != nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
+            else:
+                i += 1
+        
+        ans= []
+        for i in range(n):
+            if nums[i] - 1 != i:
+                ans.append(nums[i])
         return ans
