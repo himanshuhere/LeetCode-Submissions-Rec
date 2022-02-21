dd9e46988e9a631a269843792de796bfc42069c0
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-21 10:24:03 +0530

    Time: 219 ms (13.50%), Space: 15.3 MB (85.82%) - LeetHub

---
+++ 229-majority-element-ii/229-majority-element-ii.py
@@ -0,0 +1,35 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        #Boyer moore voting algo
+        #There will be atmost two such elemets (zero, one or two)
+        
+        if not nums:
+            return []
+        
+        count1, count2, candidate1, candidate2 = 0, 0, None, None
+        for n in nums:
+            if n == candidate1:
+                count1 += 1
+            elif n == candidate2:
+                count2 += 1
+            elif count1 == 0:
+                candidate1, count1 = n, 1
+            elif count2 == 0:
+                candidate2, count2 = n, 1
+            else:
+                count1, count2 = count1 - 1, count2 - 1
+        
+        #Now need to check, if both appears more than n/3
+        count1 = count2 = 0
+        for n in nums:
+            if n == candidate1:
+                count1 += 1
+            elif n == candidate2:
+                count2 += 1
+        ans = []
+        if count1 > len(nums)//3:
+            ans.append(candidate1)
+        if count2 > len(nums)//3:
+            ans.append(candidate2)
+            
+        return ans
