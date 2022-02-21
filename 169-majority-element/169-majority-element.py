6163ce5866319e0b0bc28400bb1ee39038b26f39
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-21 10:04:08 +0530

    Time: 164 ms (91.68%), Space: 15.5 MB (81.81%) - LeetHub

---
+++ 169-majority-element/169-majority-element.py
@@ -0,0 +1,19 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        #there are many solutions, sort, hashmap, but looking for boyer moore voting algorith
+        
+        # nums.sort()
+        # return nums[len(nums)//2]     n/2 elements will be filled with half list
+        
+        # Boyer-Moore Voting Algorithm -  worth to understacd, guaranteed algo
+        mcandidate = None       # or -1
+        count = 0
+        
+        for num in nums:
+            if count == 0:
+                mcandidate = num
+            if num == mcandidate: count += 1
+            else: count -= 1
+        return mcandidate
+        #if count of mcandidate not >= n/2 means it is not maj cadidate and then there is no maj candidate here since algo is guranteed 
+    
