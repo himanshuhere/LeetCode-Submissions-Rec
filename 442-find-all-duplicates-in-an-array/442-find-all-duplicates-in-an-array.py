6bf72463bef1ab57d42a6b3f0e4ef78b08c2962f
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-21 17:20:50 +0530

    Time: 627 ms (22.23%), Space: 21.8 MB (58.43%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.py
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.py
@@ -4,12 +4,12 @@ class Solution:
         #last problem 287. Find the Duplicate Number has contraint that we cant modify array
         #this problem dsnt have any such issue so go on
         #we ll make visited index negative 
-        # ans = [] 
-        # for i in range(len(nums)):
-        #     if nums[abs(nums[i]) - 1] < 0: 
-        #         ans.append(abs(nums[i]) - 1 + 1)
-        #     nums[abs(nums[i]) - 1] = -nums[abs(nums[i]) - 1]
-        # return ans
+        ans = [] 
+        for i in range(len(nums)):
+            if nums[abs(nums[i]) - 1] < 0: 
+                ans.append(abs(nums[i]) - 1 + 1)
+            nums[abs(nums[i]) - 1] = -nums[abs(nums[i]) - 1]
+        return ans
     
     #or dekh ke CYCLIC SORT yad aaya hoga lets do with that
         i = 0
