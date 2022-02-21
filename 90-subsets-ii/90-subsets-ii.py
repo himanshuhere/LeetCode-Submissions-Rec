0c8c204aec93daf9cd1104eabb285785c27d0485
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-21 19:05:35 +0530

    Time: 74 ms (14.61%), Space: 14.2 MB (80.02%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -1,6 +1,14 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
+        def sub2(i, tmp):
+            ans.append(tmp[:])
+            for j in range(i, n):
+                if j > i and nums[j] == nums[j-1]:  continue
+                tmp.append(nums[j])
+                sub2(j+1, tmp)
+                tmp.pop()
+        
         
         def sub(i, tmp, preChosen):
             if i == n:
@@ -17,22 +25,15 @@ class Solution:
             sub(i+1, tmp, True)
             tmp.pop()
             
-        # // If the previous number is same as current number i.e., if nums[i - 1] == nums[i], 
-        # // and if previous number was chosen(i.e., choosePre is true),
-        # // then dont re-include the current number because it would then be a duplicate
+            # // If the previous number is same as current number i.e., if nums[i - 1] == nums[i], 
+            # // and if previous number was chosen(i.e., choosePre is true),
+            # // then dont re-include the current number because it would then be a duplicate
             
             
             
-#         def sub2(i, tmp):
-#             if i == n:
-#                 ans.append(tmp[:])
-#                 return
-#             for j in range(i, len(nums)):
-#                 tmp.append(nums[j])
-#                 sub2(j+1, tmp)
-#                 tmp.pop()
         
         nums.sort()
         ans = []
-        sub(0, [], False)
+        #sub(0, [], False)
+        sub2(0, [])
         return ans
