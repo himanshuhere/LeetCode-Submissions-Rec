98513d4a5ade83898bf58f91807facd64a41e22a
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-22 21:50:40 +0530

    Time: 102 ms (65.19%), Space: 21.3 MB (71.34%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
@@ -15,8 +15,12 @@ class Solution:
             #Induction
             self.res = max(self.res, l + r + root.val)
             return max(root.val + max(l, r), 0)   
-            #0 is making sure of any ndoe to any node thing, like kadane if normal way giving neg ans, leave and start a new beginning with 0. if not 0 it will give leaf to leaf ans. think and learn
+            
         
         self.res = -math.inf
         dfs(root)
         return self.res
+    
+    
+    #This problem follows the Binary Tree Path Sum pattern and shares the algorithmic logic with Tree Diameter. We can follow the same DFS approach. The only difference will be to ignore the paths with negative sums. Since we need to find the overall maximum sum, we should ignore any path which has an overall negative sum.
+    #0 is making sure of any ndoe to any node thing, like kadane if normal way giving neg ans, leave and start a new beginning with 0. if not 0 it will give leaf to leaf ans. think and learn
