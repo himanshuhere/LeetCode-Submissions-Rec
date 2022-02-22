4453d4cccb73ddd3754518d7fc0574cb65e6450d
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-22 14:24:13 +0530

    Time: 39 ms (92.23%), Space: 16.4 MB (7.47%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -12,17 +12,35 @@ class Solution:
         return f(root)
         
     
-        #BFS
-        if not root:
-            return 0
-        q = deque([[root, 1]])
-        res = 0
-        while q:
-            for _ in range(len(q)):
-                x, h = q.popleft()
-                res = max(res, h)
-                if x.left:
-                    q.append([x.left, h+1])
-                if x.right:
-                    q.append([x.right, h+1])
-        return res
+        #DFS 2 
+        def dfs(root):
+            if not root:
+                return -math.inf
+            
+            if not root.left and not root.right: 
+                return 1
+                #this is very imp, as leaf will return inf from both side thus min will be inf only and will pass to higher level uptp the root
+                
+            
+            l, r = dfs(root.left), dfs(root.right)
+            return 1 + max(l, r)
+        
+        def bfs(root):
+            #will use bfs. bfs will always reach the first leaf first and return.
+            q = deque([root])
+            h = 1
+            while q:
+                for _ in range(len(q)):
+                    x = q.popleft()
+                    # if not x.left and not x.right:
+                    #     return h
+                    if x.left:
+                        q.append(x.left)
+                    if x.right:
+                        q.append(x.right)
+                h += 1
+            return h
+        #return dfs(root)
+        return bfs(root)
+    
+    #Maximum depth of binary tree - We will follow a similar approach. Instead of returning as soon as we find a leaf node, we will keep traversing for all the levels, incrementing maximumDepth each time we complete a level.
