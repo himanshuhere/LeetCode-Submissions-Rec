4b7b71a2215ea7f0659490a1d1a2c1b290690be8
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-22 10:18:26 +0530

    Time: 76 ms (34.72%), Space: 13.9 MB (66.66%) - LeetHub

--- 179-largest-number/179-largest-number.py
+++ 179-largest-number/179-largest-number.py
@@ -28,3 +28,15 @@ class Solution:
                 break
         
         return ans if not allzeroes else "0"
+    
+    
+    ###########################################################################
+    #def compare(n1, n2):
+            # if n1 + n2 > n2 + n1:
+            #     return -1
+            # else:
+            #     return 1
+    #key=cmp_to_key() needs only [-1, 0, 1]. -1 if fist numbers is priority, 1 if second and 0 if both same.
+    #since we dont need zero as if n1+n2 same from both side add aything so return 1 will work.
+    #str(int(ans)) would work in python for zeroes, but other lang can have overflow. Careful!!
+            
