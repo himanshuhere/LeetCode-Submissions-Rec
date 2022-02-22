52c0533c74d16c27586a313d6d060d3e9ec02040
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-22 10:16:19 +0530

    Time: 73 ms (38.33%), Space: 13.9 MB (66.66%) - LeetHub

---
+++ 179-largest-number/179-largest-number.py
@@ -0,0 +1,30 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        #Greedy
+        #Best thing would be take two numbers, and decide which one should go first.
+        #We ll use sort with compare dont worry, nlogn needs but
+        #lets say 3, 30 then wll see 330 or 303, then decide that 3 needs first position.
+        #Edge case, multiple 0s can be in string, only return 0
+        
+        #int -> str
+        for i in range(len(nums)):
+            nums[i] = str(nums[i])      #num can max be 100, no overflow. use str
+        
+        def compare(n1, n2):
+            if n1 + n2 > n2 + n1:
+                return -1
+            else:
+                return 1
+            
+        nums = sorted(nums, key=cmp_to_key(compare))
+        
+        ans = "".join(nums)
+        
+        #check if all zeros
+        allzeroes = True
+        for ch in ans:
+            if ch != "0":
+                allzeroes = False
+                break
+        
+        return ans if not allzeroes else "0"
