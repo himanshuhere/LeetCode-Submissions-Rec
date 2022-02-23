8d64035b1010fd5aa6b4f32393c83fb6c8dc5c79
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-23 13:35:37 +0530

    Time: 937 ms (16.05%), Space: 15.1 MB (92.79%) - LeetHub

---
+++ 437-path-sum-iii/437-path-sum-iii.py
@@ -0,0 +1,31 @@
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], target: int) -> int:
+        #1. Brute Force: O(nlogn) ~ O(n^2)
+        # 1.1 High level walk-through:
+        # (Define return) Define a global var: self.numOfPaths in the main function.
+        # (1st layer DFS) Use recursive traverse to go through each node (can be any order: pre, in, post all fine).
+        # (2nd layer DFS) For each node, walk all paths. If a path sum equals to the target: self.numOfPaths += 1
+        # Return result: return self.numOfPaths
+        
+        def dfs(node):
+            if node is None:
+                return 
+
+            test(node, target)         # you can move the line to any order, here is pre-order
+            dfs(node.left)
+            dfs(node.right)
+
+        def test(node, target):
+            if node is None:
+                return
+            
+            if node.val == target:          #at any node, not only leaf
+                self.numOfPaths += 1
+
+            test(node.left, target-node.val)
+            test(node.right, target-node.val)
+        
+        self.numOfPaths = 0
+        # 1st layer DFS to go through each node
+        dfs(root)
+        return self.numOfPaths
