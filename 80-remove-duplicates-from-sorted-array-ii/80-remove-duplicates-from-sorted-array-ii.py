1d35a8b098061d1bed63fa242b63722bdf07fd15
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-23 18:58:25 +0530

    Time: 115 ms (10.57%), Space: 13.9 MB (43.60%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.py
@@ -12,19 +12,4 @@ class Solution:
     
         
         
-        #Remove Duplicates from Sorted Array I
-        # i = 0
-        # for num in nums:
-        #     if i < 1 or num > nums[i-1]:
-        #         nums[i] = num
-        #         i += 1
-        # return i
-        
-        #80. Remove Duplicates from Sorted Array II
-        i = 0
-        for num in nums:
-            if i < 2 or num > nums[i-2]:
-                nums[i] = num
-                i += 1
-        return i
         
