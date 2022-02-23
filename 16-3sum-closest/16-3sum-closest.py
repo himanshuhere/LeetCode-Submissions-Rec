0d633de1c482814034ca7624ba427244e3abcfda
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-23 19:12:58 +0530

    Time: 500 ms (15.80%), Space: 14 MB (64.90%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -4,19 +4,21 @@ class Solution:
         #as per constraint size will be min 3 so consider all them as res
         res = A[0] + A[1] + A[2]
         
-        A= sorted(A)
+        A = sorted(A)
         #skipping duplicates will boost performance
+        
         for i in range(len(A)-2):
-            l, r = i+1, len(A)-1
-            
-            while l < r:
-                cursum = A[i]+A[l]+A[r]
-                if cursum > target:
-                    r-=1
-                else:
-                    l+=1
-                
-                if abs(res-target) > abs(cursum-target):
-                    res = cursum
+            if i == 0 or i > 0 and A[i-1] != A[i]:
+                l, r = i+1, len(A)-1
+
+                while l < r:
+                    cursum = A[i]+A[l]+A[r]
+                    if cursum > target:
+                        r-=1
+                    else:
+                        l+=1
+
+                    if abs(res-target) > abs(cursum-target):
+                        res = cursum
         return res
         
