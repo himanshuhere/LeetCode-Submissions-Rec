f180e861b89907e8673eb623f0d308b45dfad95f
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-23 10:53:42 +0530

    Time: 51 ms (55.16%), Space: 14.5 MB (48.95%) - LeetHub

--- 133-clone-graph/133-clone-graph.py
+++ 133-clone-graph/133-clone-graph.py
@@ -4,9 +4,7 @@ class Solution:
         
         if not node:
             return None
-       
         
-        #Only Graph traversal + map
         #DFS
         def dfs(old):
             copy = Node(old.val)
@@ -19,8 +17,8 @@ class Solution:
 
             return copy
         
-        #old_to_copy = {}
-        #return dfs(node)
+        old_to_copy = {}
+        return dfs(node)
                     
         #DFS - Why map? see visiting cost of graph is V+E, because vertices will be visisted once but due to some extra edges we might visit same node again, like 1-2, 3-2. 2 can be visisted twice. If in first visit we have already created copy of 2, next time we would not want to oerate on that again so map can be useful getting previosly stored copies.
         
