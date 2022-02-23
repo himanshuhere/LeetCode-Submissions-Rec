37e9f27b585f9bbb279ef52c8f99a690254390ea
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-23 19:56:32 +0530

    Time: 764 ms (72.95%), Space: 16.6 MB (87.26%) - LeetHub

---
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        i = 0
+        j = 0
+        p = 1
+        ans = 0
+        while j < len(nums):
+            p *= nums[j]
+            
+            while i < len(nums) and p >= k:
+                p //= nums[i]
+                i += 1
+                
+            ans += (j-i+1)
+            
+            j += 1
+        return ans if ans >= 0 else 0
