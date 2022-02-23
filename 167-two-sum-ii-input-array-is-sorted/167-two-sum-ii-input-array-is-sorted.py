becab0f73f63dcd5c024d3720a98ac48762d2345
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-23 11:11:49 +0530

    Time: 212 ms (14.02%), Space: 15 MB (15.37%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,7 +1,5 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #sorted thus no map, just two pointer
-        
         lo, hi = 0, len(nums)-1
         while lo < hi:
             if nums[lo] + nums[hi] < target:
