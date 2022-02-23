d8b31cae8b01f90bf192a17b533307cafc9e484e
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-23 10:42:25 +0530

    Time: 59 ms (38.72%), Space: 14.3 MB (91.72%) - LeetHub

--- 133-clone-graph/133-clone-graph.py
+++ 133-clone-graph/133-clone-graph.py
@@ -15,11 +15,13 @@ class Solution:
             old_to_copy[old] = copy
             
             for nei in old.neighbors:
-                old_to_copy[nei] = dfs(nei)
+                if nei not in old_to_copy:        
+                    old_to_copy[nei] = dfs(nei)
                 copy.neighbors.append(old_to_copy[nei])
-            
+
             return copy
         
         old_to_copy = {}
         return dfs(node)
                     
+        #DFS - Why map? see visiting cost of graph is V+E, because vertices will be visisted once but due to some extra edges we might visit same node again, like 1-2, 3-2. 2 can be visisted twice. If in first visit we have already created copy of 2, next time we would not want to oerate on that again so map can be useful getting previosly stored copies.
