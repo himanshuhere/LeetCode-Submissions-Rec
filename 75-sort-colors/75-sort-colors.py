b16ae60b1f9c43693d51e906a40309b1ea02d3d5
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-23 20:12:34 +0530

    Time: 53 ms (37.57%), Space: 14 MB (52.33%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -1,8 +1,5 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
     #see notes
     #1 - sort - nlog | 2 - count 0,1,2 and modify - n | 3 - dutch national flag - n three pointer
         
@@ -21,4 +18,5 @@ class Solution:
                 j -= 1              #dont inc mid, might possible 2,2 hi swap huye or 0 agya ho mid me end se usko i se swap bhi karna hai isliye let mid be there
             else:
                 mid += 1
-        
+                
+        #see left se mid i sath chale the to sure hai 0s hi hai udhar but right se 0 ya 2 ay 1 kuch b swap ho sakta hai to let mid be there and check once again
