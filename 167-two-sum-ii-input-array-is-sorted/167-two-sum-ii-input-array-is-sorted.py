17595d3d18eae0730511a194a5422adedb8981fe
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-23 11:12:32 +0530

    Time: 199 ms (16.28%), Space: 15 MB (15.37%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,5 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #sorted thus no map, just two pointer
+        
         lo, hi = 0, len(nums)-1
         while lo < hi:
             if nums[lo] + nums[hi] < target:
@@ -7,5 +9,4 @@ class Solution:
             elif nums[lo] + nums[hi] > target:
                 hi -= 1
             else:
-                return [lo+1, hi+1]
-        
+                return [lo+1, hi+1]         #take care as ans wants 1-based indexing
