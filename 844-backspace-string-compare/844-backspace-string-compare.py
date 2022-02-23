b7b2f2fac7d28b9d2ea7ee39c5648e451e43d3c4
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-23 21:51:13 +0530

    Time: 28 ms (94.67%), Space: 14 MB (69.52%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.py
+++ 844-backspace-string-compare/844-backspace-string-compare.py
@@ -2,14 +2,57 @@ class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
         #you need to remove previous element when you find #, ab## but in test case like this you also need to remember previous chars thus when you get multiple # you can remove recent chars. Itne ke bad bhi nahi samjh aarha STACK use hoga brute me atleast. Buddhiheen jara soch liya karo itna
         
-        def build(str):
-            st = []
-            for ch in str:
-                if ch != "#":
-                    st.append(ch)
-                else:
-                    if st:
-                        st.pop()
-            return "".join(st)
+#         def build(str):
+#             st = []
+#             for ch in str:
+#                 if ch != "#":
+#                     st.append(ch)
+#                 else:
+#                     if st:
+#                         st.pop()
+                        
+#             return "".join(st)
         
-        return build(s) == build(t)
+#         return build(s) == build(t)
+    
+    
+    #O(1) space
+        i, j = len(s)-1, len(t)-1
+        sskip, tskip = 0, 0
+        
+        while i >= 0 or j >= 0:
+            while i >= 0:
+                if s[i] == "#":
+                    sskip += 1
+                    i -= 1
+                elif sskip > 0:
+                    sskip -= 1
+                    i -= 1
+                else:
+                    break
+            
+            while j >= 0:
+                if t[j] == "#":
+                    tskip += 1
+                    j -= 1
+                elif tskip > 0:
+                    tskip -= 1
+                    j -= 1
+                else:
+                    break
+            
+            
+            #if two char are different
+            if i >= 0 and j >= 0 and s[i] != t[j]:
+                return False
+            
+            
+           
+            if (i >= 0) != (j >= 0):
+                return False
+            
+            i -= 1
+            j -= 1
+        
+        return True
+            
