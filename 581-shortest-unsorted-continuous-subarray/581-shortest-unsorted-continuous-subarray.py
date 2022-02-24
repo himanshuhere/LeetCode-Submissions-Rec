730a94d8a6e82a2154b143cccbd91078ae7e3890
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-24 11:12:40 +0530

    Time: 402 ms (16.54%), Space: 15.2 MB (78.92%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.py
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.py
@@ -1,14 +1,10 @@
 class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
-        #very easy o(4n)
-        n = len(nums)
-        st = []
-        #there might be many diff zero space ways but this problem i did for monotonic stack toh see
-        #from left we ll find the first value which causing issue
-        
         #from left maintain increasing monotonicity, if anywhere it breaks capture the index, make sure to let left the legftmost index only, leftmost breakpoint thus min.
         #two pointer wold have worked if find left firts and right first worked, somw like 20,22,21,4,5,3,15, left most is 22 right most is 3, but technically 20, 15 is the edges, thus check all breakpoints and keep the one close to edge
         
+        n = len(nums)
+        st = []
         left, right = math.inf, -math.inf
         
         #increaisng stack
