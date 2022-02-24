338c9a5d6544dbfb4fd1467a9bb28e72aa4fd47f
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-24 22:46:43 +0530

    Time: 1089 ms (50.32%), Space: 20.7 MB (10.74%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.py
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -6,14 +6,17 @@ class Solution:
         
         i = j = ans = 0
         d = defaultdict(int)
+        count = 0
         
         while j < len(fruits):
             d[fruits[j]] += 1
+            if d[fruits[j]] == 1:
+                count += 1
                 
-            while len(d) > 2 and i < j:
+            while count > 2 and i < j:
                 d[fruits[i]] -= 1
                 if d[fruits[i]] == 0:
-                    d.pop(fruits[i])
+                    count -= 1
                 i += 1
             
             ans = max(ans, j - i + 1)
