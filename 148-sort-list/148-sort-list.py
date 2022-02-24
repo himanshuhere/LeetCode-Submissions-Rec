59d36eea49165ee533715c1471e07a730f761337
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-24 10:42:32 +0530

    Time: 790 ms (11.32%), Space: 29.9 MB (91.34%) - LeetHub

--- 148-sort-list/148-sort-list.py
+++ 148-sort-list/148-sort-list.py
@@ -1,37 +1,41 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+# The problem is to sort the linked list in O(nlogn) time and using only constant extra space. If we look at various sorting algorithms, Merge Sort is one of the efficient sorting algorithms that is popularly used for sorting the linked list. The merge sort algorithm runs in O(nlogn) time in all the cases. Let's discuss approaches to sort linked list using merge sort.
+
+# Quicksort is also one of the efficient algorithms with the average time complexity of O(nlogn). But the worst-case time complexity is O(n^2). Also, variations of the quick sort like randomized quicksort are not efficient for the linked list because unlike arrays, random access in the linked list is not possible in O(1) time. If we sort the linked list using quicksort, we would end up using the head as a pivot element which may not be efficient in all scenarios.
+
+
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        #MERGE SORT
+        def merge(l, r):
+            dummy = p = ListNode(0)
+            while l and r:
+                if l.val > r.val:
+                    p.next = r
+                    r = r.next
+                else:
+                    p.next = l
+                    l = l.next
+                p = p.next
+            p.next = l or r
+            return dummy.next
+    
+        
         if not head or not head.next:
             return head
         
-        #cut the list into two halves
-        fast, slow = head.next, head
-        while fast and fast.next:
-            fast = fast.next.next
-            slow = slow.next
-            
-        second = slow.next
-        slow.next = None
+        #Find the middle to devide
+        pre = ListNode(None)
+        sl, fs = head, head
+        while fs and fs.next:
+            pre = sl
+            sl = sl.next
+            fs = fs.next.next
+        pre.next = None
         
+        #call recursive for smaller length
         l = self.sortList(head)
-        r = self.sortList(second)
+        r = self.sortList(sl)
         
-        return self.merge(l, r)
+        return merge(l, r)
     
-    
-    def merge(self, l, r):
-        dummy = head1 = ListNode(0)
-        while l and r:
-            if l.val <= r.val:
-                head1.next = l
-                l = l.next
-            else:
-                head1.next = r
-                r = r.next    
-            head1 = head1.next
-        head1.next = l or r
-        return dummy.next
+        
