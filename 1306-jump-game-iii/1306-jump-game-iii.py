e32c10ea0ae47958da49c18fb3195db75d50d5e9
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-25 11:41:04 +0530

    Time: 489 ms (30.89%), Space: 21 MB (59.97%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.py
+++ 1306-jump-game-iii/1306-jump-game-iii.py
@@ -1,29 +1,38 @@
-import sys
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
 
-        def dfs(i):
-            if  i < 0 or i >= len(arr) or vis[i]:
-                return False
-            if arr[i] == 0:
-                return True
-            vis[i] = 1
-            return dfs(i + arr[i]) or dfs(i - arr[i])
+#         def dfs(i):
+#             if  i < 0 or i >= len(arr) or vis[i]:
+#                 return False
+#             if arr[i] == 0:
+#                 return True
+#             vis[i] = 1
+#             return dfs(i + arr[i]) or dfs(i - arr[i])
 
-        vis = [0]*len(arr)
-        return dfs(start)
+#         vis = [0]*len(arr)
+#         return dfs(start)
     
     #2
-    #iterative - as it is dfs - u need stack variation to make iter. pls understand which solution
-    #are dfs so u can also try stack possibility if it comes to iter implementation req
-#     stack, visited, n = [start], set(), len(arr)
+        queue = deque([start])
+        visited = set()
         
-#         while stack:
-#             pos = stack.pop()
-#             if arr[pos] == 0: return True
-#             visited.add(pos)
-#             cand1, cand2 = pos + arr[pos], pos - arr[pos]
-#             if cand1 <  n and cand1 not in visited: stack.append(cand1)
-#             if cand2 >= 0 and cand2 not in visited: stack.append(cand2)
-                
-#         return False
+        while queue:
+            u = queue.popleft()
+            if arr[u] == 0:
+                return True
+            visited.add(u)
+            
+            nextjump = u + arr[u]
+            if nextjump < len(arr) and nextjump not in visited:
+                if arr[nextjump] == 0:
+                    return True
+                visited.add(nextjump)
+                queue.append(nextjump)
+
+            nextjump = u - arr[u]
+            if nextjump >= 0 and nextjump not in visited:
+                if arr[nextjump] == 0:
+                    return True
+                visited.add(nextjump)
+                queue.append(nextjump)
+        return False
