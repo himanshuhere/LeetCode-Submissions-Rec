d319141f6980665d7b7cd25617877a133a366f2a
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-25 10:41:47 +0530

    Time: 599 ms (64.08%), Space: 15.3 MB (30.21%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,24 +1,29 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-#         def fn(i):
-#             if i > len(nums)-1:
-#                 return False
+        #TLE
+#         @lru_cache(None)
+#         def f(i):
 #             if i == len(nums)-1:
 #                 return True
-#             if (i, nums[i]) in self.m:
-#                 return self.m[(i, nums[i])]
             
-#             for j in range(1, nums[i]+1):
-#                 if fn(i+j):
-#                     self.m[(i, nums[i])] = True
+#             jumps = nums[i]
+#             for j in range(1, jumps+1):
+#                 if (i+j) < len(nums) and f(i+j):
 #                     return True
-#             self.m[(i, nums[i])] = False
 #             return False
+#         return f(0)
+    
+        # dp = [False]*len(nums)
+        # dp[-1] = True
+        # for i in range(len(nums)-2, -1, -1):
+        #     jumps = nums[i]
+        #     for j in range(1, jumps+1):
+        #         if (i+j) < len(nums) and dp[i+j]:
+        #             dp[i] = True
+        #             break
+        # return dp[0]
         
-#         self.m = {}  
-#         return fn(0)
-
-#this shit looking for something else that out of the box algo see, btw dp giving TLE
+        #this shit looking for something else that out of the box algo see, btw dp giving TLE
         last = len(nums)-1
         for i in range(len(nums)-2, -1, -1):
             #if with one or more jump, can i cross or land at last index if yes one sub prob solved
