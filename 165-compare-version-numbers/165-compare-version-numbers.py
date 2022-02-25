7629dfc5e5708ca05993cfa77871f9ff68e8fb98
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-25 10:08:18 +0530

    Time: 45 ms (45.51%), Space: 13.9 MB (68.52%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.py
+++ 165-compare-version-numbers/165-compare-version-numbers.py
@@ -1,16 +1,19 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        v1 ,v2 = version1.split("."), version2.split(".")
-        i, j = 0, 0
-        while i < len(v1) or j < len(v2):
-            v1cur = v1[i] if i < len(v1) else 0
-            v2cur = v2[i] if i < len(v2) else 0
-            
-            if int(v1cur) < int(v2cur):
-                return -1
-            elif int(v1cur) > int(v2cur):
+        #constraint -- 1 <= version1.length, version2.length <= 500 wud be coverred in parsing Int in other lang
+
+        v1 = version1.split('.')
+        v2 = version2.split('.')
+        n = max(len(v1), len(v2))
+        
+        for i in range(n):
+            rev1 = int(v1[i]) if i < len(v1) else 0     #clever right
+            rev2 = int(v2[i]) if i < len(v2) else 0
+            if rev1 > rev2:
                 return 1
-            i+=1
-            j+=1
+            elif rev1 < rev2:
+                return -1
+            else:
+                continue                    #imp to understand, we will not return 0 here untill we compare all revisions
+        
         return 0
-            
