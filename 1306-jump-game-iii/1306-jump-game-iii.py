4ea82c19c257bd2ba355c296d6ddbbde653d183c
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-25 11:42:39 +0530

    Time: 472 ms (35.83%), Space: 69.5 MB (26.59%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.py
+++ 1306-jump-game-iii/1306-jump-game-iii.py
@@ -1,16 +1,16 @@
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
 
-#         def dfs(i):
-#             if  i < 0 or i >= len(arr) or vis[i]:
-#                 return False
-#             if arr[i] == 0:
-#                 return True
-#             vis[i] = 1
-#             return dfs(i + arr[i]) or dfs(i - arr[i])
+        def dfs(i):
+            if  i < 0 or i >= len(arr) or vis[i]:
+                return False
+            if arr[i] == 0:
+                return True
+            vis[i] = 1
+            return dfs(i + arr[i]) or dfs(i - arr[i])
 
-#         vis = [0]*len(arr)
-#         return dfs(start)
+        vis = [0]*len(arr)
+        return dfs(start)
     
     #2
         queue = deque([start])
@@ -21,7 +21,6 @@ class Solution:
             u = queue.popleft()
             if arr[u] == 0:
                 return True
-            #visited.add(u)
             
             nextjump = u + arr[u]
             if nextjump < len(arr) and nextjump not in visited:
