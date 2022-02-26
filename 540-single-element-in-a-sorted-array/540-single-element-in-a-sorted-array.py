ba262a04dd365ff402f28a7d1ab9d82dda526ada
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-26 14:06:26 +0530

    Time: 336 ms (11.35%), Space: 23.8 MB (56.13%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
@@ -14,3 +14,10 @@ class Solution:
             else:
                 hi = mid
         return nums[lo]
+    
+    #Logic is simple.
+    #if pair is available in array like 1,1 so every pairs first ele will be at even index and second at odd index. If any of the culprit is present in between this pattern will break.
+    #we just need to check if patter holds then left side list is good go on right
+    #else goon left
+    #just check = if mid is even and duplicate is on right good or if mid is odd dupicate is in left fine. move right
+    #else move left
