661402abb645fb948316f3a7239b6f64aec61718
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-27 11:54:36 +0530

    Time: 58 ms (61.34%), Space: 14.8 MB (88.19%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.py
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.py
@@ -14,11 +14,16 @@ class Solution:
         
         while q: 
             ans = max(ans, q[-1][1] - q[0][1] + 1)
-            
+            #min_ind = q[0][1]
             for _ in range(len(q)):
                 node, index = q.popleft()
+                #index = index - min_ind
                 if node.left: 
                     q.append((node.left, 2*index))      #2*i
                 if node.right: 
                     q.append((node.right, 2*index+1))   #2*i+1        
         return ans 
+    
+    #commented lines are to save overflow in other language, as for long long skewed tree 2*i can lead to overflow if nodes can be 10^5 or something but hrer only 3000. so fine.
+    #that code will reset every level line 1 2 3 n something
+    
