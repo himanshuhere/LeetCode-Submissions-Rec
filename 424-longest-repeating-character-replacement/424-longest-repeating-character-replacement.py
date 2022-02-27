76c396e88037bc227b891125dcaf677312103540
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-27 13:34:47 +0530

    Time: 128 ms (83.12%), Space: 14 MB (59.68%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -5,18 +5,76 @@ class Solution:
         i, j = 0, 0
         ans = 0
         count = 0
+        maxRepLetter = ""
+        maxRepLetterCount = 0
         
         while j < len(s):
             m[s[j]] +=1 
+            if m[s[j]] > maxRepLetterCount:     #keep the max occured (max(m.value()))
+                maxRepLetterCount = m[s[j]]
+                maxRepLetter = s[j]
             
-            window = j-i+1
-            if window - max(m.values()) <= k:       #greedy
-                ans = max(ans, j-i+1)
-                
-            elif window - max(m.values()) > k:
+            window = j-i+1                
+            if window - maxRepLetterCount > k:
                 if s[i] in m:
                     m[s[i]] -=1 
+                    if s[i] == maxRepLetter:
+                        maxRepLetterCount -= 1
                 i += 1
+            
+            ans = max(ans, j-i+1)
+            
             j += 1
         return ans
-#window - max(m.values()), we want to long the string so we should look for replacing the least count chars in window, so that we can save more k for others, use k less and raise yuour widnow size thats y
+    
+        
+#         while j < len(s):
+#             m[s[j]] +=1 
+            
+#             window = j-i+1
+#             if window - max(m.values()) <= k:       #greedy
+#                 ans = max(ans, j-i+1)
+                
+#             elif window - max(m.values()) > k:
+#                 if s[i] in m:
+#                     m[s[i]] -=1 
+#                 i += 1
+#             j += 1
+#         return ans
+# #window - max(m.values()), we want to long the string so we should (greed) look for replacing the least count chars in window, so that we can save more k for others, use k less and raise yuour widnow size thats y
+
+
+
+
+
+#         #we can always make sliding widow variable to more elegant like
+#     #try to shrik window first if it is long, then capture ans. if not long we wud be capturing ans only
+#         while j < len(s):
+#             m[s[j]] +=1 
+#             window = j-i+1
+#             while window - max(m.values()) > k:
+#                 if s[i] in m:
+#                     m[s[i]] -=1 
+#                 i += 1
+#             ans = max(ans, j-i+1)
+#             j += 1
+#         return ans
+    
+#     #now ques comes, how to short bet this max(m.values()), you can right using some extra variable and maintain some logc
+    
+    
+#         maxRepLetterCount = 0
+#         while j < len(s):
+#             m[s[j]] +=1 
+#             maxRepLetterCount = max(maxRepLetterCount, m[s[j]])
+            
+#             window = j-i+1
+#             if window - maxRepLetterCount <= k:       #greedy
+#                 ans = max(ans, j-i+1)
+                
+#             elif maxRepLetterCount > k:
+#                 if s[i] in m:
+#                     m[s[i]] -=1 
+#                 i += 1
+#             j += 1
+#         return ans
