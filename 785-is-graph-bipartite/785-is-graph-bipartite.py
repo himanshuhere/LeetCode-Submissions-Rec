860b4c7eb19bde04273fac1318d0545ab9902438
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-28 12:46:09 +0530

    Time: 205 ms (69.86%), Space: 14.7 MB (52.52%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.py
+++ 785-is-graph-bipartite/785-is-graph-bipartite.py
@@ -1,6 +1,31 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-#         #bfs
+
+        #dfs
+        n = len(graph)
+        colored, vis = [None]*n, [0]*n
+        
+        def dfs(node, c):
+            vis[node] = 1       #technically you can use colored as vis too, if none not visited. 
+            colored[node] = c
+            
+            for k in graph[node]:
+                if vis[k] == 0:
+                    if dfs(k, c^1) == False:
+                        return False
+                else:       #visited, pehle se assigned color now check if right or not
+                    if colored[k] == colored[node]:
+                        return False
+            return True
+        
+        for i in range(n):
+            if vis[i] == 0:
+                if not dfs(i, 0): 
+                    return False
+        return True
+    
+    
+    #         #bfs
 #         n, colored = len(graph), {}
         
 #         for i in range(n):
@@ -18,25 +43,3 @@ class Solution:
 #                             return False
 #         return True
         
-        #dfs
-        n = len(graph)
-        colored, vis = [None]*n, [0]*n
-        
-        def dfs(node, c):
-            vis[node] = 1       #technically you can use colored as vis too, if none not visited. 
-            colored[node] = c
-            
-            for k in graph[node]:
-                if vis[k] == 0:
-                    if dfs(k, c^1) == False:
-                        return False
-                else:
-                    if colored[k] == colored[node]:
-                        return False
-            return True
-        
-        for i in range(n):
-            if vis[i] == 0:
-                if not dfs(i, 0): 
-                    return False
-        return True
