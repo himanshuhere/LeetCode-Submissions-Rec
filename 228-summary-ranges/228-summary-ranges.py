26d173dcaf95bbbffefec99154bcd9edabc90fde
Himanshu Soni <himanshusoni3105@outlook.com>
2022-02-28 10:10:30 +0530

    Time: 62 ms (7.61%), Space: 13.9 MB (61.40%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.py
+++ 228-summary-ranges/228-summary-ranges.py
@@ -1,15 +1,16 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
-        i=0
-        ans=[]
-        while i<len(nums):
-            j=i
-            while j<len(nums)-1 and nums[j]+1==nums[j+1]:
-                j+=1
-            if i!= j:
-                ans.append(str(nums[i])+"->" + str(nums[j]))
+        #Merge interval kind of flow i did
+        i = 0
+        ans = []
+        while i < len(nums):
+            j = i
+            while j < len(nums)-1 and nums[j]+1 == nums[j+1]:
+                j += 1
+            if i != j:
+                ans.append(str(nums[i]) + "->" + str(nums[j]))
             else:
                 ans.append(str(nums[i]))
-            i=j+1
+            i = j + 1
         return ans
         
