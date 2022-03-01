fb6d59a4ee6b824d698d2137e4f63c1e3bb39160
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-01 13:12:19 +0530

    Time: 186 ms (26.91%), Space: 20.9 MB (20.83%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,6 +1,30 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans = []
-        for i in range(0, n+1):
-            ans += [(bin(i)[2:]).count("1")]
+        #nlogn
+        # ans = []
+        # for i in range(0, n+1):
+        #     ans += [(bin(i)[2:]).count("1")]
+        # return ans
+        
+        
+        res = []
+        for i in range(n+1):
+            
+            #karninghan algo
+            count = 0
+            while i != 0:
+                i = i & ( i - 1 )
+                count += 1
+            res += [count]
+            
+        return res
+    
+        ans = [0]
+        m = [0, 1, 1, 2, 1, 2, 2, 3]    
+        for i in range(1, n+1):
+            ans += [m[i%8] + i//8 if i%8!=0 else 1]
         return ans
+        
+        
+        
+        
