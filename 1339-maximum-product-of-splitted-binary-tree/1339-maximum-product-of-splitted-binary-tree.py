b2c9acdb511282aeb6a836797e053b752bd9e956
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-01 21:58:19 +0530

    Time: 521 ms (42.48%), Space: 74.8 MB (79.27%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.py
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.py
@@ -45,32 +45,4 @@ class Solution:
         
         
         
-        #cutedge, delete edge
-        #its a tree, will do dfs tree algo, nit need visited because tree not graph, only par is enough. grah has cycle to keep visited
-        #no par needed actually its a tree we wont go back up
-        
-        def dfs(node):
-            nonlocal n
-            subsum[n] = node.val
-            n += 1
-            #for child in g[node] - not a graphbut binary tree only two poss childs
-            if node.left:
-                tmp = dfs(node.left)
-                subsum[n] += tmp
-            if node.right:
-                tmp = dfs(node.right)
-                subsum[n] += tmp
-            return subsum[n]
-        
-        # n = len(root)
-        subsum = [0]*1000000 #cant define its lenght
-        n = 1
-        tmp = dfs(root)
-        ans = 0
-        mod = int(1e9+7)
-        for i in range(2, n+1):
-            sub1 = subsum[i]
-            sub2 = subsum[1] - sub1
-            ans = max(ans, (sub1*sub2)%mod)
-        return ans%mod
         
