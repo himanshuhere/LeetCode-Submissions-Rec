f41388e7a4c60ee94655731969b10552facb133a
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-01 18:44:41 +0530

    Time: 3822 ms (31.30%), Space: 81.3 MB (37.89%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
@@ -24,7 +24,7 @@ class Solution:
                 if k not in vis:            #forward edge, can be bridge
                     dfs(k, node)
                     #after dfs call done
-                    low[node] = min(low[node], low[k])
+                    low[node] = min(low[node], low[k])      #check if conncted to ancestor(ancestor = means low timer)
                     if tin[node] < low[k]:      #k mere se b bada h mtlb koi ancestor se conncted nhi h bridge h ye mat kato
                         res.append([node, k])   #bridge
                         
