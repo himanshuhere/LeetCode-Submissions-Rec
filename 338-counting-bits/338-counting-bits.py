3c5095557888970e96072af9538eea8275f4fc9a
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-01 13:24:23 +0530

    Time: 169 ms (31.36%), Space: 20.8 MB (87.66%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -9,22 +9,27 @@ class Solution:
         
         res = []
         for i in range(n+1):
-            
             #karninghan algo
             count = 0
             while i != 0:
                 i = i & ( i - 1 )       #off the last bit
                 count += 1
             res += [count]
-            
         return res
     
-        ans = [0]
-        m = [0, 1, 1, 2, 1, 2, 2, 3]    
-        for i in range(1, n+1):
-            ans += [m[i%8] + i//8 if i%8!=0 else 1]
-        return ans
+        #Index : 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+
+        #num : 0 1 1 2  1 2 2 3  1 2 2 3  2 3 3 4
+        #dp[0]=0, dp[1]=dp[0]+1, d[2]=dp[0]+1, dp[3]=dp[2]+1...dp[4]=dp[0]+1, dp[8]=dp[0]+1
+        #dp[i] = dp[i-offset]+1, offset will change inly when offset x 2 == i, then it will be same like 2, 4, 8, 16
         
+        offset = 1
+        dp = [0]*(n+1)
+        for i in range(n+1):
+            if offset * 2 == i:
+                offset = offset * 2
+            dp[i] = dp[i-offset] + 1
+        return dp
         
         
         
