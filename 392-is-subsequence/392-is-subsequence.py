55fed42cf25fdf5189d9e95bc487f73fd79bedfa
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-02 13:51:42 +0530

    Time: 126 ms (5.02%), Space: 14 MB (55.23%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -18,14 +18,17 @@ class Solution:
     #lcs of two string cud be 0 to min(s1,s2), this case we ll find lcs and match its size with s1 if yes then return true
         m = len(s)
         n = len(t)
-        tt = [[0]*(n+1) for _ in range(m+1)]
-
+        prev = [0]*(n+1)
+        
+        
         for i in range(1, m + 1):
+            cur = [0]*(n+1)
             for j in range(1, n + 1):
                 if s[i-1] == t[j-1]:
-                    tt[i][j] = tt[i-1][j-1] + 1   
+                    cur[j] = prev[j-1] + 1   
                 else:
-                    tt[i][j] = max(tt[i][j - 1], tt[i - 1][j])
-                    
-        lcs_ans = tt[m][n]
+                    cur[j] = max(cur[j - 1], prev[j])
+            prev = cur
+            
+        lcs_ans = prev[-1]
         return lcs_ans == len(s)
