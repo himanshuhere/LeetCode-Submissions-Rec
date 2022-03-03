97b618bbea7f954ce1ece3a6ac18daf127dd0087
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-03 10:37:01 +0530

    Time: 57 ms (45.42%), Space: 14.1 MB (75.02%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.py
+++ 413-arithmetic-slices/413-arithmetic-slices.py
@@ -5,11 +5,31 @@ class Solution:
             return 0
         
         dp = [0]*n
-        #base case setting
-        if nums[1]-nums[0] == nums[2]-nums[1]:
-            dp[2] = 1
+#         #base case setting
+#         if nums[1]-nums[0] == nums[2]-nums[1]:
+#             dp[2] = 1
         
-        for i in range(3, n):
+#         for i in range(3, n):
+#             if nums[i]-nums[i-1]==nums[i-1]-nums[i-2]:
+#                 dp[i] = dp[i-1] + 1
+#         return sum(dp)
+        
+        #short optimized
+        res= 0
+        for i in range(2, n):
             if nums[i]-nums[i-1]==nums[i-1]-nums[i-2]:
                 dp[i] = dp[i-1] + 1
-        return sum(dp)
+            res+=dp[i]
+        return res
+    
+    
+    #dp[i] is the number of valid arithmetic seq till nums[i]. 
+    #so if till i-1, seq were dp[i-1], then as per condition if we found one more, so add one more.
+    
+    #Let dp[i] denote the number of arithmetic subarray ends at nums[i].
+# If if nums[i-1] - nums[i-2] == nums[i] - nums[i-1] then we can form the Arithmetic subarray ends at nums[i].
+# So dp[i] = dp[i-1] + 1.
+# For example: nums = [1, 3, 5, 7, 9]
+# dp[2] = 1 arithmetic subarrays are {1, 3, 5}
+# dp[3] = dp[2] + 1 = 2, arithmetic subarrays are {1, 3, 5, 7}, {3, 5, 7}
+# dp[4] = dp[3] + 1 = 3, arithmetic subarrays are {1, 3, 5, 7, 9}, {3, 5, 7, 9}, {5, 7, 9}
