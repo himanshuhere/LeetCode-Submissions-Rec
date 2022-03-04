91deda1d1f9901e293eb8a70e9fd0325f6f31484
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-04 22:15:44 +0530

    Time: 241 ms (27.67%), Space: 14.5 MB (20.39%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.py
+++ 799-champagne-tower/799-champagne-tower.py
@@ -1,6 +1,8 @@
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
-        arr = [[0]*101 for _ in range(101)]
+        #arr = [[0]*101 for _ in range(101)]
+        arr = [[0]*(query_glass+2) for _ in range(query_row+1)]
+        
         arr[0][0] = poured
         for i in range(query_row):      #one level less is fine, we already calcuting next row
             for j in range(query_glass+1):  #but col we need to reach
