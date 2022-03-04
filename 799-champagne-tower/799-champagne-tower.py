1618e91804e7f00bfa5298e0257d76f04e23de57
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-04 22:12:09 +0530

    Time: 219 ms (33.01%), Space: 14.2 MB (56.31%) - LeetHub

---
+++ 799-champagne-tower/799-champagne-tower.py
@@ -0,0 +1,11 @@
+class Solution:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        arr = [[0]*101 for _ in range(101)]
+        arr[0][0] = poured
+        for i in range(query_row):
+            for j in range(query_glass+1):
+                rem = max(arr[i][j] - 1, 0)
+                arr[i+1][j] += rem/2
+                arr[i+1][j+1] += rem/2
+        ans = arr[query_row][query_glass] 
+        return ans if ans <= 1 else 1
