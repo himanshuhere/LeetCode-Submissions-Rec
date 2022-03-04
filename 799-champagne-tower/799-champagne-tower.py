58b6874e808c85aafab78f4537c5a707f8c2ea83
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-04 22:12:59 +0530

    Time: 371 ms (12.14%), Space: 14.1 MB (56.31%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.py
+++ 799-champagne-tower/799-champagne-tower.py
@@ -3,8 +3,8 @@ class Solution:
         arr = [[0]*101 for _ in range(101)]
         arr[0][0] = poured
         for i in range(query_row):      #one level less is fine, we already calcuting next row
-            for j in range(query_glass+1):
-                rem = max(arr[i][j] - 1, 0)
+            for j in range(query_glass+1):  #but col we need to reach
+                rem = max(arr[i][j] - 1, 0) 
                 arr[i+1][j] += rem/2
                 arr[i+1][j+1] += rem/2
         ans = arr[query_row][query_glass] 
