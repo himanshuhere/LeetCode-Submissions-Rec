1b9ead0780d23203b66d8656217094012e910dcf
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-05 21:48:39 +0530

    Time: 3065 ms (11.11%), Space: 49.1 MB (77.78%) - LeetHub

---
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -0,0 +1,39 @@
+class Solution:
+    def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+        #need direct ancestor plus unke b ancestor, either go kahn and recusive to parent or this
+        g = defaultdict(list)
+        ind = [0]*n
+        for u , v in edges:
+            g[u].append(v)
+            ind[v] += 1
+        
+        q = deque()
+        for i in range(n):
+            if ind[i] == 0:
+                q.append(i)
+                
+        ind2 = copy.deepcopy(ind)
+        q2 = copy.deepcopy(q)
+        
+        ans = [set() for _ in range(n)]
+        
+        while q:
+            node = q.popleft()
+            for k in g[node]:
+                ans[k].add(node)         #direct parent
+                ind[k] -= 1
+                if ind[k] == 0:
+                    q.append(k)
+        
+        while q2:
+            node = q2.popleft()
+            for k in g[node]:
+                for p in set(ans[k]):
+                    for v in ans[p]:
+                        ans[k].add(v)
+                ind2[k] -= 1
+                if ind2[k] == 0:
+                    q2.append(k)
+                    
+        return [sorted(row) for row in ans]
+        
