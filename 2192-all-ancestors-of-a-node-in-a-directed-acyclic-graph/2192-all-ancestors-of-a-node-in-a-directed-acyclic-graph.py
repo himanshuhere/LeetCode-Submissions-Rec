a1ccbf5130dc6d0eeefc2b80983ce42c30c3315a
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-05 22:09:32 +0530

    Time: 1757 ms (11.11%), Space: 57.5 MB (22.22%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -1,6 +1,29 @@
 class Solution:
     def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
-        def contesttime():
+        #rough copy me bahut sahi samjh ayega actually slow hai but understandable h
+        #hint - says reverse graph - transpose graph and now run dfs on every ele fuckkkkk pehle ku nhi aya dimag me
+        #kabhi b graph me upar ki or jana ho to hosiyar me parent store mat karo reverse karo edges ko bc, unless asked to not modify the graph shit
+        
+        g = defaultdict(list)
+        for u, v in edges:
+            g[v].append(u)
+            
+        def dfs(node, root):
+            vis[node] = 1
+            for k in g[node]:
+                if vis[k]==0:
+                    ans[root].add(k)
+                    dfs(k, root)
+        
+        ans = [set() for _ in range(n)]
+        for i in range(n):
+            vis=[0]*n
+            dfs(i, i)
+        return [sorted(row) for row in ans]
+    
+    
+    #see the contest time wala intuitive 
+    def contesttime():
             #need direct ancestor plus unke b ancestor, either go kahn and recusive to parent or this
             g = defaultdict(list)
             ind = [0]*n
@@ -36,26 +59,4 @@ class Solution:
                         q2.append(k)
 
             return [sorted(row) for row in ans]
-
-        #rough copy me bahut sahi samjh ayega actually slow hai but understandable h
-        
-        #hint - says reverse graph - transpose graph and now run dfs on every ele fuckkkkk pehle ku nhi aya dimag me
-        #kabhi b graph me upar ki or jana ho to hosiyar me parent store mat karo reverse karo edges ko bc, unless asked to not modify the graph shit
-        
-        g = defaultdict(list)
-        for u, v in edges:
-            g[v].append(u)
-            
-        def dfs(node, root):
-            vis[node] = 1
-            for k in g[node]:
-                if vis[k]==0:
-                    ans[root].add(k)
-                    dfs(k, root)
-        
-        ans = [set() for _ in range(n)]
-        for i in range(n):
-            vis=[0]*n
-            dfs(i, i)
-        return [sorted(row) for row in ans]
         
