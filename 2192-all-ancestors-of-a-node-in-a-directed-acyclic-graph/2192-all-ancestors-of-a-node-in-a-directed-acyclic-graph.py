902a18aeaf1789264d41886df1215aa752a959ed
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-05 22:00:00 +0530

    Time: 1367 ms (11.11%), Space: 57.5 MB (22.22%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -1,40 +1,61 @@
 class Solution:
     def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
-        #need direct ancestor plus unke b ancestor, either go kahn and recusive to parent or this
+        def contesttime():
+            #need direct ancestor plus unke b ancestor, either go kahn and recusive to parent or this
+            g = defaultdict(list)
+            ind = [0]*n
+            for u , v in edges:
+                g[u].append(v)
+                ind[v] += 1
+
+            q = deque()
+            for i in range(n):
+                if ind[i] == 0:
+                    q.append(i)
+
+            ind2 = copy.deepcopy(ind)
+            q2 = copy.deepcopy(q)
+            ans = [set() for _ in range(n)]
+
+            while q:
+                node = q.popleft()
+                for k in g[node]:
+                    ans[k].add(node)         #direct parent
+                    ind[k] -= 1
+                    if ind[k] == 0:
+                        q.append(k)
+
+            while q2:
+                node = q2.popleft()
+                for k in g[node]:
+                    for p in set(ans[k]):           #set karna jaruri hai bc acccept nhi hua contest me
+                        for v in ans[p]:
+                            ans[k].add(v)
+                    ind2[k] -= 1
+                    if ind2[k] == 0:
+                        q2.append(k)
+
+            return [sorted(row) for row in ans]
+
+        #rough copy me bahut sahi samjh ayega actually slow hai but understandable h
+        
+        #hint - says reverse graph - transpose graph and now run dfs on every ele fuckkkkk pehle ku nhi aya dimag me
+        #kabhi b graph me upar ki or jana ho to hosiyar me parent store mat karo reverse karo edges ko bc, unless asked to not modify the graph shit
+        
         g = defaultdict(list)
-        ind = [0]*n
-        for u , v in edges:
-            g[u].append(v)
-            ind[v] += 1
+        for u, v in edges:
+            g[v].append(u)
+            
+        def dfs(node, root):
+            vis[node] = 1
+            for k in g[node]:
+                if vis[k]==0:
+                    ans[root].add(k)
+                    dfs(k, root)
         
-        q = deque()
-        for i in range(n):
-            if ind[i] == 0:
-                q.append(i)
-                
-        ind2 = copy.deepcopy(ind)
-        q2 = copy.deepcopy(q)
         ans = [set() for _ in range(n)]
-        
-        while q:
-            node = q.popleft()
-            for k in g[node]:
-                ans[k].add(node)         #direct parent
-                ind[k] -= 1
-                if ind[k] == 0:
-                    q.append(k)
-        
-        while q2:
-            node = q2.popleft()
-            for k in g[node]:
-                for p in set(ans[k]):           #set karna jaruri hai bc acccept nhi hua contest me
-                    for v in ans[p]:
-                        ans[k].add(v)
-                ind2[k] -= 1
-                if ind2[k] == 0:
-                    q2.append(k)
-                    
+        for i in range(n):
+            vis=[0]*n
+            dfs(i, i)
         return [sorted(row) for row in ans]
-    
-    #rough copy me bahut sahi samjh ayega actually slow hai but understandable h
         
