4e3c7a9acc2a63c1c642c6508ebb8cce3164a27b
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-07 10:41:37 +0530

    Time: 76 ms (72.02%), Space: 19.3 MB (5.59%) - LeetHub

--- 155-min-stack/155-min-stack.py
+++ 155-min-stack/155-min-stack.py
@@ -1,33 +1,33 @@
-# class MinStack:
-#     #Approach - two stack to one stack to LinkedList
+class MinStack:
+    #Approach - two stack to one stack to LinkedList
         
-#     def __init__(self): #same thing u can do with LL
-#         self.stack = []
-#         self.minStack = []
+    def __init__(self): #same thing u can do with LL
+        self.stack = []
+        self.minStack = []
     
-#     def push(self, x):
-#         self.stack.append(x)
-#         if len(self.minStack) == 0 or self.minStack[-1] >= x:
-#             self.minStack.append(x)
+    def push(self, x):
+        self.stack.append(x)
+        if len(self.minStack) == 0 or self.minStack[-1] >= x:
+            self.minStack.append(x)
             
 
-#     def pop(self):
-#         if len(self.stack) == 0:
-#             return
-#         x = self.stack[-1]
-#         self.stack.pop()
-#         if x == self.minStack[-1]:
-#             self.minStack.pop()
+    def pop(self):
+        if len(self.stack) == 0:
+            return
+        x = self.stack[-1]
+        self.stack.pop()
+        if x == self.minStack[-1]:
+            self.minStack.pop()
 
-#     def top(self):
-#         if len(self.stack) == 0:
-#             return -1
-#         return self.stack[-1]
+    def top(self):
+        if len(self.stack) == 0:
+            return -1
+        return self.stack[-1]
 
-#     def getMin(self):
-#         if len(self.minStack) == 0:
-#             return -1
-#         return self.minStack[-1]
+    def getMin(self):
+        if len(self.minStack) == 0:
+            return -1
+        return self.minStack[-1]
 
 #Linked List
 class Node:
