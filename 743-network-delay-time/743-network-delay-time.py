840e7bb2d180225828d63e9df95a0aadaeeff057
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-07 21:47:20 +0530

    Time: 1025 ms (12.95%), Space: 16.9 MB (12.17%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -1,6 +1,6 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, src: int) -> int:
-        #pls try dijktra
+        #Dijktra
         g = defaultdict(list)
         for u, v, w in times:
             g[u].append((v, w))
@@ -18,13 +18,3 @@ class Solution:
         return max(dist[1:]) if max(dist[1:]) < float("inf") else -1
     
         
-#         dist = [float("inf") for _ in range(n)]
-#         dist[k-1] = 0
-        
-#         for _ in range(n-1):
-#             for u, v, w in times:
-#                 if dist[u-1] + w < dist[v-1]:
-#                     dist[v-1] = dist[u-1] + w
-                    
-#         return max(dist) if max(dist) < float("inf") else -1
-#         #ofc dijktra would be more nice if -ve weights are not here, i guess not. 
