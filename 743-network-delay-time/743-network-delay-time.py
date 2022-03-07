25858eae8e5f9d596c396487139232bfa1b4fde8
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-07 21:47:07 +0530

    Time: 697 ms (46.12%), Space: 16.7 MB (20.56%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -1,14 +1,30 @@
 class Solution:
-    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+    def networkDelayTime(self, times: List[List[int]], n: int, src: int) -> int:
         #pls try dijktra
+        g = defaultdict(list)
+        for u, v, w in times:
+            g[u].append((v, w))
+
+        dist = [float("inf")]*(n+1)
+        dist[src] = 0
+        h = [(0, src)]
+        while h:
+            cost, u = heappop(h)
+            for k, w in g[u]:
+                if cost + w < dist[k]:
+                    dist[k] = cost + w
+                    heappush(h, (dist[k], k))
         
-        dist = [float("inf") for _ in range(n)]
-        dist[k-1] = 0
+        return max(dist[1:]) if max(dist[1:]) < float("inf") else -1
+    
         
-        for _ in range(n-1):
-            for u, v, w in times:
-                if dist[u-1] + w < dist[v-1]:
-                    dist[v-1] = dist[u-1] + w
+#         dist = [float("inf") for _ in range(n)]
+#         dist[k-1] = 0
+        
+#         for _ in range(n-1):
+#             for u, v, w in times:
+#                 if dist[u-1] + w < dist[v-1]:
+#                     dist[v-1] = dist[u-1] + w
                     
-        return max(dist) if max(dist) < float("inf") else -1
-        #ofc dijktra would be more nice if -ve weights are not here, i guess not. 
+#         return max(dist) if max(dist) < float("inf") else -1
+#         #ofc dijktra would be more nice if -ve weights are not here, i guess not. 
