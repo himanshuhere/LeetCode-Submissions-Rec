54399e71e761636a49cef85873e2183e80737678
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-10 10:59:21 +0530

    Time: 56 ms (17.01%), Space: 14 MB (49.98%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.py
@@ -43,3 +43,7 @@ class MyQueue:
 
     def empty(self):
         return not self.s1 and not self.s2
+    
+    #Amorized o(1) means one operation may take n time but for overall cals it might give you o(1) result. think like if we do all 5 push then 4 pop, since on first pop, we have put s1 data to s2 and return s2.top, now for second third so on calls POP we ll direct return s2.pop, thats y first called peek. Peek returns the last element not pop, thus second line to pop that element.
+    #On long time span that can be ref as constant time thus amortized o(1). Then again for all push we do in s1 and will use s2 until it is empty then again fill s2 with s1 bruh.
+    #THIS IS A BEAUTIFUL ALGO AND EXPLANATION FOR AMORTIMZED CONSTANT
