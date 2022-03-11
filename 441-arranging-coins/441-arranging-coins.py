4369e136ef0583f608a71698bad2976cf2f257aa
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-11 10:37:28 +0530

    Time: 44 ms (82.59%), Space: 14 MB (39.63%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.py
+++ 441-arranging-coins/441-arranging-coins.py
@@ -1,8 +1,27 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        row = 1
-        while n >= row:
-            n -= row
-            row += 1
-        return row-1
+        # #1 Linear
+        # row = 1
+        # while n >= row:
+        #     n -= row
+        #     row += 1
+        # return row-1
+        
+        #2 Binary Search 
+        # range is sorted 1-n
+        # 1 + 2 + 3 + 4 ... k = k*(k+1)/2, thus for kth row we need that formula coins
+        
+        lo, hi = 1, n
+        ans = 0
+        while lo <= hi:
+            row = (lo+hi) >> 1
+            coinsneed = row*(row+1)//2
+            
+            if coinsneed <= n:
+                ans = row
+                lo = row + 1
+            else:
+                hi = row - 1
+        return ans
+            
         
