16c2fcc4eee36aaf405ed4fc0944bfdf555d09a8
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-11 12:27:38 +0530

    Time: 28 ms (93.99%), Space: 13.8 MB (97.32%) - LeetHub

--- 168-excel-sheet-column-title/168-excel-sheet-column-title.py
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.py
@@ -3,7 +3,7 @@ class Solution:
         res = ""
         n = columnNumber
         while n:
-            n-=1
+            n-=1            #offset as here A-1 not A-0, yes observation nothing logical
             c = chr(n%26 + ord('A'))     
             res += c
             n = n//26
