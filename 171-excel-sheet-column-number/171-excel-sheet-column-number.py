eaa27467add3e29302875c31538b27a7e0c7946e
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-11 12:17:36 +0530

    Time: 36 ms (83.87%), Space: 13.9 MB (62.87%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.py
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.py
@@ -6,6 +6,11 @@ class Solution:
         return res
     
     
+    #any strting(coulb be of any base X), say algo just need to change base to X
+    #(ord(ch) - ord('A') + 1) beacuse treating A=1, no logic just observation that this ques can be solved only this way else A shold be 0 but ans would diff ans (ord(ch) - ord('A'))
+    
+    
+    
     
     # It is like converting a decimal number to base26 except there is no zero in that we need to subtract 1 from n to make that adjustment to get the answer we get from convert a normal decimal number to any base.
 # Time: O(k) - k is the numer of digits in the input
