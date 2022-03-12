c8a811e46ae8f33716097b29cf54f05005152173
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-12 13:49:38 +0530

    Time: 710 ms (57.77%), Space: 70.5 MB (66.61%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.py
+++ 115-distinct-subsequences/115-distinct-subsequences.py
@@ -4,16 +4,42 @@ class Solution:
         #two string, keep an index on both
         m, n = len(s), len(t)
         
-        @lru_cache(None)
-        def f(i, j):
-            if j >= n:      #pehle i ke liye likh rha tha ans hi nhi ayega, apan ko bas ye dekhna hai j exhauset hua hai mtlb we have matched all t, kuki apan j+1 matching wakt hi kar rhe else wahi khada rahega
-                return 1
-            if i >= m:
-                return 0
+#         @lru_cache(None)
+#         def f(i, j):
+#             if j >= n:      #pehle i ke liye likh rha tha ans hi nhi ayega, apan ko bas ye dekhna hai j exhauset hua hai mtlb we have matched all t, kuki apan j+1 matching wakt hi kar rhe else wahi khada rahega
+#                 return 1
+#             if i >= m:
+#                 return 0
             
-            if s[i] == t[j]:
-                return f(i+1, j) + f(i+1, j+1)
-            else:
-                return f(i+1, j)
+#             if s[i] == t[j]:
+#                 return f(i+1, j) + f(i+1, j+1)
+#             else:
+#                 return f(i+1, j)
         
-        return f(0, 0)
+#         return f(0, 0)
+#     #recurions - exponential/O(m+n) aux space
+#     #memoized  - O(m*n)/O(m*n) + O(m+n)
+#     #tab - o(m*n)/O(m*n)
+        
+#         #See kahani
+#         #1
+#         def f(i, j):
+#             if j >= n:    return 1
+#             if i >= m:    return 0
+#             if s[i] == t[j]:
+#                 return f(i-1, j) + f(i-1, j-1)
+#             else:
+#                 return f(i-1, j)
+#         return(m-1, n-1)
+        
+        #2
+        dp = [[0]*(n+1) for _ in range(m+1)]
+        for i in range(m+1):
+            dp[i][0] = 1
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if s[i-1] == t[j-1]:
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j-1]
+                else:
+                    dp[i][j] = dp[i-1][j]
+        return dp[-1][-1]
