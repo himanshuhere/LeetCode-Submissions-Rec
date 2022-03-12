4dd07b3f365164cf4435a1b9fafdc0f82c12e67d
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-12 11:28:42 +0530

    Time: 62 ms (28.88%), Space: 15 MB (29.48%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
@@ -57,7 +57,7 @@ class Solution:
             head2.next = head1.next.next
             head1 = head1.next
             head2 = head2.next
-        #head1.next = None
+        head1.next = None       #not needed actually, but still good practice
         
         return secondHead
     
