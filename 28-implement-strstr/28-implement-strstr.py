ec3885c909f9fc225a05963b53663a60bbf8ec21
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-13 22:32:06 +0530

    Time: 393 ms (5.10%), Space: 14.2 MB (40.13%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -10,7 +10,7 @@ class Solution:
 #         return -1
 
         #2
-        return haystack.find(needle)
+        #return haystack.find(needle)
 
         #Rabin karp
         
@@ -19,24 +19,12 @@ class Solution:
         d = 26      #depends on data space, we have only small lettes cud cover in 26
 
         powr=m-1
-        texth = 0
-        patth = 0
-        for i in range(m-1, -1, -1):
-            patth += ((ord(patt[i])*(d**powr))%p)      #cud have taken ord(ch)-ord('a') also
-            texth += ((ord(text[i])*(d**powr))%p)
-            powr-=1
+        patth = hash(patt)
         
         for i in range(len(text)-m+1):
-            if patth == texth:
+            if patth == hash(text[i:i+m]):
                 if text[i:i+m] == patt:
                     return i
-            else:
-                if i == len(text)-m:
-                    return -1
-                
-                texth -= ((ord(text[i])*(d**(m-1)))%p)
-                texth *= d          #base shift to left one
-                texth += ((ord(text[i+m])*(d**0))%p)
         return -1
             
         
