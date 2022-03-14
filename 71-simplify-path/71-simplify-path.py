58d59ec612656458cca13ca23ecdeb3bbfc3048f
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-14 10:13:02 +0530

    Time: 57 ms (28.28%), Space: 14 MB (57.22%) - LeetHub

--- 71-simplify-path/71-simplify-path.py
+++ 71-simplify-path/71-simplify-path.py
@@ -2,7 +2,6 @@ class Solution:
     def simplifyPath(self, path: str) -> str:
         path = path.split('/')
         st = []
-        
         for p in path:
             if p == "" or p == ".":
                 continue
@@ -11,5 +10,10 @@ class Solution:
                     st.pop()
             else:
                 st.append(p)
-                
         return "/" + "/".join(st)
+
+    
+    #Whole idea is that, if we find "." so we ll jump on current directory thus, just ignore it. Do not add to valid path. and split /// will result in "" thus ignore these too
+    #If "..", thus we need to jump current + previous dir, thus only because of this we need STACK, else kaam normal string se ho jana tha. Baki ignore "/////" etc.
+        #bhery easzy
+        #ONLY Thing needs to handle is .. else baki flow hai sidha l to r
