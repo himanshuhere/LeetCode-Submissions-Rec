f3781239400286596f52251e75134ed4471eeff3
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-14 10:18:37 +0530

    Time: 63 ms (17.11%), Space: 14 MB (32.22%) - LeetHub

--- 71-simplify-path/71-simplify-path.py
+++ 71-simplify-path/71-simplify-path.py
@@ -1,19 +1,37 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        path = path.split('/')
-        st = []
-        for p in path:
-            if p == "" or p == ".":
-                continue
-            if p == "..":
-                if st:
-                    st.pop()
-            else:
-                st.append(p)
-        return "/" + "/".join(st)
+        # path = path.split('/')
+        # st = []
+        # for p in path:
+        #     if p == "" or p == ".":
+        #         continue
+        #     if p == "..":
+        #         if st:
+        #             st.pop()
+        #     else:
+        #         st.append(p)
+        # return "/" + "/".join(st)
 
     
     #Whole idea is that, if we find "." so we ll jump on current directory thus, just ignore it. Do not add to valid path. and split /// will result in "" thus ignore these too
     #If "..", thus we need to jump current + previous dir, thus only because of this we need STACK, else kaam normal string se ho jana tha. Baki ignore "/////" etc.
         #bhery easzy
         #ONLY Thing needs to handle is .. else baki flow hai sidha l to r
+        
+        st=[]
+        i=0
+        while i< len(path):
+            p=""
+            while i<len(path) and path[i]!='/':
+                p+=path[i]
+                i+=1
+            if p=="" or p==".":
+                pass
+            elif p=="..":
+                if st:  st.pop()
+            else:
+                st.append(p)
+            i+=1
+        return '/' + '/'.join(st)
+                
+        
