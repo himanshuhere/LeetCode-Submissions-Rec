00b8e77cab1f6f2c8367f8b5f485f8d9a24072fa
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-15 11:50:52 +0530

    Time: 1003 ms (47.37%), Space: 131.9 MB (13.94%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.py
+++ 44-wildcard-matching/44-wildcard-matching.py
@@ -25,4 +25,15 @@ class Solution:
                 return False            #straight not matching and no ?, *
             
         return f(len(s)-1, len(p)-1)
+    
+    
+    #SEE,
+#     First, if MATCHING (ALSO INCLUDES ? CASE):
+#         Simply shrink as matches
+
+#     Second, if patter has *, so since * can ba matchew with 0 to n letters, thus we can match it with "" or in coding case we can skip * and we can match start with 1 or more so what looping no we are doing recursion anyway so better match with one char and move further putting start at same location. Every call have two branch in star case, first branch will leave star and ceond will leave other string char, both combines can help in skipp the n number of chars and go start matching with others.
+
+#     Third, if no ? or *, and chars dont match just return False
+
+# NOW, COMPLICATES BASE CASES, trying some cases you will get that both should exhausted together to return true, if any one of then exhausted before other return False, but wait wait wait there is one wildcard which helps in matching with empty string yes *, so better break two cases if patter is empty and string is remaining, no chance return False, but for vice versa we ONLY need *, ? cam match with char not empty thats why check for all *, if all * return True else return False
                 
