5518db5eabcb60f81f9eaaccd72cdfd30d31d04a
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-15 12:13:41 +0530

    Time: 865 ms (58.42%), Space: 22.5 MB (58.28%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.py
+++ 44-wildcard-matching/44-wildcard-matching.py
@@ -39,28 +39,57 @@ class Solution:
             
     
         #Now TABULATION, so lets first make it 1-based indexing carefully pls
-        @lru_cache(None)
-        def f(i, j):
-            if i == 0 and j == 0:
-                return True
-            if j == 0 and i > 0:    #if pattern ends, no chance
-                return False
+#         @lru_cache(None)
+#         def f(i, j):
+#             if i == 0 and j == 0:
+#                 return True
+#             if j == 0 and i > 0:    #if pattern ends, no chance
+#                 return False
             
-            if i == 0 and j > 0:    #only if all asterick *, else not possible like , "", "*****"
-                for k in range(1, j+1):
-                    if p[k-1] != '*':
-                        return False
-                return True
+#             if i == 0 and j > 0:    #only if all asterick *, else not possible like , "", "*****"
+#                 for k in range(1, j+1):
+#                     if p[k-1] != '*':
+#                         return False
+#                 return True
                 
             
-            if s[i-1] == p[j-1] or p[j-1] == '?':
-                return f(i-1, j-1)
+#             if s[i-1] == p[j-1] or p[j-1] == '?':
+#                 return f(i-1, j-1)
             
-            elif p[j-1] == '*':
-                return f(i, j-1) or f(i-1, j)
-            else:
-                return False            #straight not matching and no ?, *
+#             elif p[j-1] == '*':
+#                 return f(i, j-1) or f(i-1, j)
+#             else:
+#                 return False            #straight not matching and no ?, *
             
-        return f(len(s), len(p))
+#         return f(len(s), len(p))
+        
+        m, n = len(s), len(p)
+        dp = [[False]*(n+1) for _ in range(m+1)]
+        dp[0][0] = True
+        
+        for i in range(1, m+1):
+            dp[i][0] = False
+        
+        for j in range(1, n+1):
+            flag = True
+            for k in range(1, j+1):
+                if p[k-1] != '*':
+                    flag = False
+                    break
+            dp[0][j] = flag
+        
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if s[i-1] == p[j-1] or p[j-1] == '?':
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j-1] == '*':
+                    dp[i][j] = dp[i][j-1] or dp[i-1][j]
+                else:
+                    dp[i][j] = False   
+        return dp[-1][-1]
+        
+    
+        
+        
         
                 
