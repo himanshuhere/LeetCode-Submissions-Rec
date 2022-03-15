8ca27c4e806ae1b3266ae723c8606cd8b853d2b3
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-15 13:53:29 +0530

    Time: 136 ms (65.73%), Space: 15.4 MB (86.76%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -1,7 +1,7 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         #forward
-        ans = []
+        ans = ""
         op, cl = 0, 0
         for ch in s:
             if ch == '(':
@@ -12,11 +12,11 @@ class Solution:
             if cl > op:
                 cl -= 1
             else:
-                #ans += ch       #this is n opertation. if want use string arr to get o(1)
-                ans.append(ch)
+                ans += ch       #this is n opertation. if want use string arr to get o(1)
+
         #backward
-        s = ''.join(ans)
-        ans = []
+        s = ans
+        ans = ""
         op, cl = 0, 0
         for ch in s[::-1]:
             if ch == '(':
@@ -27,6 +27,6 @@ class Solution:
             if cl > op:
                 cl -= 1
             else:
-                ans.append(ch)  
+                ans += ch 
                 
-        return ''.join(ans[::-1])
+        return ans[::-1]
