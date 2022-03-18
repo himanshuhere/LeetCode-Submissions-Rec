f88f44c01d906437b66467ad45a76c6bcdb5a935
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-18 16:24:20 +0530

    Time: 40 ms (85.62%), Space: 14.3 MB (32.47%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
@@ -1,23 +1,33 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
-        self.ans = 0
-        def dfs(root, cur):
-            if not root:
-                return
-            if not root.left and not root.right:
-                cur += str(root.val)
-                self.ans += int(cur, 2)
-                return
-            cur += str(root.val)
-            dfs(root.left, cur)
-            dfs(root.right, cur)
+        #Brute 
+#         self.ans = 0
+#         def dfs(root, cur):
+#             if not root:
+#                 return
+#             if not root.left and not root.right:
+#                 cur += str(root.val)
+#                 self.ans += int(cur, 2)
+#                 return
+#             cur += str(root.val)
+#             dfs(root.left, cur)
+#             dfs(root.right, cur)
         
-        dfs(root, "")
-        return self.ans
+#         dfs(root, "")
+#         return self.ans
+    
+        #REAL SMOOTH SHIT - test, it works like really butter the binary thing 
+        def dfs(root, sum_):
+            if not root:
+                return 0
+            
+            sum_ = sum_ * 2 + root.val          #works so good, try a test case
+            
+            if not root.left and not root.right:
+                return sum_
+            return dfs(root.left, sum_) + dfs(root.right, sum_)
+        
+        return dfs(root, 0)
+        
+    
                 
