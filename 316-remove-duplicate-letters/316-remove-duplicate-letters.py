ccfca4f13563920faa50411b916a477265872e4d
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-18 23:28:34 +0530

    Time: 57 ms (46.33%), Space: 14 MB (29.21%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.py
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.py
@@ -19,3 +19,11 @@ class Solution:
             seen[ord(ch)-ord('a')] = True
 
         return ''.join(st)
+    
+#     "now try to add "b" in the stack and mark it as true, for this since the current_element("b") is less than
+# the peek(top) element of stack("d")so we should pop the "d" for now and push "b" in stack but before 
+# poping "d" check is there "d" is again persent in given string after this "b" , since "d" is not more 
+# persent after this "b" so we can't do that pop and push operation for "d" and "b" respectively , simply 
+# add "b" into the stack and mark is as true that's set, but in some case if "d" is available again after 
+# that "b" then we will definitely be poping "d" from the stack and adding "b" into the stack( we can take 
+# example for this case as "cbadcbcd" and try to dry run it )
