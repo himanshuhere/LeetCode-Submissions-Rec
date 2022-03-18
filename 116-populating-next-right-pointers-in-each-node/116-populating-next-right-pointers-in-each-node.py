880bfe9aff06c333be2f9b7a2b9553f8c281bb5f
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-18 23:07:08 +0530

    Time: 104 ms (32.73%), Space: 15.8 MB (21.03%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
@@ -2,10 +2,11 @@ class Solution:
     def connect(self, root: 'Node') -> 'Node':
         #easy peasy but hard on LC IB so see pepcoding video pls pls noice ques and acha 
         #or do BFS and same logic instead of external while do bfs and internal logic same i meant think lil. Bas you have to think of attaching links while u on root so rrot then attach left and right dont think like u r on same level and do same level links using parent no, on parent connect childs then go childs
-       
+        if not root:
+            return None
         #1
         head = root
-        while head and head.left:
+        while head.left:
             x = head
             while x:
                 x.left.next = x.right
