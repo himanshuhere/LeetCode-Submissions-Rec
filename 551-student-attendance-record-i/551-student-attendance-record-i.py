c71ec1d34de18ea624370a55a443cc9ff2413095
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-21 16:56:24 +0530

    Time: 24 ms (98.54%), Space: 13.8 MB (66.34%) - LeetHub

--- 551-student-attendance-record-i/551-student-attendance-record-i.py
+++ 551-student-attendance-record-i/551-student-attendance-record-i.py
@@ -4,10 +4,12 @@ class Solution:
         for ch in s:
             if ch == 'A':
                 ab += 1
+                
             if ch == 'L':
                 late += 1
             else:
                 late = 0
+                
             if ab >= 2 or late > 2:
                 return False
         return True
