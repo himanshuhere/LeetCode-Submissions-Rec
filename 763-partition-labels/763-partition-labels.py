dbeb7d63f1b420d684a8eac5e73435cb2a9326d6
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-21 11:03:41 +0530

    Time: 40 ms (92.01%), Space: 14 MB (36.30%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.py
@@ -0,0 +1,20 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        lastocc = [0]*26
+        for i, ch in enumerate(s):
+            lastocc[ord(ch)-ord('a')] = i
+            
+        ans = [0]
+        n = len(s)
+        i = 0
+        while i < n:
+            last = lastocc[ord(s[i])-ord('a')] 
+            j = i
+            while j < n and j <= last:
+                if lastocc[ord(s[j])-ord('a')] > last:
+                    last = lastocc[ord(s[j])-ord('a')]
+                j += 1            
+            pre = sum(ans)
+            ans.append((last-pre)+1)
+            i = j
+        return ans[1:]
