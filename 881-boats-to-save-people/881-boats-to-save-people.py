c72ceef6e301d5958090e57c36645567b58ff941
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-24 12:19:44 +0530

    Time: 519 ms (72.27%), Space: 20.8 MB (80.59%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.py
+++ 881-boats-to-save-people/881-boats-to-save-people.py
@@ -1,19 +1,20 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        #Two pointer 
+        #bhcnho kese mene adhe min me ek bar me bana diya solution
+        #kabhi kabhi khud pe bharosa nhi hota
+        #sorting+two pointer
         
         A = sorted(people)
-        ans = 0
-        i, j = 0, len(A)-1
-        while i <= j:
-            if A[i]+A[j] > limit:
-                if A[i]>A[j]:
-                    i += 1
-                else:
-                    j -= 1
+        l, r = 0, len(A)-1
+        boats = 0 
+        while l <= r:
+            weights = A[l]+A[r]
+            if weights > limit:
+                r -= 1                  #bcs surely r is max than l, greedly fill boat with r
             else:
-                i += 1
-                j -= 1
+                l += 1
+                r -= 1
                 
-            ans += 1
-        return ans
+            boats += 1
+        return boats
+                
