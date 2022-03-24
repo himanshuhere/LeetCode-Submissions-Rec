2ed3aa0315b14bd0f2431efc4417bd81b6c03e68
Himanshu Soni <himanshusoni3105@outlook.com>
2022-03-24 12:15:59 +0530

    Time: 464 ms (88.54%), Space: 20.9 MB (34.38%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.py
+++ 881-boats-to-save-people/881-boats-to-save-people.py
@@ -1,21 +1,17 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        #bhcnho kese mene adhe min me ek bar me bana diya solution
-        #kabhi kabhi khud pe bharosa nhi hota
-        #sorting+two pointer
-        
-        #atmost two people on boats, thus either l and r or only r
         A = sorted(people)
-        l, r = 0, len(A)-1
-        boats=0
-        while l<=r:
-            weights = A[l]+A[r]
-            if weights <= limit:
-                l+=1
-                r-=1
+        ans = 0
+        i, j = 0, len(A)-1
+        while i <= j:
+            if A[i]+A[j] > limit:
+                if A[i]>A[j]:
+                    i += 1
+                else:
+                    j -= 1
             else:
-                r-=1
-            boats+=1
-        return boats
+                i += 1
+                j -= 1
                 
-        #at last, l==r for odd people. weights of same person will get added and fark nhi pdta solution will not get affected as whatever answer will be one boat is going to go. so fine. else u can write one condition l==r, boats++
+            ans += 1
+        return ans
