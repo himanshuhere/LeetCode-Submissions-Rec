a7858772c06f1234e4e773587b4e9485e9ee19fd
Himanshu Soni <himanshusoni3105@outlook.com>
2022-04-01 00:02:59 +0530

    Time: 5868 ms (5.02%), Space: 22.5 MB (9.50%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.py
+++ 410-split-array-largest-sum/410-split-array-largest-sum.py
@@ -1,27 +1,34 @@
 class Solution:
     def splitArray(self, nums: List[int], m: int) -> int:
-        #BINARY SEARCH ON ANSWERS. How d you figured it out actually i dint. was takig striver masterclass and he did this ques. but for sure one hack i found or say observation or pattter. No for binary search on answer, but for this feasible or black box function what you call. See allocate books also asked "MAXIMIZE THE MINIMUM values somehtins" OR "MINIMIZE THE MAXI..", here also it asked to minimize the max sum of diff m subarrays. actually ques ko samjhna bahot imp h to understand this, but kahi b lage that it is max the min or min the max, pls thing of a valid answer range and if that is fine then apply BS ona answers.
-        #One more thing i learned, that range of answer also depends on contraints, see here if n subarray asked, then max subarray sum is the max(ele) and if 1 subarray is asked sum(arr) is the max one. so range is [max(arr) - sum(arr)]
-        #Not intuitive to me now, dont worry will come soon. Some things here too, range always lies or most of the times, like 0 , 1, min, max, sum, yes think and try every possibility. Cant confirm in things but hope patterns will hint me. now code
-        #TC = O(nLog(sum-max))
+        n = len(nums)
         
-        def feasible(threshold) -> bool:
-            subs = 1
-            sm = 0
-            for num in nums:
-                sm += num
-                if sm > threshold:
-                    sm = num
-                    subs += 1
-                    if subs > m:
-                        return False
-            return True
+        # Create a prefix sum array of nums.
+        prefix_sum = [0] + list(itertools.accumulate(nums))
+        
+        @functools.lru_cache(None)
+        def get_min_largest_split_sum(curr_index: int, subarray_count: int):
+            # Base Case: If there is only one subarray left, then all of the remaining numbers
+            # must go in the current subarray. So return the sum of the remaining numbers.
+            if subarray_count == 1:
+                return prefix_sum[n] - prefix_sum[curr_index]
+        
+            # Otherwise, use the recurrence relation to determine the minimum largest subarray sum
+            # between curr_index and the end of the array with subarray_count subarrays remaining.
+            minimum_largest_split_sum = prefix_sum[n]
+            for i in range(curr_index, n - subarray_count + 1):
+                # Store the sum of the first subarray.
+                first_split_sum = prefix_sum[i + 1] - prefix_sum[curr_index]
 
-        lo, hi = max(nums), sum(nums)
-        while lo < hi:
-            mid = lo + (hi - lo) // 2
-            if feasible(mid):
-                hi = mid     
-            else:
-                lo = mid + 1
-        return lo
+                # Find the maximum subarray sum for the current first split.
+                largest_split_sum = max(first_split_sum, 
+                                        get_min_largest_split_sum(i + 1, subarray_count - 1))
+
+                # Find the minimum among all possible combinations.
+                minimum_largest_split_sum = min(minimum_largest_split_sum, largest_split_sum)
+
+                if first_split_sum >= minimum_largest_split_sum:
+                    break
+            
+            return minimum_largest_split_sum
+        
+        return get_min_largest_split_sum(0, m)
