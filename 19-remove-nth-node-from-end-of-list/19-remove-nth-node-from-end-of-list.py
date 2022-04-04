086fc36cc99564ce4938a4d1bb1a16a4e07cb74f
Himanshu Soni <himanshusoni3105@outlook.com>
2022-04-05 01:05:40 +0530

    Time: 45 ms (57.46%), Space: 13.8 MB (73.99%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -9,19 +9,19 @@ class Solution:
         # we ll do more optimized in one pass.
         #lol it is otimized, it is doing almoest same thing. kher know this for interview purpose but dont fall for this shitty trap of optimization. there is none
         
-
-        fast = slow = head
+        #two pointers
+        p1 = p2 = head
         for _ in range(n):          #1<=n<=size
-            fast = fast.next
+            p1 = p1.next
             
-        if not fast:            #need to handle, if no dummy created for slow,fast before head
-            return head.next
+        if not p1:            #need to handle, if no dummy created for slow,fast before head
+            return p2.next
         
-        while fast.next:
-            fast = fast.next
-            slow = slow.next
+        while p1.next:
+            p1 = p1.next
+            p2 = p2.next
             
-        slow.next = slow.next.next
+        p2.next = p2.next.next
         return head
     
     #and lets say you did this, your left/slow will be at exact node which needs to be deleted, so prev nooo? so one thing start with dummy and park slow/left at dummy so left will be always behind the target node.
