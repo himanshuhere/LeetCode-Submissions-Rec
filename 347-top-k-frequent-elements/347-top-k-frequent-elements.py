b31eda1dd9047e31ada67e2d5022bf863a30786b
Himanshu Soni <himanshusoni3105@outlook.com>
2022-04-09 12:04:00 +0530

    Time: 143 ms (49.54%), Space: 18.6 MB (91.71%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,19 +1,9 @@
-import heapq
-from collections import Counter
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        #you know quickselect?
-        dic = Counter(nums)
-        
+        c = Counter(nums)
         h = []
-        for key, val in dic.items():
-            heappush(h, (val, key))   
+        for ele in c:
+            heappush(h, (c[ele], ele))
             if len(h) > k:
                 heappop(h)
-        
-        
-        return [key for _, key in h]   
-        
-
-        
+        return [ele for freq, ele in h]
