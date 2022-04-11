d8b2a931500cc9a51f7053223c43e0e0a866057a
Himanshu Soni <himanshusoni3105@outlook.com>
2022-04-11 15:16:20 +0530

    Time: 616 ms (99.51%), Space: 21.1 MB (87.40%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,15 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:        
     #Like buy sell stock 1
-        # ans = 0
-        # minimum = prices[0]
-        # for i in range(1, len(prices)):
-        #     if prices[i] < minimum:
-        #         minimum = prices[i]
-        #     elif prices[i] > minimum + fee:
-        #         ans += prices[i] - fee - minimum
-        #         minimum = prices[i] - fee
-        # return ans
+        ans = 0
+        minimum = prices[0]
+        for i in range(1, len(prices)):
+            if prices[i] < minimum:
+                minimum = prices[i]
+            elif prices[i] > minimum + fee:
+                ans += prices[i] - fee - minimum
+                minimum = prices[i] - fee
+        return ans
 
         
         #dp
