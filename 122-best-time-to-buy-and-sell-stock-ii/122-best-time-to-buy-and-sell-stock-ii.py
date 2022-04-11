afcbdbb11f98d3319193af40386426cb6656ed54
Himanshu Soni <himanshusoni3105@outlook.com>
2022-04-11 13:56:12 +0530

    Time: 108 ms (25.35%), Space: 17.3 MB (5.01%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -42,4 +42,19 @@ class Solution:
                 else:
                     profit = max(prices[i] + dp[i+1][1], dp[i+1][0])                # Sell
                 dp[i][j] = profit
-        return dp[0][1]
+        #return dp[0][1]
+    
+        #space opt
+        ahead = [0]*2
+        cur = [0]*2
+        for i in range(n-1, -1, -1):
+            for j in range(0, 2):
+                profit = 0
+                if j:
+                    profit = max(-prices[i] + ahead[0], 0 + ahead[1])  # Buy (take/not take)
+                else:
+                    profit = max(prices[i] + ahead[1], ahead[0])                # Sell
+                cur[j] = profit
+            ahead = cur.copy()
+        return ahead[1]
+        
