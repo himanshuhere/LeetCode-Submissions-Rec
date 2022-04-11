26f959ac7e40afaa3a6d90b33f155c0f88d55e2a
Himanshu Soni <himanshusoni3105@outlook.com>
2022-04-11 13:59:28 +0530

    Time: 64 ms (89.33%), Space: 15.3 MB (24.12%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -7,12 +7,12 @@ class Solution:
         
         #note: you can buy it then immediately sell it on the same day.
         
-#         profit = 0
-#         for i in range(1, len(prices)):
-#             if prices[i] > prices[i-1]:
-#                 profit += prices[i]-prices[i-1]
-
-#         return profit
+        profit = 0
+        for i in range(1, len(prices)):
+            today, yester = prices[i], prices[i-1]
+            if today > yester:
+                profit += today-yester
+        return profit
     
         
         #dp
@@ -58,3 +58,5 @@ class Solution:
             ahead = cur.copy()
         return ahead[1]
         
+        
+        #But greedy is better as you can do any num of transaction better keep checking if today > yest better count this profit. thats all
