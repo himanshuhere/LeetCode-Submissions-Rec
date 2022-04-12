eee799c62450bf98af99e4ac62321f237542ef7d
Himanshu Soni <himanshusoni3105@outlook.com>
2022-04-12 18:17:51 +0530

    Time: 46 ms (54.07%), Space: 13.8 MB (99.78%) - LeetHub

--- 289-game-of-life/289-game-of-life.py
+++ 289-game-of-life/289-game-of-life.py
@@ -1,28 +1,25 @@
 class Solution:
-    def gameOfLife(self, board: List[List[int]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
-        dummy = [[0]*(len(board[0])+2) for _ in range(len(board)+2)]
-        for i in range(1, len(dummy)-1):
-            for j in range(1, len(dummy[0])-1):
-                dummy[i][j] = board[i-1][j-1]
+    def gameOfLife(self, b: List[List[int]]) -> None:
+        # all new 0's denotes as -1, (1 ==> 0)
+        # all new 1's denotes as 2   (0 ==> 1)
+        m, n = len(b), len(b[0])
+        dirs = [[-1,-1],[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1]]
         
-        #print(dummy)
+        for i in range(m):
+            for j in range(n):
+                livecount = 0
+                for r, c in dirs:
+                    nr, nc = i + r, j + c
+                    if 0 <= nr < m and 0 <= nc < n and abs(b[nr][nc]) == 1: # originally 1's
+                        livecount += 1
+                if b[i][j] == 1:
+                    if livecount < 2 or livecount > 3:   
+                        b[i][j] = -1
+                else:
+                    if livecount == 3:  
+                        b[i][j] = 2
         
-        def neiCounts(i, j):
-            return dummy[i][j-1] + dummy[i][j+1] + dummy[i-1][j] + dummy[i+1][j] + dummy[i+1][j-1] + dummy[i+1][j+1] + dummy[i-1][j-1] + dummy[i-1][j+1]
-                
-            
-        for i in range(1, len(dummy)-1):
-            for j in range(1, len(dummy[0])-1):
-                nc = neiCounts(i, j)
-                if dummy[i][j] == 1 and nc < 2:
-                    board[i-1][j-1] = 0
-                elif dummy[i][j] == 1 and (nc == 2 or nc == 3):
-                    board[i-1][j-1] = 1
-                elif dummy[i][j] == 1 and nc > 3:
-                    board[i-1][j-1] = 0
-                elif dummy[i][j] == 0 and nc == 3:
-                    board[i-1][j-1] = 1
-        return board
+        for i in range(m):
+            for j in range(n):
+                if b[i][j] == 2:    b[i][j] = 1
+                elif b[i][j] == -1: b[i][j] = 0
