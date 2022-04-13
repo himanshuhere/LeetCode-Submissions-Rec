13ac1a74c130ced332aa261c67e4ad8cfc5135ff
Himanshu Soni <himanshusoni3105@outlook.com>
2022-04-13 17:55:27 +0530

    Time: 286 ms (75.03%), Space: 16.8 MB (33.86%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -1,27 +1,27 @@
-# class BrowserHistory:
+class BrowserHistory:
     #1 - 2 STACK - History will always have one current page as element becasue rwe return hist[-1], there should be something there always  bcs 'Return the current url after moving back in history at most steps.'
     #future one dsnt need
     
-#     def __init__(self, homepage: str):
-#         self.history = []
-#         self.future = []
-#         self.history.append(homepage)
+    def __init__(self, homepage: str):
+        self.history = []
+        self.future = []
+        self.history.append(homepage)
 
-#     def visit(self, url: str) -> None:
-#         self.history.append(url)
-#         self.future = []
+    def visit(self, url: str) -> None:
+        self.history.append(url)
+        self.future = []
 
-#     def back(self, steps: int) -> str:
-#         while steps > 0 and len(self.history) > 1:
-#             self.future.append(self.history.pop())
-#             steps -= 1
-#         return self.history[-1]
+    def back(self, steps: int) -> str:
+        while steps > 0 and len(self.history) > 1:
+            self.future.append(self.history.pop())
+            steps -= 1
+        return self.history[-1]
 
-#     def forward(self, steps: int) -> str:
-#         while steps > 0 and len(self.future) > 0:
-#             self.history.append(self.future.pop())
-#             steps -= 1
-#         return self.history[-1]
+    def forward(self, steps: int) -> str:
+        while steps > 0 and len(self.future) > 0:
+            self.history.append(self.future.pop())
+            steps -= 1
+        return self.history[-1]
     
     #2 - Doubly LL
 class ListNode:
