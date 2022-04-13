2712c45e8ab8c3020cc9c3b440d7e2d8fc736c98
Himanshu Soni <himanshusoni3105@outlook.com>
2022-04-13 12:40:22 +0530

    Time: 303 ms (65.55%), Space: 16.5 MB (97.32%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -1,5 +1,5 @@
 class BrowserHistory:
-    #2 STACK - History will always have one current page as element
+    #2 STACK - History will always have one current page as element becasue rwe return hist[-1], there should be something there always  bcs 'Return the current url after moving back in history at most steps.'
     #future one dsnt need
     
     def __init__(self, homepage: str):
