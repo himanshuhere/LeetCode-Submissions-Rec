a8451978cdf153525928e9cd4b2a9c1be39a4347
Himanshu Soni <himanshusoni3105@outlook.com>
2022-04-13 17:54:39 +0530

    Time: 292 ms (72.67%), Space: 16.6 MB (60.64%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -1,24 +1,54 @@
-class BrowserHistory:
-    #2 STACK - History will always have one current page as element becasue rwe return hist[-1], there should be something there always  bcs 'Return the current url after moving back in history at most steps.'
+# class BrowserHistory:
+    #1 - 2 STACK - History will always have one current page as element becasue rwe return hist[-1], there should be something there always  bcs 'Return the current url after moving back in history at most steps.'
     #future one dsnt need
     
+#     def __init__(self, homepage: str):
+#         self.history = []
+#         self.future = []
+#         self.history.append(homepage)
+
+#     def visit(self, url: str) -> None:
+#         self.history.append(url)
+#         self.future = []
+
+#     def back(self, steps: int) -> str:
+#         while steps > 0 and len(self.history) > 1:
+#             self.future.append(self.history.pop())
+#             steps -= 1
+#         return self.history[-1]
+
+#     def forward(self, steps: int) -> str:
+#         while steps > 0 and len(self.future) > 0:
+#             self.history.append(self.future.pop())
+#             steps -= 1
+#         return self.history[-1]
+    
+    #2 - Doubly LL
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.prev = None
+        self.next = None
+        
+class BrowserHistory:
+        
     def __init__(self, homepage: str):
-        self.history = []
-        self.future = []
-        self.history.append(homepage)
+        self.root = ListNode(homepage)
 
     def visit(self, url: str) -> None:
-        self.history.append(url)
-        self.future = []
+        node = ListNode(url)
+        node.prev = self.root
+        self.root.next = node
+        self.root = self.root.next
 
     def back(self, steps: int) -> str:
-        while steps > 0 and len(self.history) > 1:
-            self.future.append(self.history.pop())
+        while(steps and self.root.prev):
+            self.root = self.root.prev
             steps -= 1
-        return self.history[-1]
+        return self.root.val
 
     def forward(self, steps: int) -> str:
-        while steps > 0 and len(self.future) > 0:
-            self.history.append(self.future.pop())
+        while(steps and self.root.next):
+            self.root = self.root.next
             steps -= 1
-        return self.history[-1]
+        return self.root.val
