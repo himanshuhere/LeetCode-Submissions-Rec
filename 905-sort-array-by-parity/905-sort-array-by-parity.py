7223017638e546ce662891f44224667d4e3dd03f
Himanshu Soni <himanshusoni3105@outlook.com>
2022-05-02 11:21:55 +0530

    Time: 100 ms (55.81%), Space: 14.8 MB (17.91%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.py
+++ 905-sort-array-by-parity/905-sort-array-by-parity.py
@@ -14,3 +14,16 @@ class Solution:
                 nums[i], nums[j] = nums[j], nums[i]
                 j-=1 
         return nums
+    
+        #2
+        i, j = 0, len(nums)-1
+        while i < j:
+            if nums[i] & 1 == 0:    #even
+                i += 1
+            elif nums[j] & 1 == 1:  #odd
+                j -= 1
+            else:
+                nums[i], nums[j] = nums[j], nums[i]
+                i+=1 
+                j-=1
+        return nums
