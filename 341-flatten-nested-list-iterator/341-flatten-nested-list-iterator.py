14ef231f394430fc1ad210e0bf8397db85714368
Himanshu Soni <himanshusoni3105@outlook.com>
2022-05-09 00:00:15 +0530

    Time: 74 ms (71.27%), Space: 17.8 MB (41.16%) - LeetHub

--- 341-flatten-nested-list-iterator/341-flatten-nested-list-iterator.py
+++ 341-flatten-nested-list-iterator/341-flatten-nested-list-iterator.py
@@ -1,18 +1,34 @@
+def dfs(nested, flat):
+    for elem in nested:
+        if elem.isInteger():
+            flat.append(elem.getInteger())
+        else:
+            dfs(elem.getList(), flat)
 
+class NestedIterator:
+    def __init__(self, nestedList: [NestedInteger]):
+        self.flatList, self.i = [], 0
+        dfs(nestedList, self.flatList)
+    
+    def next(self) -> int:
+        self.i += 1
+        return self.flatList[self.i-1]
+    
+    def hasNext(self) -> bool:
+        return self.i < len(self.flatList)
+    
+
+    
+    
+    
 class NestedIterator:
     def __init__(self, nestedList: [NestedInteger]):
         self.stack = nestedList[::-1]
         
     def next(self):
-        """
-        :rtype: int
-        """
         return self.stack.pop().getInteger()
         
     def hasNext(self):
-        """
-        :rtype: bool
-        """
         while self.stack:
             top = self.stack[-1]
             if top.isInteger():
