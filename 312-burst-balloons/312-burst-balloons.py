1978b5a37c487eaec1ca1e16b0bf598d2a3d2aee
Himanshu Soni <himanshusoni3105@outlook.com>
2022-05-19 18:51:46 +0530

    Time: 11908 ms (23.86%), Space: 30.9 MB (32.93%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.py
+++ 312-burst-balloons/312-burst-balloons.py
@@ -1,20 +1,22 @@
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
-        #MCM - Classic MCM
+        #MCM - PARTITION DP
+        #cuts can be in any order but if wanna go DP recursing you have to have cuts in sorted way else left right calls of i..j will be messed up. second imp thing is that every time we make cut we take the lenght of current stick in account and thats something to take care of since you ll be given on cuts location in array but stick is something diff like 3,4,7,8 is actually 1,2,3,4,5,6,7,8,9 or anything. so inorder to calc lenght we have something see, we are appeneding the actual cuts ( both ends ) and can use them, recusrsivey we ll always have i-1, j+1 as left and right cuts and can have lenght of curr rod
+        cuts = nums
+        cuts = [1] + cuts + [1]
+        #cuts.sort()
         
         @lru_cache(None)
-        def mcm(i, j):
+        def f(i, j):
             if i > j:
                 return 0
-            
-            coins = 0
-            for k in range(i, j):       #i to j-1
-                tmp = mcm(i, k) + mcm(k+1, j) + (nums[i-1]*nums[k]*nums[j])
-                coins = max(coins, tmp)
-            
-            return coins
+            #if i == j:
+                #return cuts[j+1]-cuts[i-1]
+                
+            mini = -math.inf
+            for k in range(i, j+1):
+                cost = cuts[j+1]*cuts[k]*cuts[i-1] + f(i, k-1) + f(k+1, j)
+                mini = max(mini, cost)
+            return mini
         
-        nums = [1] + nums + [1]
-        return mcm(1, len(nums)-1)      #for loop range is till j-1 so j wud be left for multipication
-    
-    #1. 3 1 5 8 .1
+        return f(1, len(cuts)-2)
