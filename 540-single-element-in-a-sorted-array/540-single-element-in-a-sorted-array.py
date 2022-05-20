f234ce90083af40d7bd67d07e402aa4a525c570d
Himanshu Soni <himanshusoni3105@outlook.com>
2022-05-20 17:01:29 +0530

    Time: 265 ms (31.35%), Space: 23.8 MB (45.97%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
@@ -21,3 +21,8 @@ class Solution:
     #else goon left
     #just check = if mid is even and duplicate is on right good or if mid is odd dupicate is in left fine. move right
     #else move left
+    
+    
+    #lets make solution considering that our left of mid is always correct thus only right side has the culprint(that single ele), just assume. So if left is fine now there are two cases if my mid index is first of two elements (like 4,4 and my mid is first 4) so imagine if every element appears twice in left side means always first occurent of duplicates will take even index like 0,2,4,6 and second occurance will always take odd indices like 1, 3, 5. so two case is
+    #if a[mid]==a[mid-1], means our mid is at second occurence of duplicates and it should be odd index right,
+    #similarly, if a[mid+1]==a[mid], means our mid is first one, so index should be even, if any of this condition met means left side is fine lets move to right one else left side is wrong and has the single elements (that ruined our even/odd index logic) so bhai chalo chalo pakdo bhen k lode ko left me hi hai bas
