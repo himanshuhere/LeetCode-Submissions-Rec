3228a7055e7e6f86974b8e23d85267fdb6c80ee6
Himanshu Soni <himanshusoni3105@outlook.com>
2022-05-20 11:48:56 +0530

    Time: 61 ms (43.22%), Space: 14.4 MB (5.45%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -0,0 +1,34 @@
+class Solution:
+    def uniquePathsWithObstacles(self, A: List[List[int]]) -> int:
+        m,n = len(A), len(A[0])
+        
+        @lru_cache(None)    #dp
+        def f(i,j):
+            if i>=m or j>=n or A[i][j] == 1:
+                return 0
+            if (i,j) == (m-1,n-1):
+                return 1
+            return f(i+1,j)+f(i,j+1)
+        return f(0,0)
+        
+        #2
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        # if A[-1][-1] == 1:      
+        #     return 0
+        
+        for i in range(m-1, -1, -1):
+            for j in range(n-1, -1, -1):
+                
+                if A[i][j] == 0:
+                    
+                    if i==m-1 and j==n-1:
+                        dp[i][j] = 1
+                    elif i == m-1:
+                        dp[i][j] = dp[i][j+1]
+                    elif j == n-1:
+                        dp[i][j] = dp[i+1][j]
+                    else:
+                        dp[i][j] = dp[i+1][j] + dp[i][j+1]
+            
+        return dp[0][0]
+                
