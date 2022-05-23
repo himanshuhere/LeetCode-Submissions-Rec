df7a3260be5919ef7aa7ae41b601a1f3197cc994
Himanshu Soni <himanshusoni3105@outlook.com>
2022-05-23 13:29:22 +0530

    Time: 3619 ms (67.09%), Space: 303.5 MB (5.05%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.py
+++ 474-ones-and-zeroes/474-ones-and-zeroes.py
@@ -4,7 +4,7 @@ class Solution:
         def f(i, m, n):
             if m < 0 or n < 0:
                 return -1
-            if (not m and not n) or i == len(strs):
+            if (m==0 and n==0) or i == len(strs):
                 return 0
             
             return max(1 + f(i+1, m-strs[i].count('0'), n-strs[i].count('1')), f(i+1, m, n))
