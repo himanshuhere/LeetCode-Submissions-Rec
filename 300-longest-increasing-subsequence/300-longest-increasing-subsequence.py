1c3a40e3428b909e4364308382c3a2af2d4c1510
Himanshu Soni <himanshusoni3105@outlook.com>
2022-05-26 02:44:11 +0530

    Time: 7346 ms (5.00%), Space: 14.4 MB (14.70%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -2,38 +2,39 @@ class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n = len(nums)
         
+        
+#         #1
 #         @lru_cache(None)
-#         def f(i, previ):
-#             if i == n:
-#                 return 0
-            
-#             l = 0 + f(i+1, previ)       #not take
-#             if previ == -1 or nums[i] > nums[previ]:
-#                 l = max(l, 1 + f(i+1, i))   #take
-#             return l
+#         def f(idx, pre, ans = 0):
+#             if idx == n:
+#                 return ans
+#             result = -math.inf
+#             if pre == -1 or nums[pre] < nums[idx]:
+#                 result = max(result, f(idx+1,idx,ans+1))
+#             result = max(result, f(idx+1,pre,ans))
+#             return result
         
 #         return f(0, -1)
         
-#         #tabulation
-#         #dp = [[0]*(n+1) for _ in range(n+1)]
+#           #2 - tabulation
+#         dp = [[0]*(n+1) for _ in range(n+1)]
 #         #base case handled
-#         # for i in range(n-1, 0, -1):
-#         #     for j in range(i-1, -1, -1):        #prev can be from i-1 to -1
-#         #         l = dp[i+1][j+1]
-#         #         if j == -1 or nums[i] > nums[j]:
-#         #             l = max(l, 1+dp[i+1][i])
-#         #         dp[i][j] = l
-#         #return dp
+#         for i in range(n-1, 0, -1):
+#             for j in range(i-1, -1, -1):        #prev can be from i-1 to -1
+#                 l = dp[i+1][j+1]
+#                 if j == -1 or nums[i] > nums[j]:
+#                     l = max(l, 1+dp[i+1][i])
+#                 dp[i][j] = l
+#         return dp
     
 #         #best tab - it is not intuitive you should know
 #         #t - n^2 and s - n
-#         ans = [1]*n         #eveyone make 1 LIS
-#         for i in range(n):
-#             for j in range(i):
-#                 if nums[i] > nums[j]:
-#                     ans[i] = max(ans[i], 1+ans[j])
-#         return max(ans)
-    
+        ans = [1]*n         #eveyone make 1 LIS
+        for i in range(n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    ans[i] = max(ans[i], 1+ans[j])
+        return max(ans)
     
     
         #BS+DP - nlogn
