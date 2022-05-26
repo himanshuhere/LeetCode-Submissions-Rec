e100e2c28ac65ee8ed29b7b79a184bb76b4f8665
Himanshu Soni <himanshusoni3105@outlook.com>
2022-05-26 15:00:53 +0530

    Time: 7219 ms (8.14%), Space: 14.2 MB (25.58%) - LeetHub

---
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minimumMountainRemovals(self, nums: List[int]) -> int:
+        #Find the LBS, longest bitonic subsequence and then subtract it from the total lenght. you ll get your maountain fucker
+        
+        #LBS from LIS see
+        n = len(nums)
+        inc = [0] * n
+        dec = [0] * n
+        
+        #lis
+        for i in range(1,n):
+            for j in range(0,i):
+                if nums[i] > nums[j]:
+                    inc[i] = max(inc[i], inc[j] + 1)
+          
+        #lds
+        for i in range(n-2,-1,-1):
+            for j in range(n-1,i,-1):
+                if nums[i] > nums[j]:
+                    dec[i] = max(dec[i], dec[j] + 1)
+        
+        res = 0
+        for i in range(0,n):
+            if inc[i] > 0 and dec[i] > 0:
+                res = max(res, inc[i] + dec[i])
+                
+        return n - res - 1
