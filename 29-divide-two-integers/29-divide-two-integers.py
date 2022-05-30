b4609dbdf2cd8a0c35e8b26c8eaf9ce8d6129801
Himanshu Soni <himanshusoni3105@outlook.com>
2022-05-30 17:20:22 +0530

    Time: 53 ms (34.12%), Space: 13.8 MB (98.39%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.py
+++ 29-divide-two-integers/29-divide-two-integers.py
@@ -9,8 +9,9 @@ class Solution:
         for i in range(31, -1, -1): #31 to 0
             if t + (divisor << i) <= dividend:
                 t += (divisor << i)
-                quot = quot | 1 << i
+                quot |= (1 << i)
                 
         if not positive:
             quot = -quot
+            
         return min(max(-2 ** 31, quot), 2 ** 31-1)
