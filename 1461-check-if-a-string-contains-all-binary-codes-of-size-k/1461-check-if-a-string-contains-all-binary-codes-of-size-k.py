1c1fc209b6ce8317a43a615416d49f60799c64c9
Himanshu Soni <himanshusoni3105@outlook.com>
2022-05-31 16:51:59 +0530

    Time: 741 ms (23.89%), Space: 23.2 MB (80.53%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -1,14 +1,15 @@
 class Solution:
     def hasAllCodes(self, s: str, k: int) -> bool:
         #brute - generating all 2^k ( 1<<k) codes thats costly
+        #logically ye bhi nk hi hai
         st = set()
         for i in range(0, len(s)-k+1):
             st.add(int(s[i:i+k], 2))
         
-        # for i in range(0, 2**k):
-        #     if i not in st:
-        #         return False
-        # return True
+        for i in range(0, 2**k):
+            if i not in st:
+                return False
+        return True
     
         
         #omptimized (will not generate all codes, rather try this) o(nk)
