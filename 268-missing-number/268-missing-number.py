ed4451debd04bf015d38dc93fd59e24088c193bc
Himanshu Soni <himanshusoni3105@outlook.com>
2022-06-06 12:49:52 +0530

    Time: 235 ms (32.27%), Space: 15.2 MB (75.69%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,9 +1,9 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        # missing = len(nums)
-        # for i, num in enumerate(nums):
-        #     missing ^= i ^ num
-        # return missing\
+        missing = len(nums)
+        for i, num in enumerate(nums):
+            missing ^= i ^ num
+        return missing
         currsum = sum(nums)
         actsum = len(nums) * (len(nums) + 1) // 2
         return actsum - currsum
