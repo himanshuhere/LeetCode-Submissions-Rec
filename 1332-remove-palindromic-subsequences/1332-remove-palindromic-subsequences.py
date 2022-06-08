0476a1fef5d4fdf9fa930e3a4fd5a150273838df
Himanshu Soni <himanshusoni3105@outlook.com>
2022-06-08 12:29:16 +0530

    Time: 50 ms (21.27%), Space: 13.9 MB (53.87%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.py
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.py
@@ -1,3 +1,10 @@
 class Solution:
     def removePalindromeSub(self, s: str) -> int:
         return 1 if s == s[::-1] else 2
+    
+    #That means, there are 3 situations of this problem:
+        #1. String length equals 0 -> return 0;
+        #2. String itself is a Palindrome -> return 1; (because you can remove them all at once)
+        #3. Other cases, you can first remove all 'a' and then all 'b' or vice versa (first all 'b' then all 'a') -> return 2;
+        
+        #sare 'a' pakdo to palidrom hai nikal k feko 1 cost, sare b reh gye wo b palindrom hai cost =2, ab combination k bare me sochna hi nhi hai
