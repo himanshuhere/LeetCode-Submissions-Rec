4be9d156aac6afe2d353ff7d26c8bbe8524175a7
Himanshu Soni <himanshusoni3105@outlook.com>
2022-06-08 12:58:08 +0530

    Time: 139 ms (15.60%), Space: 26.8 MB (13.81%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -27,17 +27,15 @@ class Solution:
             if root is None:
                 return None
 
-            # If both n1 and n2 are smaller than root, then LCA
             # lies in left
             if(root.val > n1.val and root.val > n2):
                 return lca(root.left, n1, n2)
-
-            # If both n1 and n2 are greater than root, then LCA
             # lies in right
-            if(root.val < n1 and root.val < n2):
+            elif(root.val < n1 and root.val < n2):
                 return lca(root.right, n1, n2)
-
-            return root
+            else:
+                return root
+        
         #Iterative LCA ****BST*****
         def lca(root, n1, n2):
             while root:
@@ -46,5 +44,5 @@ class Solution:
                 elif root.data < n1 and root.data < n2:
                     root = root.right
                 else:
-                    break
+                    return root
             return root
