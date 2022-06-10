4be1df5f78662454cf6ed20c2ab27e09be47c85a
Himanshu Soni <himanshusoni3105@outlook.com>
2022-06-10 13:24:14 +0530

    Time: 1543 ms (17.87%), Space: 33.3 MB (88.75%) - LeetHub

---
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minimumCardPickup(self, s: List[int]) -> int:
+        #variable sliding window, k not given but see, instead of len(map) you can use count logic you know right
+        i = 0
+        map_ = defaultdict(int)
+        ans = math.inf
+        start = -1
+        
+        for j in range(len(s)):
+            if s[j] in map_:
+                start = map_[s[j]]
+                ans = min(ans, j-start+1)
+                start = j
+            map_[s[j]] = j
+            
+        return ans if ans!=math.inf else -1
+            
