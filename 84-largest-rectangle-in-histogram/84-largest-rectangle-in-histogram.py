40b983c033d7726aec076d89251262dd5d156a46
Himanshu Soni <himanshusoni3105@outlook.com>
2022-06-13 12:48:06 +0530

    Time: 1587 ms (29.69%), Space: 26.6 MB (99.86%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
@@ -34,48 +34,52 @@ class Solution:
 
 #we tend to pop stack when top value is greater or equeal ( means this is part of res and we tend to check more forward/backward) , why we pop it, becasue the most imp step of appending index on stack every time will make sure that stack will always have values in increasing order, bigger value at top, so since we r loioking for smaller value we keep popping for the greed of smaller, then if found we put it in ans and cur index in stack.
         #calculate NSR
-        def nsr():
-            s = []
-            right = [None]*len(h)
-            last = len(h)
+#         def nsr():
+#             s = []
+#             right = [None]*len(h)
+#             last = len(h)
             
-            for i in range(len(h)-1, -1, -1):
-                while s and h[s[-1]] >= h[i]:
-                    s.pop()
-                right[i] = s[-1] if s else last
-                s.append(i)
-            return right
+#             for i in range(len(h)-1, -1, -1):
+#                 while s and h[s[-1]] >= h[i]:
+#                     s.pop()
+#                 right[i] = s[-1] if s else last
+#                 s.append(i)
+#             return right
         
-                #calculate NSR
-        def nsl():
-            s = []
-            left = [None]*len(h)
-            first = -1
-            for i in range(len(h)):
-                while s and h[s[-1]] >= h[i]:
-                    s.pop()
-                left[i] = s[-1] if s else first
-                s.append(i)
-            return left
+#                 #calculate NSR
+#         def nsl():
+#             s = []
+#             left = [None]*len(h)
+#             first = -1
+#             for i in range(len(h)):
+#                 while s and h[s[-1]] >= h[i]:
+#                     s.pop()
+#                 left[i] = s[-1] if s else first
+#                 s.append(i)
+#             return left
         
         
-        ans = -math.inf
-        left, right = nsl(), nsr()
-        for i in range(len(h)):
-            ans = max(ans, (right[i]-left[i]-1)*h[i])
-        return ans
+#         ans = -math.inf
+#         left, right = nsl(), nsr()
+#         for i in range(len(h)):
+#             ans = max(ans, (right[i]-left[i]-1)*h[i])
+#         return ans
     
     #3 optmimized
-    #calculate NSR
-        def nsr():
+        def single():
+            n = len(h)
+            l, r = [None]*n, [None]*n
             s = []
-            right = [None]*len(h)
-            last = len(h)
-            
-            for i in range(len(h)-1, -1, -1):
+            for i in range(n):
                 while s and h[s[-1]] >= h[i]:
-                    s.pop()
-                right[i] = s[-1] if s else last
+                    r[s.pop()] = i
+                l[i] = s[-1] if s else -1
                 s.append(i)
-            return right
+            return l, r
+        
+        ans = -math.inf
+        left, right = single()
+        for i in range(len(h)):
+            ans = max(ans, ((right[i] if right[i] else len(h)) -left[i]-1)*h[i])
+        return ans
                     
