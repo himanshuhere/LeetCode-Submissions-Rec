8baf6ddc26abc2a526600e1c1140e39de1b83c0e
Himanshu Soni <himanshusoni3105@outlook.com>
2022-06-13 10:41:12 +0530

    Time: 71 ms (87.14%), Space: 15.2 MB (33.19%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -4,7 +4,7 @@ class Solution:
         # 3 4
         # 6 5 7
         # 4 1 8 3
-        #technically there is always be j and j+1 so no base boundary case for j but for i only
+        #technically there ll always be j and j+1 so no base boundary case for j but for i only
         @lru_cache(None)
         def f(i, j):
             if i == len(triangle)-1:
@@ -13,16 +13,18 @@ class Solution:
             return triangle[i][j] + min(f(i+1, j) , f(i+1, j+1))
         #return f(0,0)
         
+        
         #2d tabulation
-        dp = [[0]*(i+1) for i in range(len(triangle))]
         n = len(triangle)
+        dp = [[0]*(i+1) for i in range(n)]
+        
         for j in range(n):
             dp[n-1][j] = triangle[n-1][j]
         
         for i in range(n-2, -1, -1):
-            for j in range(len(dp[i])-1, -1, -1):
+            for j in range(len(dp[i])):
                 dp[i][j] = triangle[i][j] + min(dp[i+1][j], dp[i+1][j+1])
-        #return dp[0][0]
+        return dp[0][0]
     
     
         #1d since i=1 eberytime
