5f0f761b3fc8360e874013435219390ba84b7347
Himanshu Soni <himanshusoni3105@outlook.com>
2022-06-13 10:42:54 +0530

    Time: 120 ms (24.24%), Space: 15 MB (60.15%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -15,19 +15,20 @@ class Solution:
         
         
         #2d tabulation
+#         n = len(triangle)
+#         dp = [[0]*(i+1) for i in range(n)]
+        
+#         for j in range(n):
+#             dp[n-1][j] = triangle[n-1][j]
+        
+#         for i in range(n-2, -1, -1):
+#             for j in range(len(dp[i])):
+#                 dp[i][j] = triangle[i][j] + min(dp[i+1][j], dp[i+1][j+1])
+#         return dp[0][0]
+    
+    
+        #1d since i=1 everytime
         n = len(triangle)
-        dp = [[0]*(i+1) for i in range(n)]
-        
-        for j in range(n):
-            dp[n-1][j] = triangle[n-1][j]
-        
-        for i in range(n-2, -1, -1):
-            for j in range(len(dp[i])):
-                dp[i][j] = triangle[i][j] + min(dp[i+1][j], dp[i+1][j+1])
-        return dp[0][0]
-    
-    
-        #1d since i=1 eberytime
         dp = [0]*n
         for j in range(n):
             dp[j] = triangle[n-1][j]
@@ -35,7 +36,7 @@ class Solution:
         #print(dp)
         for i in range(n-2, -1, -1):
             cur = [0]*(i+1)
-            for j in range(len(cur)-1, -1, -1):
+            for j in range(len(cur)):
                 cur[j] = triangle[i][j] + min(dp[j], dp[j+1])
             dp = cur
         return dp[0]
