c250d928d96fca3bc227166f176b6b0f25189bd1
Himanshu Soni <himanshusoni3105@outlook.com>
2022-06-17 18:51:30 +0530

    Time: 59 ms (64.78%), Space: 14.2 MB (92.17%) - LeetHub

---
+++ 968-binary-tree-cameras/968-binary-tree-cameras.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minCameraCover(self, root: Optional[TreeNode]) -> int:
+        #there are two ways, go preorder and go postorder.
+        #if preorder, you can either put camera at root or its chidrens and go on. Here putting at root would be greedly better way right as childs are gonna be more that root max 2.
+        #If postorder, either we can put cameras at all leaves or to their partents. And see putting camera at leaves parent will cover leaves as well as their parent too, which leaves wont do if put at them. Plus leaves parent will alwys be lesser that leaves and more beneficial as per condition. 
+        #So we grt the best approach, do postorder and put cameras at leaves parent.
+        
+        # 0 means not covered。1 means covered but not has a camera on it. 2 means a camera on it.
+        # reference: https://www.itread01.com/content/1546174153.html
+        def dfs(node):
+            if not node:
+                return 1
+            l=dfs(node.left)
+            r=dfs(node.right)
+            
+            if l==0 or r==0:
+                self.sum+=1
+                return 2
+            elif l==2 or r==2:
+                return 1
+            else:
+                return 0
+        
+        self.sum=0
+        if dfs(root)==0:
+            self.sum+=1
+        
+        return self.sum
