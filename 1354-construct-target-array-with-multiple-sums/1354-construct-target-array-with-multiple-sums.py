fe50b89d3961ecdc258425d7bfc6afd66d263da9
Himanshu Soni <himanshusoni3105@outlook.com>
2022-06-24 14:42:55 +0530

    Time: 494 ms (25.88%), Space: 19.9 MB (55.29%) - LeetHub

---
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPossible(self, A: List[int]) -> bool:
+        total = sum(A)
+        A = [-a for a in A]
+        heapq.heapify(A)
+        
+        while True:
+            m = -heapq.heappop(A)
+            total -= m
+            if m == 1 or total == 1: 
+                return True
+            if m < total or total == 0 or m % total == 0:
+                return False
+            m %= total
+            total += m
+            heapq.heappush(A, -m)
+        
