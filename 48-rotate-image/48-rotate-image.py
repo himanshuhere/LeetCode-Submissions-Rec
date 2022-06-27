9263ffc72e93cbee584dcc50063b698f1dcf84e4
Himanshu Soni <himanshusoni3105@outlook.com>
2022-06-27 20:40:49 +0530

    Time: 56 ms (41.73%), Space: 14 MB (29.28%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.py
@@ -0,0 +1,23 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        # clockwise rotate
+        # first reverse up to down, then swap the symmetry
+        # 1 2 3         7 8 9       7 4 1
+        # 4 5 6     =>  4 5 6   =>  8 5 2
+        # 7 8 9         1 2 3       9 6 3
+        
+        # matrix.reverse()
+        #in place
+        #reverese
+        i = 0
+        j = len(matrix)-1
+        while i <= j:
+            for k in range(len(matrix)):
+                matrix[i][k], matrix[j][k] = matrix[j][k], matrix[i][k]
+            i += 1
+            j -= 1
+            
+        #transpose
+        for i in range(len(matrix)):
+            for j in range(i):      #range(i+1, len(matrix[0]))
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
