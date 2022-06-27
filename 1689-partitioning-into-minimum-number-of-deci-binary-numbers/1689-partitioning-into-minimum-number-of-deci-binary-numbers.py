4c2014be4fd42370c7a2e225b8e7ed53c289c893
Himanshu Soni <himanshusoni3105@outlook.com>
2022-06-27 11:42:28 +0530

    Time: 114 ms (56.83%), Space: 14.8 MB (50.25%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -10,4 +10,16 @@ class Solution:
         # 1000, 10001, 1100, 1101, 1110, 1111 - 9999
         # 99999
         
-        
+#         Eg. 
+# 1. Consider n="9"
+# => We need to add 1 for 9 times to get n
+# 2. n = "4201"
+# => We can add    1101
+#                + 1100
+#                + 1000 
+#                + 1000 
+#              -------
+# 			     4201
+# or any other combination which doesnt matter for this quesiton to get n. 
+
+# Notice how we require atleast MAX_DIGIT number of deci-binary numbers, where MAX_DIGIT is max digit in n
