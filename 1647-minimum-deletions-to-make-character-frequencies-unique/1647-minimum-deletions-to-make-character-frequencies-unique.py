7d81fafeb1738605fed4384b13c25d4b1cb362fb
Himanshu Soni <himanshusoni3105@outlook.com>
2022-06-28 13:48:23 +0530

    Time: 187 ms (71.04%), Space: 14.8 MB (93.89%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -11,4 +11,6 @@ class Solution:
             else:
                 prev = freq
         return deletions
+    
+        #if sorted, prev hamesha bada ya eq hoga, bada hai to leave and go ahead else euqal hoga to change the current freq to -1, but what if pcihe wala bada ho gya ho cur se kuki mai hi operation kar rha prev-1, so mai bina check kiya prev - 1 kar dunga, kuki socho 100, 100, 100..100, so 100, 99, 98, 97, ,....2, 100, ab 100 aya mai sidha 1 kar dunga kuki it is gurantanteed k 100 se 2 k bich ki sari val present hai kuki data sorted hai aur hum hi change lar rhe distort kar rhe aur wo prev-1 kar rhe to guarantee ye val presemt hai
             
