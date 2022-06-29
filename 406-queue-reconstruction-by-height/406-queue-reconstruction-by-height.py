6c326a27ba52cbf0da307a53285f6b5a9f200758
Himanshu Soni <himanshusoni3105@outlook.com>
2022-06-29 11:40:03 +0530

    Time: 168 ms (44.03%), Space: 14.6 MB (31.23%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
@@ -5,7 +5,8 @@ class Solution:
 # [[7,0], [7,1], [6,1], [5,0], [5,2], [4,4]]
 
 # Then, construct queue with sorted list.
-        result = []
+
+        result = []*len(people)
         people.sort(key = lambda a: (-a[0], a[1]))
         for x in people: 
             i = x[1]
