98fff9317fbdd9e23b2f758344e0657e77b6783e
Himanshu Soni <himanshusoni3105@outlook.com>
2022-06-29 17:21:28 +0530

    Time: 807 ms (9.80%), Space: 28.8 MB (85.14%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.py
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.py
@@ -2,7 +2,7 @@ class Solution:
     def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
         # Before anything, **this problem and GFG Count Subarrays with Xor as K** ,  *This problem clears a lot of concepts*
         # Xor[0, i] ^ Xor [i, j] = Xor [0, j]
-        # Xor[i, j] = Xor[0, j] ^ Xor[0, i]
+        # Xor[i, j] = Xor[0, j] ^ Xor[0, i] i-1 precisely
         d = {}
         res = []
         d[-1] = 0 #initialize it with -1 for first iter as it will be 0-1 = -1 so not error shud come na
