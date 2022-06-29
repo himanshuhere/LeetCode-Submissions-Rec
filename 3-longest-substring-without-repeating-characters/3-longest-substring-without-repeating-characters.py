0c3fb0083a94c26b8c36ff13d4b347c73900176b
Himanshu Soni <himanshusoni3105@outlook.com>
2022-06-29 16:54:37 +0530

    Time: 143 ms (26.24%), Space: 14.1 MB (49.20%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -3,15 +3,19 @@ class Solution:
         #variable sliding window, k not given but see, instead of len(map) you can use count logic you know right
         i, j = 0, 0
         map_ = defaultdict(int)
+        len_ = 0
         ans = 0
         count = 0
         
         while j < len(s):
             map_[s[j]] += 1
+            if map_[s[j]] == 1:
+                len_ += 1
             
-            while len(map_) < j-i+1:
+            while len_ < j-i+1:
                 map_[s[i]] -= 1
                 if map_[s[i]] == 0:
+                    len_ -= 1
                     del map_[s[i]]
                 i += 1
                     
