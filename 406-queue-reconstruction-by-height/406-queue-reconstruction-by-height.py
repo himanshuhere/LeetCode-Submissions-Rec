0e518ef3488bc356252e1536e553048d1ab5d3ab
Himanshu Soni <himanshusoni3105@outlook.com>
2022-06-29 11:39:06 +0530

    Time: 105 ms (87.97%), Space: 14.5 MB (31.23%) - LeetHub

---
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+#         Sort based on height in descending, and for persons with same height, sort in ascending based on k.
+# Sorted Values:
+# [[7,0], [7,1], [6,1], [5,0], [5,2], [4,4]]
+
+# Then, construct queue with sorted list.
+        result = []
+        people.sort(key = lambda a: (-a[0], a[1]))
+        for x in people: 
+            i = x[1]
+            result.insert(i, x);
+        return result
