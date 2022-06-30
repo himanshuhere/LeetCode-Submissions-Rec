959a43db842868b65abed03fb112c14a34dc39ab
Himanshu Soni <himanshusoni3105@outlook.com>
2022-06-30 12:12:28 +0530

    Time: 146 ms (19.26%), Space: 15.4 MB (33.43%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.py
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.py
@@ -1,5 +1,6 @@
 class Solution:
     def minMoves2(self, nums: List[int]) -> int:
+        #median and average could be possible thinking, but median make more sense to balance of the array
         nums.sort()
         
         med = 0
