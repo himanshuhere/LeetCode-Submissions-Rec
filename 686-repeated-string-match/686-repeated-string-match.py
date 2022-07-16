ed3fb664cc7bbe0398f91fd41262e70043f17948
Himanshu Soni <himanshusoni3105@outlook.com>
2022-07-16 16:45:33 +0530

    Time: 216 ms (10.51%), Space: 14 MB (26.85%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.py
+++ 686-repeated-string-match/686-repeated-string-match.py
@@ -2,15 +2,33 @@ class Solution:
     def repeatedStringMatch(self, a: str, b: str) -> int:
         #Idea is to simple add a till its lenght is less than B, if not then too then toh add once at back now definately you ll have your sub found. 3e can use our own method instead of sub see
         
+        #own's contains method, if asked
+        def isContains(s, sub):         #TLE
+            i, j = 0, 0
+            while i < len(s):
+                j = 0
+                while i < len(s) and j < len(sub) and s[i] == sub[j]:
+                    j += 1
+                if j == len(sub):
+                    return True
+            return False
+        
+        def isContains2(s, sub):         #TLE
+            subhash = hash(sub)
+            for i in range(0, len(s)-len(sub)+1):
+                if subhash == hash(s[i:i+len(sub)]):
+                    return True
+            return False
+        
         tmp = ""
         count = 0
         while len(tmp) < len(b):
             tmp += a
             count += 1
         
-        if b in tmp:
+        if isContains2(tmp, b):
             return count
-        if b in tmp+a:
+        if isContains2(tmp+a, b):
             return count+1
         return -1
-        
+    
