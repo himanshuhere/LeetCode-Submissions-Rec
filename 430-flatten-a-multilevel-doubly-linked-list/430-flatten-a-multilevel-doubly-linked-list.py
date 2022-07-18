9061aa240f725c0680f6ed14aedbbad9d79df4b3
Himanshu Soni <himanshusoni3105@outlook.com>
2022-07-18 11:38:42 +0530

    Time: 43 ms (80.49%), Space: 14.7 MB (67.70%) - LeetHub

---
+++ 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.py
@@ -0,0 +1,28 @@
+class Solution:
+    def flatten(self, head: 'Node') -> 'Node':
+        
+        #see saving the state, go for recursion/stack
+        #recursion kinda tricky here
+        #STACK STACK STACK
+        
+        if not head: return head
+        
+        dummy = Node(0)
+        curr, stack = dummy, [head]
+        
+        while stack:
+            last = stack.pop() 
+            
+            if last.next:
+                stack.append(last.next)
+            if last.child:
+                stack.append(last.child)
+                
+            curr.next = last
+            last.prev = curr  
+            last.child = None
+            curr = curr.next
+        
+        res = dummy.next
+        res.prev = None
+        return res
