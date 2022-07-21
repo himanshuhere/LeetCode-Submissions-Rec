86a798de0dd566c77677012382e6a89fcecba7fa
Himanshu Soni <himanshusoni3105@outlook.com>
2022-07-21 18:40:34 +0530

    Time: 346 ms (20.56%), Space: 17.1 MB (27.85%) - LeetHub

---
+++ 529-minesweeper/529-minesweeper.py
@@ -0,0 +1,32 @@
+#Let's undertstand the flow in laymans term bruh. (What i understand baki dekh lena clear karke)
+# If b[i][j] is M, game is over make that X and return
+# Else (If E)
+#     Check its 8 adjacent, and if any M present count them and fill that digit here in current cell and leave the game
+#     If no M present on adjacents, put B here and recursively go on clicking every adjacent
+
+#We ll be using smart DFS see
+
+class Solution:
+    def getAdjacentMines(self, board, x, y): 
+        numMines=0
+        for r in range(x-1, x+2): 
+            for c in range(y-1, y+2):
+                if r >= 0 and r < len(board) and c >= 0 and c < len(board[r]) and board[r][c] == "M":
+                    numMines += 1
+        return numMines
+    
+    def updateBoard(self, board, click): 
+        x, y = click
+        if board[x][y] == "M":
+            board[x][y] = "X"
+        else:
+            numMines = self.getAdjacentMines(board, x, y)
+            if numMines > 0:
+                board[x][y]= str(numMines)
+            else:
+                board[x][y] = "B"
+                for r in range(x-1, x+2):
+                    for c in range(y-1, y+2):
+                        if r >= 0 and r < len(board) and c >= 0 and c < len(board[r]) and board[r][c] != "B":
+                            self.updateBoard(board, [r, c])
+        return board
