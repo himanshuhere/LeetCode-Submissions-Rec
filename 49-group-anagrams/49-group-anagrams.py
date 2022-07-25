4af558e958e3b47612695c00d5f01665e7b46374
Himanshu Soni <himanshusoni3105@outlook.com>
2022-07-26 02:01:17 +0530

    Time: 120 ms (81.22%), Space: 18.3 MB (45.12%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -4,12 +4,20 @@ class Solution:
         #tan, nat, ant has key sorted is ant so map[ant] = [tan, nat, ant]
         #or mat[ant].values()
         
-        words = {}
-        for i in strs:
-            #string = ''.join((sorted(i)))
-            string =str(sorted(i))
-            if string not in words:
-                words[string] = [i]
-            else:
-                words[string] += [i]
-        return words.values()
+        #2
+        dic = defaultdict(list)
+        for s in strs:
+            sorteds = str(sorted(s))        #immutible string can be key not array
+            dic[sorteds].append(s)
+        return dic.values()
+        
+        #1
+        # words = {}
+        # for i in strs:
+        #     #string = ''.join((sorted(i)))
+        #     string =str(sorted(i))
+        #     if string not in words:
+        #         words[string] = [i]
+        #     else:
+        #         words[string] += [i]
+        # return words.values()
