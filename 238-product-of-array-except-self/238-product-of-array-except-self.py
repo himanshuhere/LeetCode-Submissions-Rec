83f035044508f33b9f667aa0edc0fe67dbd74d3b
Himanshu Soni <himanshusoni3105@outlook.com>
2022-07-26 02:17:48 +0530

    Time: 368 ms (44.19%), Space: 21.4 MB (38.06%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,17 +1,17 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         #1 - O(n) with space
-#         suff = [x for x in nums]
-#         for i in range(len(suff)-2, -1, -1):
-#             suff[i] = suff[i]*suff[i+1]
+        suff = [x for x in nums]
+        for i in range(len(suff)-2, -1, -1):
+            suff[i] = suff[i]*suff[i+1]
         
-#         ans = [1]*len(nums)
-#         pref = 1
-#         suff.append(1)
-#         for i in range(len(nums)):
-#             ans[i] = pref*suff[i+1]
-#             pref *= nums[i]
-#         return ans
+        ans = [1]*len(nums)
+        pref = 1
+        suff.append(1)
+        for i in range(len(nums)):
+            ans[i] = pref*suff[i+1]
+            pref *= nums[i]
+        return ans
         
         #2 o(n) without space
         res = [1]*len(nums)
