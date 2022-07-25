b4a8c232a543322aaba80aa652860d4dff8c0a17
Himanshu Soni <himanshusoni3105@outlook.com>
2022-07-25 13:43:53 +0530

    Time: 61 ms (28.53%), Space: 14.3 MB (26.96%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
@@ -7,22 +7,42 @@
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
         #see copy notes - tree striver
-        verticals = defaultdict(list)
-        queue = deque([(root, 0, 0)]) # node, x, y
+#         verticals = defaultdict(list)
+#         queue = deque([(root, 0, 0)]) # node, x, y
         
-        while queue:
-            for _ in range(len(queue)):
-                node, x, y = queue.popleft()
-                verticals[x].append((y, node.val))
+#         while queue:
+#             for _ in range(len(queue)):
+#                 node, x, y = queue.popleft()
+#                 verticals[x].append((y, node.val))
+                
+#                 if node.left:
+#                     queue.append((node.left, x-1, y+1)) #
+#                 if node.right:
+#                     queue.append((node.right, x+1, y+1))
+            
+#         output = []
+#         for x in sorted(verticals.keys()):
+#             column = [i[1] for i in sorted(verticals[x])]
+#             output.append(column)
+#         return output
+    
+        #Or can use heap inside map to not do sorting again see.
+        vert = defaultdict(list)
+        q = deque([(root, 0, 0)])
+        while q:
+            for _ in range(len(q)):
+                node, v, lev = q.popleft()
+                heappush(vert[v], (lev, node.val))
                 
                 if node.left:
-                    queue.append((node.left, x-1, y+1)) #
+                    q.append((node.left, v-1, lev+1))
                 if node.right:
-                    queue.append((node.right, x+1, y+1))
-            
-        output = []
-        for x in sorted(verticals.keys()):
-            column = [i[1] for i in sorted(verticals[x])]
-            output.append(column)
+                    q.append((node.right, v+1, lev+1))
         
-        return output
+        out = []
+        for v in sorted(vert.keys()):
+            col = []
+            while vert[v]:
+                col += [heappop(vert[v])[1]]
+            out.append(col)
+        return out
