bdb492132da81aa76de1b7853113dbf593ce3c5c
Himanshu Soni <himanshusoni3105@outlook.com>
2022-07-26 17:38:43 +0530

    Time: 1084 ms (84.45%), Space: 36.2 MB (45.28%) - LeetHub

--- 853-car-fleet/853-car-fleet.py
+++ 853-car-fleet/853-car-fleet.py
@@ -9,14 +9,14 @@ class Solution:
         
         #1 Stack
         
-        # pair = [(p, s) for p, s in zip(position, speed)]
-        # pair.sort(reverse=True)
-        # stack = []
-        # for p, s in pair:  # Reverse Sorted Order
-        #     stack.append((target - p) / s)          #ttt
-        #     if len(stack) >= 2 and stack[-1] <= stack[-2]:
-        #         stack.pop()
-        # return len(stack)
+        pair = [(p, s) for p, s in zip(position, speed)]
+        pair.sort(reverse=True)
+        stack = []
+        for p, s in pair:  # Reverse Sorted Order
+            stack.append((target - p) / s)          #ttt
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
         
         #2 without stack
         carconfig = [(pos, (target-pos)/sp) for pos, sp in zip(position, speed)]    #(pos, ttt)
