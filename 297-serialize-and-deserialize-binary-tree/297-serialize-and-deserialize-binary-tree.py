1b4698aac23f8319c7de5c96cda51418fdb5753c
Himanshu Soni <himanshusoni3105@outlook.com>
2022-07-28 14:01:08 +0530

    Time: 334 ms (28.54%), Space: 20.2 MB (64.60%) - LeetHub

--- 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.py
@@ -1,59 +1,88 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+#-ve values string me -5-6-7 ese jayegi to dhyan rakhna hai isliye separator chaiye hota
 
+
+#DFS
 class Codec:
-
-    def serialize(self, root):
-        if not root:    return ""
+    def serialdfs(self, root):
+        if not root:
+            return "N"
+        return str(root.val) +'/'+ self.serialdfs(root.left) +'/'+ self.serialdfs(root.right)
+    
+    def desdfs(self, data):
+        if data[self.i] == 'N':
+            return None
+        root = TreeNode(data[self.i])
         
-        s, q = "",  deque([root])
-        while q:
-            for _ in range(len(q)):
-                node = q.popleft()
-                if node:
-                    s += str(node.val) + "/"
-                else:
-                    s += "#/"
-                if node:
-                    q.append(node.left)
-                    q.append(node.right)
-        return s
-        
-
-    def deserialize(self, data):
-        if not data:    return None
-        s = data.split('/')
-        i = 0
-        
-        root = TreeNode(s[0])
-        q = deque([root])
-        while q:
-            for _ in range(len(q)):
-                node = q.popleft()
-
-                i += 1
-                if s[i] == "#":
-                    node.left = None
-                else:
-                    node.left = TreeNode(s[i])
-                    q.append(node.left)
-
-                i+=1
-                if s[i] == "#":
-                    node.right = None
-                else:
-                    node.right = TreeNode(s[i])
-                    q.append(node.right)
-
+        self.i += 1
+        root.left = self.desdfs(data)
+        self.i += 1
+        root.right = self.desdfs(data)
         return root
         
+    def serialize(self, root):
+        s = self.serialdfs(root)
+        print(s)
+        return s
+        
+    def deserialize(self, data):
+        data = data.split('/')
+        self.i = 0
+        return self.desdfs(data)
+    
+    
+    
+        
+#BFS
+# class Codec:
+
+#     def serialize(self, root):
+#         if not root:    return ""
+        
+#         s, q = "",  deque([root])
+#         while q:
+#             for _ in range(len(q)):
+#                 node = q.popleft()
+#                 if node:
+#                     s += str(node.val) + "/"
+#                 else:
+#                     s += "#/"
+#                 if node:
+#                     q.append(node.left)
+#                     q.append(node.right)
+#         return s
         
 
-# Your Codec object will be instantiated and called as such:
+#     def deserialize(self, data):
+#         if not data:    return None
+        
+#         s = data.split('/')
+#         i = 0
+        
+#         root = TreeNode(s[0])
+#         q = deque([root])
+#         while q:
+#             for _ in range(len(q)):
+#                 node = q.popleft()
+
+#                 i += 1
+#                 if s[i] == "#":
+#                     node.left = None
+#                 else:
+#                     node.left = TreeNode(s[i])
+#                     q.append(node.left)
+
+#                 i+=1
+#                 if s[i] == "#":
+#                     node.right = None
+#                 else:
+#                     node.right = TreeNode(s[i])
+#                     q.append(node.right)
+
+#         return root
+        
+        
+
+# # Your Codec object will be instantiated and called as such:
 # ser = Codec()
 # deser = Codec()
 # ans = deser.deserialize(ser.serialize(root))
