08333617c0db24f1b84856103ab314f73c88a61e
Himanshu Soni <himanshusoni3105@outlook.com>
2022-07-28 14:31:46 +0530

    Time: 278 ms (86.38%), Space: 33.1 MB (7.78%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -4,6 +4,7 @@ class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         ans = 0 
         
+        #1 ye b chal rha
         def dfs(root, pathmax):
             nonlocal ans
             if not root:
@@ -13,8 +14,28 @@ class Solution:
                 ans += 1
             
             dfs(root.left, max(pathmax, root.val))
-            
             dfs(root.right, max(pathmax, root.val))
+        
+        dfs(root, -inf)
+        return ans
+    
+    
+        #2 ye b chal rha 
+        def dfs(root, pathmax):
+            nonlocal ans
+            if not root:
+                return 
+            
+            if root.val >= pathmax:
+                ans += 1
+            
+            prepathmax = pathmax
+            dfs(root.left, max(pathmax, root.val))
+            pathmax = prepathmax
+            
+            prepathmax = pathmax
+            dfs(root.right, max(pathmax, root.val))
+            pathmax = prepathmax
         
         dfs(root, -inf)
         return ans
