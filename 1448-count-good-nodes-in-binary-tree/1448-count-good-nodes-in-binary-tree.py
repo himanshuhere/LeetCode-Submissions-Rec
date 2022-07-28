481c605a5c678cd8dc16a1d9b6b695d9778a02b9
Himanshu Soni <himanshusoni3105@outlook.com>
2022-07-28 14:31:11 +0530

    Time: 419 ms (39.53%), Space: 32.5 MB (86.58%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -12,13 +12,9 @@ class Solution:
             if root.val >= pathmax:
                 ans += 1
             
-            prepathmax = pathmax
             dfs(root.left, max(pathmax, root.val))
-            pathmax = prepathmax
             
-            prepathmax = pathmax
             dfs(root.right, max(pathmax, root.val))
-            pathmax = prepathmax
         
         dfs(root, -inf)
         return ans
