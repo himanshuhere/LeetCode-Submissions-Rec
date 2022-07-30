ee8c4a4c48b26196d1e9bb945b1bc63b6cb3a8bf
Himanshu Soni <himanshusoni3105@outlook.com>
2022-07-30 14:04:00 +0530

    Time: 1279 ms (50.67%), Space: 20.8 MB (10.33%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.py
@@ -0,0 +1,25 @@
+class Solution:
+    def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+        res = []
+        counters = [Counter(w) for w in words2]
+        #now make a universal counter by doing UNION of all substring map
+        
+        unicounter = defaultdict(int)
+        for m in counters:
+            for k in m:
+                if k not in unicounter:
+                    unicounter[k] = m[k]
+                else:
+                    if m[k] > unicounter[k]:
+                        unicounter[k] = m[k]
+        
+        for w in words1:
+            m1 = Counter(w)
+            flag = False
+            for k in unicounter:
+                if k not in m1 or unicounter[k] > m1[k]:
+                    flag = True
+                    break
+            if not flag:
+                res.append(w)
+        return res
