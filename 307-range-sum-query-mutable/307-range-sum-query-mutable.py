227e5daf78b0282573d6a3c9b5c3c56e3c189a55
Himanshu Soni <himanshusoni3105@outlook.com>
2022-07-31 12:43:45 +0530

    Time: 5547 ms (10.51%), Space: 48.8 MB (23.75%) - LeetHub

---
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.py
@@ -0,0 +1,58 @@
+#Basic prefix logic se toh TLE. 9/15 TC passed only.
+#Read NOTES. I am making now segment tree recursive code
+
+class TreeNode:
+    def __init__(self, i, j):
+        self.i = i
+        self.j = j
+        self.left = None
+        self.right = None
+        self.sum = 0
+
+class NumArray:
+    def buildtree(self, arr, i, j):
+        if i == j:
+            root = TreeNode(i, j)
+            root.sum = arr[i]
+        else:
+            mid = i + (j-i)//2
+            root = TreeNode(i, j)
+            root.left = self.buildtree(arr, i, mid)
+            root.right = self.buildtree(arr, mid+1, j)
+            root.sum = root.left.sum + root.right.sum
+        return root
+    
+    def updatetree(self, root, index, val):
+        if root.i == index == root.j:       #reached the correct leaf node
+            root.sum = val
+        else:
+            mid = root.i + (root.j - root.i)//2
+            if index <= mid:
+                root.left = self.updatetree(root.left, index, val)
+            else:
+                root.right = self.updatetree(root.right, index, val)
+            root.sum = root.left.sum + root.right.sum
+        return root
+    
+    def sumrange(self, root, l, r):
+        #completly overlap
+        if root.i >= l and root.j <= r:
+            return root.sum
+        
+        #disjoint, no overlap
+        if root.i > r or root.j < l:
+            return 0                    #0 for sum, -inf for range max query, +inf for range min query
+        
+        #partial overlap, let recursion handle it
+        return self.sumrange(root.left, l, r) + self.sumrange(root.right, l, r)
+    
+    def __init__(self, nums: List[int]):
+        self.root = self.buildtree(nums, 0, len(nums)-1)
+
+    def update(self, index: int, val: int) -> None:
+        self.updatetree(self.root, index, val)
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.sumrange(self.root, left, right)
+
+
