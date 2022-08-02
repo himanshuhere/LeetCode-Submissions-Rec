56bcc51ba2c56b5b97560d4043935813f2a695ac
Himanshu Soni <himanshusoni3105@outlook.com>
2022-08-02 13:16:42 +0530

    Time: 298 ms (54.47%), Space: 18.7 MB (80.82%) - LeetHub

---
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
@@ -0,0 +1,43 @@
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        #Ready for interview
+        #1 Sorting TC: MLOGM where M=n^n | SC: n^n
+        #go throught entire matrix an dput then in a new array
+        #sort array and find the kth index
+        
+        
+        #2 maxheap | MlogK, M=n^n | logK
+        maxheap = []
+        for row in matrix:
+            for c in row:
+                heappush(maxheap, -c)
+                if len(maxheap) > k:
+                    heappop(maxheap)
+        return -maxheap[0]
+        
+        
+        
+        #not sorted for imaginary
+        
+        n, m = len(matrix), len(matrix[0])
+        def countSmaller(num):
+            i, j, cnt = 0, m-1, 0
+            while i < n and j >= 0:
+                if matrix[i][j] < num:
+                    cnt += j+1
+                    i += 1
+                else:
+                    j -= 1
+            return cnt
+        
+#         def helper():
+#             lo, hi = matrix[0][0], matrix[-1][-1]
+#             while lo < hi:
+#                 mid = (lo + hi)//2
+#                 if countSmaller(mid) >= k:
+#                     hi = mid-1
+#                 else:
+#                     lo = mid+1
+#             return hi
+        
+#         return helper()
