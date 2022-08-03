e74d4c3782b54fa8c3bd7887aa4e0363c780130a
Himanshu Soni <himanshusoni3105@outlook.com>
2022-08-03 12:28:17 +0530

    Time: 264 ms (90.31%), Space: 14.7 MB (92.29%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.py
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -1,19 +1,48 @@
+# class MyCalendar:
+
+    #1. brute workd with set, but jaha tak i know ye series sayd seg tree me hai #o(n^2)
+#     def __init__(self):
+#         self.bookings = set()
+
+#     def book(self, st: int, end: int) -> bool:
+#         if (st, end) in self.bookings:
+#             return False
+#         for u, v in self.bookings:
+#             if not (v<=st or end<=u):
+#                 return False
+#         self.bookings.add((st, end))
+#         return True
+    
+    #2 - dekho mene dekha and socha yes ordered set ho to binary search laga sakte. but like java  we dont have any tree map. So what we ll create wese b bahot contest me bc haara hu kuki treemap nhi pata tha pythn ka aur unko complexity logn chaiye rehti this to lo template aaj.
+    
+class Node:
+    def __init__(self, st, end):
+        self.st = st
+        self.end = end
+        self.left = None
+        self.right = None
+    
+    def insert(self, node_st, node_end):             #root, node
+        if node_end <= self.st:
+            if not self.left:
+                self.left = Node(node_st, node_end)        #inserted
+                return True
+            return self.left.insert(node_st, node_end)
+        elif node_st >= self.end:
+            if not self.right:
+                self.right = Node(node_st, node_end)        #inserted
+                return True
+            return self.right.insert(node_st, node_end)
+        else:
+            return False               #overlapping 
+
 class MyCalendar:
-
-    #brute workd with set, but jaha tak i know ye series sayd seg tree me hai 
     def __init__(self):
-        self.bookings = set()
-
-    def book(self, st: int, end: int) -> bool:
-        if (st, end) in self.bookings:
-            return False
-        for u, v in self.bookings:
-            if not (v<=st or end<=u):
-                return False
-        self.bookings.add((st, end))
-        return True
-
-
-# Your MyCalendar object will be instantiated and called as such:
-# obj = MyCalendar()
-# param_1 = obj.book(start,end)
+        self.root = None
+    
+    def book(self, st, end):
+        if self.root == None:
+            self.root = Node(st, end)
+            return True
+        return self.root.insert(st, end)
+    
