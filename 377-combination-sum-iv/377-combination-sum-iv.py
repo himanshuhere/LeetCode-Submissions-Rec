626d5c3ef23599c1e2d32a3264bcdc0d6628a74b
Himanshu Soni <himanshusoni3105@outlook.com>
2022-08-05 12:13:44 +0530

    Time: 516 ms (5.17%), Space: 25.1 MB (5.17%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.py
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -1,5 +1,9 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
+        #Initially i did take not take, but see waha pe you go forward only and here any order combination is possible
+        #okay so i thought of doing same on nums then nums[::-1], but how would you eliminate duplicates okay plus there are many other issues
+        #then i thought if we can chose any element after any, lets loop over entire array, means select one and then again go 0-n, then select any and again go to 0-n, but gives Recursion maximum limit exceeded, because then not take function case was not necessary, yes its a loop it handles not take case through loop itself.
+        
         @lru_cache(None)
         def f(i, t):
             if t==0:
