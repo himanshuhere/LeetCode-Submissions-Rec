c1cb8733a069c0a4dd5800f8c838a8c1d0a1bb4b
Himanshu Soni <himanshusoni3105@outlook.com>
2022-08-07 12:57:58 +0530

    Time: 1090 ms (31.85%), Space: 55.2 MB (34.44%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -1,27 +1,27 @@
 class Solution:
     def countVowelPermutation(self, n: int) -> int:
-#         #smart people
-#         map = {
-#             ' ': ['a', 'e', 'i', 'o', 'u'],
-#             'a': ['e'],
-#             'e': ['a', 'i'],
-#             'i': ['a', 'e', 'o', 'u'],
-#             'o': ['i', 'u'],
-#             'u': ['a']
-#         }
+        #smart people
+        map = {
+            ' ': ['a', 'e', 'i', 'o', 'u'],
+            'a': ['e'],
+            'e': ['a', 'i'],
+            'i': ['a', 'e', 'o', 'u'],
+            'o': ['i', 'u'],
+            'u': ['a']
+        }
 
-#         @lru_cache(None)
-#         def dp(n, last):
-#             if n == 0: 
-#                 return 1          
-#             ans = 0
-#             for nxt in map[last]:                
-#                 ans = (ans + dp(n-1, nxt)) % 1_000_000_007
-#             return ans
-#         return dp(n, ' ')
+        @lru_cache(None)
+        def dp(n, last):
+            if n == 0: 
+                return 1          
+            ans = 0
+            for nxt in map[last]:                
+                ans = (ans + dp(n-1, nxt)) % 1_000_000_007
+            return ans
+        return dp(n, ' ')
         
         
-        #me 
+        #2 me 
         @lru_cache(None)
         def fn(n, prev):
             if n == 0:  #if everythinh selected till here, wow 1 n len str can be made
