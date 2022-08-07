fa8090feed0b246af5d47b34e47ae94e87609a29
Himanshu Soni <himanshusoni3105@outlook.com>
2022-08-07 12:43:28 +0530

    Time: 2487 ms (5.14%), Space: 174.9 MB (17.68%) - LeetHub

---
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -0,0 +1,39 @@
+class Solution:
+    def countVowelPermutation(self, n: int) -> int:
+#         #smart people
+#         map = {
+#             ' ': ['a', 'e', 'i', 'o', 'u'],
+#             'a': ['e'],
+#             'e': ['a', 'i'],
+#             'i': ['a', 'e', 'o', 'u'],
+#             'o': ['i', 'u'],
+#             'u': ['a']
+#         }
+
+#         @lru_cache(None)
+#         def dp(n, last):
+#             if n == 0: 
+#                 return 1          
+#             ans = 0
+#             for nxt in map[last]:                
+#                 ans = (ans + dp(n-1, nxt)) % 1_000_000_007
+#             return ans
+#         return dp(n, ' ')
+        
+        
+        #me 
+        @lru_cache(None)
+        def fn(n, cur):
+            if n == 0:
+                return 1
+                
+            ways = 0
+            for c in ['a', 'e', 'i', 'o', 'u']:
+                if (cur=='a' and c=='e') or (cur=='e' and (c=='a' or c=='i')) or (cur=='i' and not c=='i') or (cur=='o' and (c=='i' or c=='u')) or (cur=='u' and c=='a'):
+                    ways += fn(n-1, c)
+            return ways
+        
+        a=0
+        for c in ['a', 'e', 'i', 'o', 'u']:
+            a+=fn(n-1, c)
+        return a%1_000_000_007
