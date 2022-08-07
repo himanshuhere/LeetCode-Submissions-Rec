195097c34555c583475adf6309806b58be56aac2
Himanshu Soni <himanshusoni3105@outlook.com>
2022-08-07 12:47:37 +0530

    Time: 3446 ms (5.14%), Space: 175.2 MB (16.20%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -29,11 +29,9 @@ class Solution:
                 
             ways = 0
             for c in ['a', 'e', 'i', 'o', 'u']:
-                if (cur=='a' and c=='e') or (cur=='e' and (c=='a' or c=='i')) or (cur=='i' and not c=='i') or (cur=='o' and (c=='i' or c=='u')) or (cur=='u' and c=='a'):
+                if cur == '' or (cur=='a' and c=='e') or (cur=='e' and (c=='a' or c=='i')) or (cur=='i' and not c=='i') or (cur=='o' and (c=='i' or c=='u')) or (cur=='u' and c=='a'):
                     ways += fn(n-1, c)
             return ways
         
-        a=0
-        for c in ['a', 'e', 'i', 'o', 'u']:
-            a+=fn(n-1, c)
+        a = fn(n, '')
         return a%1_000_000_007
