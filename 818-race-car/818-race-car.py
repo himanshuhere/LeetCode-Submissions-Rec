ee511417c8a9a4f36dd6194b9515fc43649241bc
Himanshu Soni <himanshusoni3105@outlook.com>
2022-08-11 03:34:36 +0530

    Time: 87 ms (48.84%), Space: 14.5 MB (65.28%) - LeetHub

--- 818-race-car/818-race-car.py
+++ 818-race-car/818-race-car.py
@@ -27,8 +27,11 @@ class Solution:
             
             
             
-            #The BFS is memorizing pairs of speed and positions. So the total time complexity will be the number of such pairs formed before we hit the target.
-#Now, How many different positions are possible to reach to target. In worst case we, could have visited every single position in both the directions i.e. from -target to +target so the total no of positions possible are O(target).
-#Now, to check total no of speeds possible we could go either 1, 2, 4, 8.. or -1, -2, -4 , -8 .. .. upto (target). because we will always reach the target with speed bounded by target position. So total no of distinct speeds are 2O(log target)
-#Hence time complexity= Total distinct positions(=target) * total distinct speeds (=log(target)) = O(targetlog(target)).
-#some says 2^logn
+#             To know the TC, we first need to figure out V (the number of vertices in the graph) and E (the number of edges in the graph), because we know TC of BFS is O(V + E). For this question, each vertex is a (position, speed) state, and each (position, speed) state is connected to two other states (via accelerate or reverse). Therefore E is twice of V. So the TC can be simplified as O(V).
+# The number of positions is O(target) and number of speed is O(log(target)), so the number of state is O(target * log(target)) and this is the TC. If this analysis is too short to be convincing, the following is a more detailed analysis.
+# The solution visits 2 * target positions which are [0, 1, 2, ..., 2 * target - 1]
+# On each position you can try a number of speed and, as analysed below, this number is 2 * log2(target) + 4, at most.
+# The possible speed at each position is: [1, 2, 4, ..., s] and their opposite numbers [-1, -2, ..., -s]. s is the greatest number that is less than or equal to 2 * target (because speed greater than that would send the car out of the scope that we consider) and is power of 2. So, if s is written as 2^k, then k is the integer part of log2(2 * target).
+# So there are log2(target) + 2 (this is k+1, the length of [1, 2, ..., s] list) positive speeds, and hence 2 * log2(target) + 4 possible speeds. The number of (position, speed) states is: (2 * target) * (2 * log2(target) + 4) = 4 * target * log2(target) + 8 * target. This is the V (the number of vertices) of the graph we DFS on.
+# So the time complexity O(V) is O(4 * target * log2(target) + 8 * target), i.e. O(target * log(target)).
+# Thanks, vuluy, for your input.
