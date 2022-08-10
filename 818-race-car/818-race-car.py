b72e5022ed7cd0db385dbc8e500acb8a475f6c92
Himanshu Soni <himanshusoni3105@outlook.com>
2022-08-10 18:38:09 +0530

    Time: 64 ms (75.86%), Space: 14.4 MB (77.80%) - LeetHub

---
+++ 818-race-car/818-race-car.py
@@ -0,0 +1,20 @@
+class Solution:
+    def racecar(self, target: int) -> int:
+        queue = collections.deque([(0, 1)])
+        moves = 0
+        while queue:
+            for _ in range(len(queue)):
+                pos, vel = queue.popleft()
+                
+                if pos == target:
+                    return moves
+                
+                #2. Always consider moving the car in the direction it is already going.
+                queue.append((pos + vel, 2 * vel))
+                
+                #3. Only consider changing the direction of the car if one of the following conditions is true
+                #   i.  The car is driving away from the target.
+                #   ii. The car will pass the target in the next move.  
+                if (pos + vel > target and vel > 0) or (pos + vel < target and vel < 0):
+                    queue.append((pos, -1 if vel > 0 else 1))
+            moves += 1
