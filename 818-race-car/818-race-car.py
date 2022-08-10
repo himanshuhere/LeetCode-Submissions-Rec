446fdd42b3529071b58b3942dbd3c02ee0fd7016
Himanshu Soni <himanshusoni3105@outlook.com>
2022-08-11 03:17:23 +0530

    Time: 44 ms (96.70%), Space: 14.4 MB (65.28%) - LeetHub

--- 818-race-car/818-race-car.py
+++ 818-race-car/818-race-car.py
@@ -1,7 +1,7 @@
 class Solution:
     def racecar(self, target: int) -> int:
         
-        queue = collections.deque([(0, 1)]) #[pos, velocity]
+        queue = deque([(0, 1)]) #[pos, velocity]
         moves = 0
         while queue:
             for _ in range(len(queue)):
