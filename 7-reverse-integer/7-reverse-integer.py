6c6c1635326c6e7dd07dce68e823e61aa7ee8a75
Himanshu Soni <himanshusoni3105@outlook.com>
2022-08-10 15:10:02 +0530

    Time: 83 ms (5.20%), Space: 13.9 MB (63.35%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -1,5 +1,7 @@
 class Solution:
     def reverse(self, x: int) -> int:
+        #kuch nhi last se nikalo new me lagate jao dekh lo ek bar, neg bas alag se handle karna
+        
         neg = False
         if x < 0:
             neg = True
@@ -10,10 +12,11 @@ class Solution:
             last = x%10
             x //= 10
             new = new*10+last
-            if not (-2**31<=new<=2**31-1):
-                return 0
+            
         
         if neg:
             new = -new
+        if not (-2**31<=new<=2**31-1):
+                return 0
         return new
         
