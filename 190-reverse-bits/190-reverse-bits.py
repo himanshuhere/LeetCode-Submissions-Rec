2a5e8e5d4522c1cf03a5f72a79680cc98bb295a1
Himanshu Soni <himanshusoni3105@outlook.com>
2022-08-10 13:48:47 +0530

    Time: 46 ms (65.63%), Space: 13.9 MB (7.67%) - LeetHub

---
+++ 190-reverse-bits/190-reverse-bits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        ans = 0
+        for _ in range(31):
+            if n&1 == 1:
+                ans += 1
+            ans <<= 1
+            n >>= 1
+        
+        ans += n&1 # add the last bit 
+        return ans
