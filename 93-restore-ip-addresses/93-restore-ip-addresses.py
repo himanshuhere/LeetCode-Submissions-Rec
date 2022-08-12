25484f4e130f71a3cfe0e6f616e9a5426298d713
Himanshu Soni <himanshusoni3105@outlook.com>
2022-08-12 13:44:02 +0530

    Time: 80 ms (17.45%), Space: 13.9 MB (82.74%) - LeetHub

--- 93-restore-ip-addresses/93-restore-ip-addresses.py
+++ 93-restore-ip-addresses/93-restore-ip-addresses.py
@@ -9,19 +9,42 @@ class Solution:
                     return False
             return True
         
-        for i in range(1, 4):           #1,2,3
-            for j in range(i+1, i+4):
-                for k in range(j+1, j+4):
-                    if k >= len(s):
-                        break
-                    s1 = s[:i]
-                    s2 = s[i:j]
-                    s3 = s[j:k]
-                    s4 = s[k:]
+        def dfs(i, cur, par):
+            if i == n or par == 4:          #if any then get inside
+                if i == n and par == 4:     #if both then only right
+                    res.append(cur[:-1])    #remove last .
+                return
+                
                     
-                    if valid([s1,s2,s3,s4]):
-                        ans.append(s1+"."+s2+"."+s3+"."+s4)
-        return ans
+            dfs(i+1, cur+s[i]+".", par+1)
+            
+            if i+1 < n and valid([s[i:i+2]]):
+                dfs(i+2, cur+s[i:i+2]+".", par+1)
+                
+            if i+2 < n and valid([s[i:i+3]]):
+                dfs(i+3, cur+s[i:i+3]+".", par+1)
+        
+        res = []
+        n = len(s)
+        dfs(0, "", 0)
+        return res
+            
+                        
+        
+        
+#         for i in range(1, 4):           #1,2,3
+#             for j in range(i+1, i+4):
+#                 for k in range(j+1, j+4):
+#                     if k >= len(s):
+#                         break
+#                     s1 = s[:i]
+#                     s2 = s[i:j]
+#                     s3 = s[j:k]
+#                     s4 = s[k:]
+                    
+#                     if valid([s1,s2,s3,s4]):
+#                         ans.append(s1+"."+s2+"."+s3+"."+s4)
+#         return ans
     
     #substring - o(n)
     #three loops constant so, 3^3, each loop possibility is 3 too
