9c9eb63c1eb21b45fcbdc7a880317d09f3a0b56e
Himanshu Soni <himanshusoni3105@outlook.com>
2022-08-14 16:24:52 +0530

    Time: 44 ms (83.35%), Space: 13.9 MB (90.32%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
@@ -3,19 +3,21 @@ class Solution:
         if not preorder:
             return None
         
-        return self.construct(preorder, 0, len(preorder))
+        return self.construct(preorder, 0, len(preorder)-1)
     
     def construct(self, preorder, l, r):
-        if l >= r:
+        if l > r:
             return None
+        if l == r:
+            return TreeNode(preorder[l])
         
         root = TreeNode(preorder[l])
         i = l+1
         
-        while(i < r and preorder[i] < root.val):
+        while i <= r and preorder[i] < root.val:
             i += 1
     
-        root.left = self.construct(preorder, l+1, i)
+        root.left = self.construct(preorder, l+1, i-1)
         root.right = self.construct(preorder, i, r)     #r or i + (r-i)
         
         return root
