322400607d6cf44a3556adee9bf56f22a236ea07
Himanshu Soni <himanshusoni3105@outlook.com>
2022-08-15 00:51:06 +0530

    Time: 39 ms (99.07%), Space: 14.5 MB (56.86%) - LeetHub

--- 818-race-car/818-race-car.py
+++ 818-race-car/818-race-car.py
@@ -10,14 +10,9 @@ class Solution:
                 
                 if pos == target:
                     return moves
-                
-                #2. Always consider moving the car in the direction it is already going.
-                #ye accel ka mtlb hamesha forward nhi h jis b dire me hai car wahi jate raho aur niche eale reverse op to bas modne k liye h isko agar codition me dal diye to gadi ruk jani hai <--/-->
+               
                 queue.append((pos + vel, 2 * vel))
                 
-                #3. Only consider changing the direction of the car if one of the following conditions is true
-                #   i.  The car is driving away from the target.
-                #   ii. The car will pass the target in the next move.  
                 if (pos + vel > target and vel > 0):
                     queue.append((pos, -1))
                 
