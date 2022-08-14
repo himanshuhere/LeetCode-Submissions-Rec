e2764d335267e63e839db650509885eda182c969
Himanshu Soni <himanshusoni3105@outlook.com>
2022-08-14 16:00:05 +0530

    Time: 145 ms (20.78%), Space: 14.3 MB (14.38%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,7 +1,7 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
         def dfs(i, j):
-            if i<0 or j<0 or i>=m or j>=n or image[i][j]==newColor or image[i][j]!=oldcolor:
+            if i<0 or j<0 or i>=m or j>=n or image[i][j]!=oldcolor:
                 return 
             image[i][j] = newColor
             dfs(i+1,j)
@@ -11,12 +11,10 @@ class Solution:
         
         m, n = len(image), len(image[0])
         oldcolor = image[sr][sc]
-        dfs(sr,sc)
+        if oldcolor != newColor:
+            dfs(sr, sc)
         return image
     
-    #image[i][j]==newColor or image[i][j]!=oldcolor: both condition are necesarry yes
-    #bcs both could be same, thats the trick
-    #one simple way to pre check that 
-    # if oldColor != newColor:
-    #     dfs(sr, sc)
-    # return image
+    
+    
+   
