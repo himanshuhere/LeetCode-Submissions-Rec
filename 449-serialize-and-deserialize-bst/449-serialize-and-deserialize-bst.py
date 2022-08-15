670de12dc5be02a2c0aebb166423b2186a0ccbe2
Himanshu Soni <himanshusoni3105@outlook.com>
2022-08-15 19:52:40 +0530

    Time: 125 ms (49.27%), Space: 18.6 MB (48.44%) - LeetHub

---
+++ 449-serialize-and-deserialize-bst/449-serialize-and-deserialize-bst.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+#BT serialize to o(n) preorder any order mar do
+#but BST me b pre k bad optimized INORDER
+
+    def serialdfs(self, root):
+        if not root:
+            return "N"
+        return str(root.val) +'/'+ self.serialdfs(root.left) +'/'+ self.serialdfs(root.right)
+    
+    def desdfs(self, data):
+        if data[self.i] == 'N':
+            return None
+        
+        root = TreeNode(data[self.i])
+        
+        self.i += 1
+        root.left = self.desdfs(data)
+        self.i += 1
+        root.right = self.desdfs(data)
+        return root
+        
+    def serialize(self, root):
+        s = self.serialdfs(root)
+        print(s)
+        return s
+        
+    def deserialize(self, data):
+        data = data.split('/')
+        self.i = 0
+        return self.desdfs(data)
+
+# Your Codec object will be instantiated and called as such:
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# tree = ser.serialize(root)
+# ans = deser.deserialize(tree)
+# return ans
