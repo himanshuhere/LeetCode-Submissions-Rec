9dff32127cd697beef34f576aab20f1c05a3dbd2
Himanshu Soni <himanshusoni3105@outlook.com>
2022-08-15 18:58:58 +0530

    Time: 222 ms (19.71%), Space: 15.3 MB (90.07%) - LeetHub

---
+++ 224-basic-calculator/224-basic-calculator.py
@@ -0,0 +1,42 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        def calc(s):
+            st = []
+            i = 0
+
+            s = "+" + s
+            res, val, lsign = 0, 0, ''        #lastSign or sign before cur val
+        
+            while i < len(s):
+                if s[i].isdigit():
+                    val = 0
+                    while i < len(s) and s[i].isdigit():
+                        val = val*10 + int(s[i])
+                        i += 1
+                    i -= 1
+                    res += val if lsign == '+' else -val
+                    
+                elif s[i] == '+':    
+                    lsign = '+'
+                elif s[i] == '-':    
+                    lsign = '-'
+                
+                elif s[i] == '(':
+                    st.append(res)
+                    st.append(lsign)
+                    res = 0
+                    lsign = '+'
+                elif s[i] == ')':
+                    sign = st.pop()
+                    lastres = st.pop()
+                    res = (lastres+res) if sign == '+' else (lastres-res)
+                i += 1
+                
+            return sum(st) if st else res
+        return calc(s)
+                    
+        
+        
+                    
+            
+    
