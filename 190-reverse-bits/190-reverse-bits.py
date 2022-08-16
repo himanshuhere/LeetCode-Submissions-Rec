fcf2c2263827aa68286b95d17b1e06daff8e4857
Himanshu Soni <himanshusoni3105@outlook.com>
2022-08-16 23:58:12 +0530

    Time: 40 ms (81.03%), Space: 13.8 MB (94.11%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.py
+++ 190-reverse-bits/190-reverse-bits.py
@@ -8,14 +8,14 @@ class Solution:
         return res
             
         
-        #2
-        ans = 0
-        for _ in range(31):
-            if n&1 == 1:
-                ans |= 1        #last bit on karni hai : ans = ans OR 1 / ans += 1
-                #ans += 1
-            ans <<= 1
-            n >>= 1
+#         #2
+#         ans = 0
+#         for _ in range(31):
+#             if n&1 == 1:
+#                 ans |= 1        #last bit on karni hai : ans = ans OR 1 / ans += 1
+#                 #ans += 1
+#             ans <<= 1
+#             n >>= 1
         
-        ans += n&1 # add the last bit 
-        return ans
+#         ans += n&1 # add the last bit 
+#         return ans
