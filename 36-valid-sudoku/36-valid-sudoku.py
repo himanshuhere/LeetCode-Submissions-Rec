0864d32b5f3b4f37d45fd57da3a4dd7fbdd2501b
Himanshu Soni <himanshusoni3105@outlook.com>
2022-08-16 11:23:07 +0530

    Time: 125 ms (71.98%), Space: 14 MB (35.50%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValidSudoku(self, A: List[List[str]]) -> bool:
+        #o(n^2), o(n)
+        row=set()
+        
+        for i in range(len(A)):
+            for j in range(len(A[0])):
+                curr = A[i][j]
+                if(curr!='.'):
+                    
+                    r = ('r', i, curr)
+                    c = ('c', j, curr)
+                    b = ('b', i//3, j//3, curr)
+                    
+                    if r in row or c in row or b in row:
+                        return False
+                    
+                    row.add(r)
+                    row.add(c)
+                    row.add(b)
+                    
+        return True
