f00a56b08e71dba7d981414f039313f01f8631fa
Himanshu Soni <himanshusoni3105@outlook.com>
2022-08-17 20:24:52 +0530

    Time: 966 ms (91.88%), Space: 28.5 MB (37.75%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
@@ -6,3 +6,10 @@ class Solution:
         
         return min( max(j+1, i+1), max(n-i, n-j), i+1+n-j, j+1+n-i)
     
+    #left side se,
+    #right side se
+    #left right
+    #left right
+    
+    #we dont know i, j kom pehle hai to isliye left right me apan ne max() laga diya, aur isliye baki do condition
+    
