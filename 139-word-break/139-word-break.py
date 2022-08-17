2cad9c8cfe0bba3c4725aec75af528841b3c1447
Himanshu Soni <himanshusoni3105@outlook.com>
2022-08-17 13:24:38 +0530

    Time: 61 ms (51.65%), Space: 16.8 MB (5.10%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,22 +1,65 @@
+class TN:
+    def __init__(self):
+        self.ch = {}
+        self.end = False
+    
+    def insert(self, word):
+        node = self
+        for c in word:
+            if c not in node.ch:
+                node.ch[c] = TN()
+            node = node.ch[c]
+        node.end = True
+        
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
         @lru_cache(None)
-        def f(i, cur):
+        def f(i, node, cur):
             if i==n:
-                if cur in dic:
+                if node.end:
                     return True
                 return False
             
+            if s[i] not in node.ch:
+                return
+            
             cur += s[i]
             
-            if cur in dic:
-                return f(i+1, cur) or f(i+1, "")
+            node = node.ch[s[i]]
+            
+            #print(final, cur)
+            if node.end:
+                return f(i+1, root, "") or f(i+1, node, cur)
             else:
-                return f(i+1, cur)
+                return f(i+1, node, cur)
     
         n = len(s)
-        dic = set(wordDict)
-        return f(0, "")
+        root = TN()
+        for w in wordDict:
+            root.insert(w)
+        return f(0, root, "")
     
-    #Brute time is 2^n, as max two choices but string concat then N*2^N
-    #DP time : n*n*k, n and n for dp states and k for concat if needed
+        
+        #DP
+#         @lru_cache(None)
+#         def f(i, cur):
+#             if i==n:
+#                 if cur in dic:
+#                     return True
+#                 return False
+            
+#             cur += s[i]
+            
+#             if cur in dic:
+#                 return f(i+1, cur) or f(i+1, "")
+#             else:
+#                 return f(i+1, cur)
+    
+#         n = len(s)
+#         dic = set(wordDict)
+#         #return f(0, "")
+    
+    
+#     #Brute time is 2^n, as max two choices but string concat then N*2^N
+#     #DP time : n*n*k, n and n for dp states and k for concat if needed
