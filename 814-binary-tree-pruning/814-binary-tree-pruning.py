9fbb0494c57da25e8e781ac182b8526032fefb72
Himanshu Soni <himanshusoni3105@outlook.com>
2022-09-06 10:44:20 +0530

    Time: 63 ms (17.11%), Space: 14 MB (23.50%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.py
+++ 814-binary-tree-pruning/814-binary-tree-pruning.py
@@ -22,5 +22,15 @@ class Solution:
             
             return False
         
+        def dfs2(root):
+            if not root:
+                return None
+            root.left = self.pruneTree(root.left)
+            root.right = self.pruneTree(root.right)
+            if root.left or root.right or root.val == 1:
+               return root
+            else:
+               return None
+            
         return root if dfs(root) else None
                 
