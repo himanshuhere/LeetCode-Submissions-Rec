1d589de9b1b6571d1cac880689d3a55a8bda68bd
Himanshu Soni <himanshusoni3105@outlook.com>
2022-11-01 01:57:24 +0530

    Time: 126 ms (54.09%), Space: 17.8 MB (78.18%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -13,8 +13,7 @@ class MinStack:
     def pop(self):
         if len(self.stack) == 0:
             return
-        x = self.stack[-1]
-        self.stack.pop()
+        x = self.stack.pop()
         if x == self.minStack[-1]:
             self.minStack.pop()
 
@@ -29,55 +28,3 @@ class MinStack:
         return self.minStack[-1]
     
 #One stack
-class MinStack:
-#One stack - ghante ka kam space woi kaam ek stack me hora same space almost khair
-#Whole idea is to maintian first and second min next to each other in stack. whenvr new val is min than mininmum then this will be second min and new will be first, so puch already having min then push val given. This way you always have second min after min in stack. Lets say you pop it turn out to be min only, so you can make one more pop and you get the new min update. Do one test will clear
-    #Approach - two stack to one stack to LinkedList
-        
-    def __init__(self): #same thing u can do with LL
-        self.st = []
-        self.mn = math.inf
-    
-    def push(self, x):
-        if x <= self.mn:
-            self.st.append(self.mn)
-            self.mn = x
-        self.st.append(x)
-            
-
-    def pop(self):
-        if self.mn == self.st.pop():
-            self.mn = self.st.pop()
-        
-    def top(self):
-        return self.st[-1]
-    
-    def getMin(self):
-        return self.mn
-    
-#Linked List
-class Node:
-    def __init__(self, val, mn, nxt):
-        self.val = val
-        self.mn = mn
-        self.next = nxt
-        
-class MinStack:
-    #Approach - two stack to one stack to LinkedList
-    def __init__(self):
-        self.head = None
-    
-    def push(self, x):
-        if not self.head:
-            self.head = Node(x, x, None)
-        else:
-            self.head = Node(x, min(x, self.head.mn), self.head)
-            
-    def pop(self):
-        self.head = self.head.next
-
-    def top(self):
-        return self.head.val
-
-    def getMin(self):
-        return self.head.mn
